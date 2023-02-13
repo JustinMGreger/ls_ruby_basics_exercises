f2e876e5f96202b3619229f46dfec72770552bc9
Justin Greger <justingreger@gmail.com>
2023-02-13 16:47:11 -0500

    partial answer.

--- reading_documentation/3_return_value_of_while.rb
+++ reading_documentation/3_return_value_of_while.rb
@@ -1,3 +1,10 @@
 =begin
+Exercise:
 Using the ruby documentation, determine what value a while loop returns.
+
+Answer:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "control expressions".
+In the "control expressions" section search for "while loop".
 =end
