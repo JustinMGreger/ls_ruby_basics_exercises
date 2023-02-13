5e4a8100cb3e49738b14f34fa82c97fe674cd822
Justin Greger <justingreger@gmail.com>
2023-02-13 16:51:06 -0500

    answered.

--- reading_documentation/3_return_value_of_while.rb
+++ reading_documentation/3_return_value_of_while.rb
@@ -7,4 +7,6 @@ Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
 Select the API for the version of Ruby you are using.
 Search for "control expressions".
 In the "control expressions" section search for "while loop".
+Read the "while loop" section for the answer.
+The return value of a while loop is nil unless break is used to supply a value.
 =end
