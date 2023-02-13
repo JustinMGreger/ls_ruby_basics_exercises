e013b95dc04c61e87a8c14fd6483235a43e0c5e7
Justin Greger <justingreger@gmail.com>
2023-02-13 16:55:36 -0500

    description of exercise.

---
+++ reading_documentation/4_return_value_of_break.rb
@@ -0,0 +1,5 @@
+=begin
+Description of exercise:
+In the previous exercise, you learned that the while loop returns nil unless break is used. 
+Locate the documentation for break, and determine what value break sets the return value to for the while loop.
+=end
