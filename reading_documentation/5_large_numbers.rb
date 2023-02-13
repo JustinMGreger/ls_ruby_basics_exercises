d0af0a4f737e2b83fd1350ca8ad32f6ed9e21522
Justin Greger <justingreger@gmail.com>
2023-02-13 17:55:12 -0500

    partial answer.

--- reading_documentation/5_large_numbers.rb
+++ reading_documentation/5_large_numbers.rb
@@ -1,4 +1,11 @@
 =begin
 Description of exercise:
 Using the ruby documentation, determine how you can write large numbers in a way that makes them easier to read.
+
+Description of solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "literals".
+Once in the "literals" section search for "number".
+This sould bring you to Number Literals & specifically Integer Literals.
 =end
