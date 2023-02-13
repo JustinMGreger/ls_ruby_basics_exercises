2eb97d5296a8b22e6d1ba7f76d7f04f4fac432c5
Justin Greger <justingreger@gmail.com>
2023-02-13 16:41:15 -0500

    answered.

--- reading_documentation/2_while_loops.rb
+++ reading_documentation/2_while_loops.rb
@@ -1,3 +1,8 @@
 =begin
 Locate the description of the while loop in the ruby documentation.
+Answer:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "control expressions".
+In the "control expressions" section search for "while loop".
 =end
