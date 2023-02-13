9881a5f3f79052d849497f8565fe68a84bee7b7c
Justin Greger <justingreger@gmail.com>
2023-02-13 17:04:58 -0500

    partial answer.

--- reading_documentation/4_return_value_of_break.rb
+++ reading_documentation/4_return_value_of_break.rb
@@ -2,4 +2,14 @@
 Description of exercise:
 In the previous exercise, you learned that the while loop returns nil unless break is used. 
 Locate the documentation for break, and determine what value break sets the return value to for the while loop.
+
+Description of solution:
+Answer:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "control expressions".
+In the "control expressions" section search for "break".
+Read the "break" section for the answer.
+If break is not supplied a value then the return value is nil.
+The documentations says "break accepts a value that supplies the result of the expression it is “breaking” out of"
 =end
