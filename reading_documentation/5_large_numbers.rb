94e07e224cb320fe6eb681e04f176b05f21c3bb7
Justin Greger <justingreger@gmail.com>
2023-02-13 17:58:33 -0500

    completed.

--- reading_documentation/5_large_numbers.rb
+++ reading_documentation/5_large_numbers.rb
@@ -8,4 +8,12 @@ Select the API for the version of Ruby you are using.
 Search for "literals".
 Once in the "literals" section search for "number".
 This sould bring you to Number Literals & specifically Integer Literals.
+
+Answer:
+The documentation provides this answer:
+"1234
+1_234
+These numbers have the same value, 1,234. 
+The underscore may be used to enhance readability for humans. 
+You may place an underscore anywhere in the number."
 =end
