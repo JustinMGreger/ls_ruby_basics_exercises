ef2e685c7f20b2f1ebe1ed940da6e79446740727
Justin Greger <justingreger@gmail.com>
2023-02-14 14:50:51 -0500

    syntax and explanation.

--- variable_scope/1_what_is_my_value_part_1.rb
+++ variable_scope/1_what_is_my_value_part_1.rb
@@ -15,22 +15,61 @@ puts a
 
 
 Description of Solution:
+Syntax:
+a = 7
+
+Explanation:
 variable "a" is assigned to integer 7.
 
+Syntax:
+def my_value(b)
+  b += 10
+end
+
+Explanation:
 my_value is a method.
 my_value accepts the argument "b".
 my_value is defined as incrementing "b" by 10 using the += operator. 
 += is the addition assignment operator.
 This adds 10 to "b".
-That result of 10 added to "b" is then assigned to "b".
+That result of 10 added to "b" is then assigned to new object "b".
+Written differently b = b + 10
 There are no explicit returns in the my_value method.
 The return value is the value of the last expression evaluated.
-Which is the value of "b" after it has been incremented by 10.
+Which is the value of new object "b" after it has been incremented by 10.
+
 For example if my_value(1) were called the method would increment the value of "b" by 10.
-That would "b" = 1.
+That would be "b" = 1.
 Then 1 += 10.
-Which is 11.
+Which is new object "b" = 11.
 11 would be the return value.
+
+Syntax:
+my_value(a)
+
+Explanation:
+my_value method accepts the argument "a".
+my_value is a method.
+my_value accepts the argument "b".
+my_value is defined as incrementing "b" by 10 using the += operator. 
++= is the addition assignment operator.
+This adds 10 to "b".
+That result of 10 added to "b" is then assigned to new object "b".
+Written differently b = b + 10
+There are no explicit returns in the my_value method.
+The return value is the value of the last expression evaluated.
+Which is the value of new object "b" after it has been incremented by 10.
+
+For example if my_value(1) were called the method would increment the value of "b" by 10.
+That would be "b" = 1.
+Then 1 += 10.
+Which is new object "b" = 11.
+11 would be the return value.
+
+Syntax:
+puts a
+
+Explanation:
 
 Answer:
 =end
