879c87837a482dc175266f7a26f00213da3047b9
Justin Greger <justingreger@gmail.com>
2023-02-14 14:37:36 -0500

    my_value

--- variable_scope/1_what_is_my_value_part_1.rb
+++ variable_scope/1_what_is_my_value_part_1.rb
@@ -23,6 +23,14 @@ my_value is defined as incrementing "b" by 10 using the += operator.
 += is the addition assignment operator.
 This adds 10 to "b".
 That result of 10 added to "b" is then assigned to "b".
+There are no explicit returns in the my_value method.
+The return value is the value of the last expression evaluated.
+Which is the value of "b" after it has been incremented by 10.
+For example if my_value(1) were called the method would increment the value of "b" by 10.
+That would "b" = 1.
+Then 1 += 10.
+Which is 11.
+11 would be the return value.
 
 Answer:
 =end
