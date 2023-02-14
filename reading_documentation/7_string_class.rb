00282c6405f503cd0e1abed4d51133f2aa7eab89
Justin Greger <justingreger@gmail.com>
2023-02-13 19:49:11 -0500

    description of exercise.

---
+++ reading_documentation/7_string_class.rb
@@ -0,0 +1,5 @@
+=begin
+Description of exercise:
+Let's move on now to the documentation you will use most often; the core API section on Classes and Modules. 
+All of these are listed under Classes on the Core API page.
+=end
