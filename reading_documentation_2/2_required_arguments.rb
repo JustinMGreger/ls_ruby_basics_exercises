3dc86a7dd528c4407e8acdc46325609c49d61984
Justin Greger <justingreger@gmail.com>
2023-02-13 21:12:18 -0500

    partial answer.

---
+++ reading_documentation_2/2_required_arguments.rb
@@ -0,0 +1,13 @@
+=begin
+Description of Exercise:
+Assume you have this Array:
+a = %w(a b c d e)
+How would you use Array#insert to insert the numbers 5, 6, and 7 between the elements with values 'c' and 'd'?
+
+Description of Solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "Array".
+The first result should take you to the Array Class.
+Search the word "insert" on the Array Class page.
+=end
