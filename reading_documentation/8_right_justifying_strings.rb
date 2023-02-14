b72156f3d6ab1ce044e65f7d37aef235453922e3
Justin Greger <justingreger@gmail.com>
2023-02-13 20:17:47 -0500

    completed.

--- reading_documentation/8_right_justifying_strings.rb
+++ reading_documentation/8_right_justifying_strings.rb
@@ -7,4 +7,9 @@ Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
 Select the API for the version of Ruby you are using.
 Search for "String".
 The first result should take you to the String Class.
+Search the word "right" on the String Class page.
+You should find "rjust" for right-justified.
+
+Answer:
+rjust
 =end
