68a352a04819da773492901db3430a0de4c35911
Justin Greger <justingreger@gmail.com>
2023-02-14 16:39:11 -0500

    Syntax.

--- variable_scope/3_what_is_my_value_part_3.rb
+++ variable_scope/3_what_is_my_value_part_3.rb
@@ -4,6 +4,15 @@ What will the following code print, and why?
 Don't run the code until you have tried to answer.
 
 Syntax:
+a = 7
+
+def my_value(b)
+  a = b
+end
+
+my_value(a + 5)
+puts a
+
 Description of Solution:
 Syntax: 
 Explanation:
