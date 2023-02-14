defbb5b7f3a97ea3a0ca8a1d6bbf7cde45b7023b
Justin Greger <justingreger@gmail.com>
2023-02-13 20:32:22 -0500

    partial answer.

--- reading_documentation_2/1_methods_without_arguments.rb
+++ reading_documentation_2/1_methods_without_arguments.rb
@@ -1,4 +1,11 @@
 =begin
 Description of Exercise:
 How would you use String#upcase to create an uppercase version of the string "xyz"?
+
+Description of Solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "String".
+The first result should take you to the String Class.
+Search the word "upcase" on the String Class page.
 =end
