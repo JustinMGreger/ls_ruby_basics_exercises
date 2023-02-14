6c715915506f522a4bc026805c263f025e287adc
Justin Greger <justingreger@gmail.com>
2023-02-13 20:11:55 -0500

    string class.

--- reading_documentation/8_right_justifying_strings.rb
+++ reading_documentation/8_right_justifying_strings.rb
@@ -1,4 +1,10 @@
 =begin
 Description of Exercise:
 Use the ruby documentation for the String class to determine which method can be used to right justify a String object.
+
+Description of Solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "String".
+The first result should take you to the String Class.
 =end
