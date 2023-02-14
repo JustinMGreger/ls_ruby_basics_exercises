d45f7b2d23f0083f1bfb6f696148b6183bab5528
Justin Greger <justingreger@gmail.com>
2023-02-14 12:24:44 -0500

    partial answer.

--- reading_documentation_2/3_optional_arguments.rb
+++ reading_documentation_2/3_optional_arguments.rb
@@ -6,4 +6,11 @@ puts s.split.inspect
 puts s.split(',').inspect
 puts s.split(',', 2).inspect
 What will each of the 3 puts statements print?
+
+Description of Solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "String".
+The first result should take you to the String Class.
+Search the word "split" on the String Class page.
 =end
