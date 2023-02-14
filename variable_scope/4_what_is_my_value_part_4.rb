8f92b505c1558d264644b39ea77439ce5daf2a34
Justin Greger <justingreger@gmail.com>
2023-02-14 17:31:51 -0500

    Description of Exercise & Syntax.

--- variable_scope/4_what_is_my_value_part_4.rb
+++ variable_scope/4_what_is_my_value_part_4.rb
@@ -1,4 +1,16 @@
 =begin
 Description of Exercise:
+What will the following code print, and why? 
+Don't run the code until you have tried to answer.
+
 Syntax:
+a = "Xyzzy"
+
+def my_value(b)
+  b[2] = '-'
+end
+
+my_value(a)
+puts a
+
 =end
