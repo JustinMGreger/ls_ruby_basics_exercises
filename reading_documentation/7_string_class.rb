97141e49dd2904c0d82a1ad4b9cf4b527ea048d0
Justin Greger <justingreger@gmail.com>
2023-02-13 19:58:55 -0500

    completed.

--- reading_documentation/7_string_class.rb
+++ reading_documentation/7_string_class.rb
@@ -1,8 +1,14 @@
 =begin
-Description of exercise:
+Description of Exercise:
 Let's move on now to the documentation you will use most often; the core API section on Classes and Modules. 
 All of these are listed under Classes on the Core API page.
 Don't worry too much about the term class. For now, think of classes as data types: 
 numbers, character strings, arrays, hashes, etc.
 Locate and open the class documentation for the String class.
+
+Description of Solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "String".
+The first result should take you to the String Class.
 =end
