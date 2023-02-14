0ca2cca3ec1ef400bfca813ead6eabe5a51c9afd
Justin Greger <justingreger@gmail.com>
2023-02-14 13:00:00 -0500

    definitions.

--- reading_documentation_2/3_optional_arguments.rb
+++ reading_documentation_2/3_optional_arguments.rb
@@ -13,4 +13,9 @@ Select the API for the version of Ruby you are using.
 Search for "String".
 The first result should take you to the String Class.
 Search the word "split" on the String Class page.
+The split method is used to divide a string into an array of smaller strings based on a delimiter.
+A delimiter is a specific character or series of characters.
+
+Syntax:
+string.split(delimeter, limit) => array
 =end
