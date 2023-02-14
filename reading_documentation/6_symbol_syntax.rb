eec54791e84ab9cf7017bd735699a9f206ff17d1
Justin Greger <justingreger@gmail.com>
2023-02-13 19:46:34 -0500

    completed.

--- reading_documentation/6_symbol_syntax.rb
+++ reading_documentation/6_symbol_syntax.rb
@@ -4,4 +4,13 @@ Using the ruby documentation, determine how you would write a Symbol that repres
 We aren't looking for a String; we want a Symbol, which is one of ruby's datatypes.
 
 Description of the solution:
+Go to https://docs.ruby-lang.org/en/ or https://ruby-doc.org/
+Select the API for the version of Ruby you are using.
+Search for "literals".
+Once in the "literals" section search for "symbol".
+
+Answer:
+The documentation says " You may reference a symbol using a colon: :my_symbol"
+To write a symbol that represents your name you would write:
+:Name
 =end
