00caf807013b2884b06e3328f85cf47c92e17e42
Justin Greger <justingreger@gmail.com>
2023-02-14 20:27:02 -0500

    Syntax.

--- variable_scope/8_what_is_my_value_part_8.rb
+++ variable_scope/8_what_is_my_value_part_8.rb
@@ -2,4 +2,14 @@
 Description of Exercise:
 What will the following code print, and why? 
 Don't run the code until you have tried to answer.
+
+Syntax:
+array = [1, 2, 3]
+
+array.each do |element|
+  a = element
+end
+
+puts a
+
 =end
