11cbf2f6c69c7216881b820b532672728fc9acfd
Justin Greger <justingreger@gmail.com>
2023-02-14 21:47:21 -0500

    Description of Exercise.

---
+++ loops_1/2_loopception.rb
@@ -0,0 +1,7 @@
+=begin
+Description of Exercise:
+The code below is an example of a nested loop. 
+Both loops currently loop infinitely. 
+Modify the code so each loop stops after the first iteration.
+
+=end
