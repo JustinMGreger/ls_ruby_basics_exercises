37d6651c5c6c929e0734be4feb09f936686a6a96
Justin Greger <justingreger@gmail.com>
2023-02-14 21:11:51 -0500

    shadowing.

--- variable_scope/9_what_is_my_value_part_9.rb
+++ variable_scope/9_what_is_my_value_part_9.rb
@@ -24,5 +24,22 @@ array = [1, 2, 3]
 Explanation:
 Local variable "array" is assigned [1, 2, 3].
 
+Syntax:
+array.each do |a|
+  a += 1
+end
+Explanation:
+array.each method is invoked on local variable array. 
+As each element of the array is passed to the block it is being assigned to the block local variable element.
+Local variable "a" is being assigned to element.
+This is all happening in a block with an inner scope.
+The local variable "a" is local to this block.
+That local variable "a" is unable to be accesed in outer scopes.
+a += 1 is the same as a = a + 1.
+Ruby can only see the local variable "a" within the block.
+When Ruby looks for "a" it finds it inside of the block and stops looking.
+Varaible shadowing is happening because "a" is used more than once in this block as stated above.
+If the variables were named something other than "a" then Ruby could find the value.
+
 
 =end
