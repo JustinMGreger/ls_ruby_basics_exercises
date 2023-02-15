c787f1f4bfd792474a08c9e728151a5301585455
Justin Greger <justingreger@gmail.com>
2023-02-15 17:17:45 -0500

    Syntax.

--- loops_1/4_loop_on_command.rb
+++ loops_1/4_loop_on_command.rb
@@ -1,4 +1,10 @@
 =begin
 Description of Exercise:
 Modify the code below so the loop stops iterating when the user inputs 'yes'.
+
+Syntax:
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+end
 =end
