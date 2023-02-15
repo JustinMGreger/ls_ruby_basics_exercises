fb6ae77499b08ee8b385df7a8ba00f8f82e4f097
Justin Greger <justingreger@gmail.com>
2023-02-14 20:35:31 -0500

    syntax array each.

--- variable_scope/8_what_is_my_value_part_8.rb
+++ variable_scope/8_what_is_my_value_part_8.rb
@@ -18,5 +18,16 @@ array = [1, 2, 3]
 Explanation: 
 Local variable "array" is assigned [1, 2, 3].
 
+Syntax:
+array.each do |element|
+  a = element
+end
+Explanation:
+array.each method is invoked on local variable array. 
+As each element of the array is passed to the block it is being assigned to the block local variable element.
+Local variable "a" is being assigned to element.
+This is all happening in a block with an inner scope.
+The local variable "a" is local to this block.
+That local variable "a" is unable to be accesed in outer scopes.
 
 =end
