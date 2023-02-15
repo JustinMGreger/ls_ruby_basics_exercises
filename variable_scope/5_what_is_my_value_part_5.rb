6964022d60d442bc995f910a019d2f7fe6404f0d
Justin Greger <justingreger@gmail.com>
2023-02-14 19:03:00 -0500

    my_value.

--- variable_scope/5_what_is_my_value_part_5.rb
+++ variable_scope/5_what_is_my_value_part_5.rb
@@ -20,9 +20,19 @@ Explanation:
 Local variable "a" is assigned to "Xyzzy".
 
 Syntax:
+def my_value(b)
+  b = 'yzzyX'
+end
+
 Explanation:
-
-
+Local my_value is a method.
+def my_value(b)
+my_value method accepts argument "b".
+b = 'yzzyX'
+Argument "b" is assigned to 'yzzyX'.
+Meaning that new "b" is created at this point.
+Which can be thought of as new_b =  'yzzyX'.
+The return value of my_value regardless of argument "b" will be 'yzzyX'.
 
 
 =end
