27d66bd1c04d9731f7ab1cbf5e1fd90a1840eaff
Justin Greger <justingreger@gmail.com>
2023-02-15 16:47:29 -0500

    completed.

--- loops_1/3_control_the_loop.rb
+++ loops_1/3_control_the_loop.rb
@@ -23,4 +23,26 @@ The loop runs with an iteration of 1.
 After the loop puts "Number of iterations = #{iterations}" it breaks.
 The break reserved word exits the loop.
 
+Answer:
+Syntax:
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+  break if iterations > 5
+end
+
+Explanation:
+The number of intial iterations remains unchanged.
+Within the loop the following syntax was added:
+  iterations += 1
+  break if iterations > 5
+iterations += 1 is short hand for addition reassignment of iterations = iterations + 1.
+That occurs every time the loop is run.
+The syntax: break if iterations > 5 means that the break will exit the loop once iterations are greater than 5.
+When the loop is run iterations will under go addition reassignment increasing by 1. 
+This will occur until iterations have been increased to 6. 
+Then the loop will break before getting a change to interact with puts again.
+So the last output of puts will be "Number of iterations = 5".
 =end
