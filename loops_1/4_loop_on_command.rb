1283ab99592cf0a741112db8aba9c5b591d22f12
Justin Greger <justingreger@gmail.com>
2023-02-15 17:55:00 -0500

    answer and syntax.

--- loops_1/4_loop_on_command.rb
+++ loops_1/4_loop_on_command.rb
@@ -14,6 +14,7 @@ loop do
   puts 'Should I stop looping?'
   answer = gets.chomp
 end
+
 Explanation:
 This is a loop. 
 The loop executes puts 'Should I stop looping?'
@@ -21,4 +22,25 @@ Then the loop will recieve input from the user with the gets.chomp command.
 Then the loop will save the input to the variable "answer".
 There isn't a break conition in this loop.
 This loop is an infinite loop.
+
+Answer:
+Syntax:
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+  puts 'Incorrect answer. Please answer "yes".'
+end
+
+Explanation:
+The syntax starts the same as the original syntax:
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+Then the syntax is modified with a break if statement:
+break if answer == 'yes'
+
+
+
+
 =end
