569650c81e97c9a3fb159ad39c5489b670a5544a
Justin Greger <justingreger@gmail.com>
2023-02-14 19:23:20 -0500

    Syntax.

--- variable_scope/6_what_is_my_value_part_6.rb
+++ variable_scope/6_what_is_my_value_part_6.rb
@@ -2,4 +2,15 @@
 Description of Exercise:
 What will the following code print, and why? 
 Don't run the code until you have tried to answer.
+
+Syntax:
+a = 7
+
+def my_value(b)
+  b = a + a
+end
+
+my_value(a)
+puts a
+
 =end
