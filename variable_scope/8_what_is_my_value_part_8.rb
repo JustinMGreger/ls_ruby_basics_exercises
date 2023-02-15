60e045a44816475510e4de8d75a9f83a2cadfce3
Justin Greger <justingreger@gmail.com>
2023-02-14 20:38:08 -0500

    Syntax puts a.

--- variable_scope/8_what_is_my_value_part_8.rb
+++ variable_scope/8_what_is_my_value_part_8.rb
@@ -30,4 +30,13 @@ This is all happening in a block with an inner scope.
 The local variable "a" is local to this block.
 That local variable "a" is unable to be accesed in outer scopes.
 
+Syntax:
+puts a
+Explanation:
+The only "a" in this code is within the block.
+As previously described that "a" is not accessible for the puts a.
+This means that the "a" in puts a is an undefined local variable. 
+That means that an error occurs when puts a is ran.
+
+
 =end
