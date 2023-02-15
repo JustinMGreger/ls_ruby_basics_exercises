ea9c38c6e8cdb181248db55c5bb3bd21ba10a97e
Justin Greger <justingreger@gmail.com>
2023-02-15 16:12:19 -0500

    Syntax.

--- loops_1/3_control_the_loop.rb
+++ loops_1/3_control_the_loop.rb
@@ -1,4 +1,12 @@
 =begin
 Description of Exercise:
 Modify the following loop so it iterates 5 times instead of just once.
+
+Syntax:
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  break
+end
 =end
