aa093ea02de9e500094080b9cc10eeecb8cfa9e7
Justin Greger <justingreger@gmail.com>
2023-02-14 21:44:01 -0500

    completed.

--- loops_1/1_runaway_loop.rb
+++ loops_1/1_runaway_loop.rb
@@ -19,5 +19,12 @@ Explanation:
 This syntax means that a loop is running "puts 'Just keep printing...'".
 There is nothing in this syntax to stop the loop form running.
 
-
+Answer:
+Stopping a loop can be accomplished by adding the reserved word "break" inside of the loop.
+When loop executes "break" it stops iterating and immediately exits the block.
+The answer syntax would be:
+loop do
+  puts 'Just keep printing...'
+  break
+end 
 =end
