63dcc4b3567f1d6b363ebb61b117f110adecebef
Justin Greger <justingreger@gmail.com>
2023-02-14 19:33:16 -0500

    Description of Solution.

--- variable_scope/6_what_is_my_value_part_6.rb
+++ variable_scope/6_what_is_my_value_part_6.rb
@@ -18,4 +18,18 @@ Syntax:
 a = 7
 Explanation:
 Local variable "a" is assigned 7.
+
+Syntax:
+def my_value(b)
+  b = a + a
+end
+Explanation:
+my_value is a local method that accepts argument "b".
+Methods in Ruby are entirely self contained they have their own scope.
+The only input for this method is "b".
+Argument "b" is then assigned a + a.
+This is a problem because the "a"s in this method have no input.
+The "a"s in this method are not able to access local variable "a" outside of the method.
+This means that within the my_value method the "a"s are undefined variables.
+This raises an error message.
 =end
