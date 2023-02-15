d5954507691de4aa9af190d287a359c87b370f49
Justin Greger <justingreger@gmail.com>
2023-02-15 17:52:16 -0500

    Syntax explanation.

--- loops_1/4_loop_on_command.rb
+++ loops_1/4_loop_on_command.rb
@@ -10,5 +10,15 @@ end
 
 Desciption of Solution:
 Syntax:
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+end
 Explanation:
+This is a loop. 
+The loop executes puts 'Should I stop looping?'
+Then the loop will recieve input from the user with the gets.chomp command. 
+Then the loop will save the input to the variable "answer".
+There isn't a break conition in this loop.
+This loop is an infinite loop.
 =end
