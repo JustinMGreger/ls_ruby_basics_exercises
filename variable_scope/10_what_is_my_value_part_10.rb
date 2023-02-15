c45d303848ceedf584084f78f2221ffb28a9175b
Justin Greger <justingreger@gmail.com>
2023-02-14 21:26:46 -0500

    method block error.

--- variable_scope/10_what_is_my_value_part_10.rb
+++ variable_scope/10_what_is_my_value_part_10.rb
@@ -26,4 +26,19 @@ Syntax:
 array = [1, 2, 3]
 Explanation:
 Local variable "array" is assigned [1, 2, 3].
+
+Syntax:
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+Explanation:
+The array.each block is inside of the my_value method.
+Methods are entirely self-contained.
+The only way to access the intialized local variable "a" to the method is as an argument.
+This does not happen and an error occurs as "a" is not defined.
+
+Answer:
+
 =end
