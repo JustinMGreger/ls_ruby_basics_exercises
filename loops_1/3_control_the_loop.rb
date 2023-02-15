f2d7df81dc5c3802d7fa6a903fa685f265cf1999
Justin Greger <justingreger@gmail.com>
2023-02-15 16:31:05 -0500

    Description of Solution.

--- loops_1/3_control_the_loop.rb
+++ loops_1/3_control_the_loop.rb
@@ -9,4 +9,18 @@ loop do
   puts "Number of iterations = #{iterations}"
   break
 end
+
+Description of Solution:
+Syntax:
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  break
+end
+Explanation:
+The loop runs with an iteration of 1.
+After the loop puts "Number of iterations = #{iterations}" it breaks.
+The break reserved word exits the loop.
+
 =end
