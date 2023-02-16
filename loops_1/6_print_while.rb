0ead9504b9980762ac6a160f6393ee3d188068ba
Justin Greger <justingreger@gmail.com>
2023-02-15 20:22:41 -0500

    Syntax.

--- loops_1/6_print_while.rb
+++ loops_1/6_print_while.rb
@@ -2,4 +2,11 @@
 Description of Exercise:
 Using a while loop, print 5 random numbers between 0 and 99 (inclusive). 
 The code below shows an example of how to begin solving this exercise.
+
+Syntax:
+numbers = []
+
+while <condition>
+  # ...
+end
 =end
