14405f4bd42800fb823ee6dd29e51c63847f9ca3
Justin Greger <justingreger@gmail.com>
2023-02-15 19:24:06 -0500

    Syntax.

--- loops_1/5_say_hello.rb
+++ loops_1/5_say_hello.rb
@@ -1,4 +1,12 @@
 =begin
 Description of Exercise:
 Modify the code below so "Hello!" is printed 5 times.
+
+Syntax:
+say_hello = true
+
+while say_hello
+  puts 'Hello!'
+  say_hello = false
+end
 =end
