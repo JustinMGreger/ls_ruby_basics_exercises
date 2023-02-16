559194d0f59c9aefc6121547441154a273cb3a3b
Justin Greger <justingreger@gmail.com>
2023-02-15 20:21:50 -0500

    Description of Exercise.

---
+++ loops_1/6_print_while.rb
@@ -0,0 +1,5 @@
+=begin
+Description of Exercise:
+Using a while loop, print 5 random numbers between 0 and 99 (inclusive). 
+The code below shows an example of how to begin solving this exercise.
+=end
