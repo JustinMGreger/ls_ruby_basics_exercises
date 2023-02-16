1671e2f360bee889fe1eb80de89d5ef8995f22a0
Justin Greger <justingreger@gmail.com>
2023-02-15 19:45:23 -0500

    Syntax.

--- loops_1/5_say_hello.rb
+++ loops_1/5_say_hello.rb
@@ -28,4 +28,10 @@ while loop has condition provided to it say_hello.
 while say_hello = true the loop will run.
 When the loop is run the following syntax will be executed:
   puts 'Hello!'
+Then the loop encounters syntax:
+  say_hello = false
+This assigns say_hello to fasle from the original true value.
+This causes the while loop to end when it is called again at the begining of the loop.
+This results in  puts 'Hello!' being executed once.
+The printed result is "Hello!".
 =end
