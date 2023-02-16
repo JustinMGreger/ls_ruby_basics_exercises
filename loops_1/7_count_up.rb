dcb4d653781e010821c6a0567f9b2e594f400e68
Justin Greger <justingreger@gmail.com>
2023-02-15 21:16:12 -0500

    Syntax.

--- loops_1/7_count_up.rb
+++ loops_1/7_count_up.rb
@@ -2,4 +2,12 @@
 Description of Exercise:
 The following code outputs a countdown from 10 to 1. 
 Modify the code so that it counts from 1 to 10 instead.
+
+Syntax:
+count = 10
+
+until count == 0
+  puts count
+  count -= 1
+end
 =end
