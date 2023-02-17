935d0dfee40af727b55006ae4c463cb87a1158f3
Justin Greger <justingreger@gmail.com>
2023-02-16 22:00:28 -0500

    Expected output.

--- loops_1/10_greet_your_friends.rb
+++ loops_1/10_greet_your_friends.rb
@@ -5,4 +5,10 @@ Given the following array of names, use a for loop to greet each friend individu
 
 Syntax:
 friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+Expected output:
+Hello, Sarah!
+Hello, John!
+Hello, Hannah!
+Hello, Dave!
 =end
