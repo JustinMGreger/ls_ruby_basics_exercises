209ae332387c32a6c5a70a0b7c2735930b3b5fe4
Justin Greger <justingreger@gmail.com>
2023-02-16 20:40:44 -0500

    Syntax.

--- loops_1/9_thats_odd.rb
+++ loops_1/9_thats_odd.rb
@@ -2,4 +2,9 @@
 Description of Exercise:
 The code below shows an example of a for loop. 
 Modify the code so that it only outputs i if i is an odd number.
+
+Syntax:
+for i in 1..100
+  puts i
+end
 =end
