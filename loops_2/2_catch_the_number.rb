2011deb68bce4948d7c16a240934c9f5b499b6d7
Justin Greger <justingreger@gmail.com>
2023-02-17 21:40:45 -0500

    Syntax of Initial Code.

--- loops_2/2_catch_the_number.rb
+++ loops_2/2_catch_the_number.rb
@@ -1,4 +1,10 @@
 =begin
 Description of Exercise:
 Modify the following code so that the loop stops if number is equal to or between 0 and 10.
+
+Syntax of Initial Code:
+loop do
+  number = rand(100)
+  puts number
+end
 =end
