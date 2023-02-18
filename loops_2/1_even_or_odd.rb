57c810d4656a8d52d9c371c89a968621d3e17f3d
Justin Greger <justingreger@gmail.com>
2023-02-17 20:03:52 -0500

    Syntax of Initial Code.

--- loops_2/1_even_or_odd.rb
+++ loops_2/1_even_or_odd.rb
@@ -3,5 +3,11 @@ Description of Exercise:
 Write a loop that prints numbers 1-5 and whether the number is even or odd. 
 Use the code below to get started.
 
+Syntax of Initial Code:
+count = 1
+
+loop do
+  count += 1
+end
 
 =end
