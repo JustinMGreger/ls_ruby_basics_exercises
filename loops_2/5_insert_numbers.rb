1872d8cb05445006d922af34d7d19c87c5909fee
Justin Greger <justingreger@gmail.com>
2023-02-19 17:55:48 -0500

    Initial Syntax Given.

--- loops_2/5_insert_numbers.rb
+++ loops_2/5_insert_numbers.rb
@@ -3,4 +3,16 @@ Description of Exercise:
 Modify the code below so that the user's input gets added to the numbers array. 
 Stop the loop when the array contains 5 numbers.
 
+Initial Syntax Given:
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+end
+puts numbers
+
+Syntax of Solution:
+Explanation of Solution Syntax:
+Summary of Solution Syntax:
 =end
