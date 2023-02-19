786330c9c96a1023f4d6c966a46d5eb7a6e8b115
Justin Greger <justingreger@gmail.com>
2023-02-18 19:30:03 -0500

    syntax.

--- loops_2/2_catch_the_number.rb
+++ loops_2/2_catch_the_number.rb
@@ -52,5 +52,24 @@ loop do
 end
 
 Explanation of Solution Syntax:
+Syntax:
+loop do
+
+Explanation:
+loop do means run loop which means repeatedly execute a block of code until a certain condition is met.
+
+Syntax:
+ number = rand(100)
+
+Explanation:
+Local variable "number" is assigned the value of rand(100).
+rand(100) is a method that generates a random number between 0 and 99 excluding the number 100.
+That is rand generates a random number of 1 less than the largest number in the range given.
+
+Syntax:
+  puts number
+
+Explanation:
+print number to the screen.
 
 =end
