52b4c08c9d7058c77a0d97c558f54b4311049d12
Justin Greger <justingreger@gmail.com>
2023-02-18 21:24:23 -0500

    Description of Exercise.

---
+++ loops_2/4_get_the_sum.rb
@@ -0,0 +1,6 @@
+=begin
+Description of Exercise:
+The code below asks the user "What does 2 + 2 equal?" and uses #gets to retrieve the user's answer. 
+Modify the code so "That's correct!" is printed and the loop stops when the user's answer equals 4. 
+Print "Wrong answer. Try again!" if the user's answer doesn't equal 4.
+=end
