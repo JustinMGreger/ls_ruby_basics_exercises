3bcdcc0623f5224a94ac58dad323d568b7ae8423
Justin Greger <justingreger@gmail.com>
2023-02-18 21:38:11 -0500

    Syntax of Solution.

--- loops_2/4_get_the_sum.rb
+++ loops_2/4_get_the_sum.rb
@@ -10,4 +10,19 @@ loop do
   answer = gets.chomp.to_i
 end
 
+Syntax of Solution:
+
+Syntax:
+loop do
+  puts 'What does 2 + 2 equal?'
+  answer = gets.chomp.to_i
+
+  if answer == 4
+    puts "That's correct!"
+    break
+  end
+
+  puts 'Wrong answer. Try again!'
+end
+
 =end
