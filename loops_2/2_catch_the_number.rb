5ba945086162962db526abddc9ec9fdd12f4eece
Justin Greger <justingreger@gmail.com>
2023-02-18 19:56:21 -0500

    Summary Syntax.

--- loops_2/2_catch_the_number.rb
+++ loops_2/2_catch_the_number.rb
@@ -93,6 +93,26 @@ Explanation:
 break means if a condition is true exit the loop.
 
 Summary of Solution Syntax:
+loop do means run loop which means repeatedly execute a block of code until a certain condition is met.
+Local variable "number" is assigned the value of rand(100).
+rand(100) is a method that generates a random number between 0 and 99 excluding the number 100.
+That is rand generates a random number of 1 less than the largest number in the range given.
+print number to the screen.
+
+What this means is run a loop that prints a randomly generated number between 0 and 99 to the screen.
+
+if statements are used to execute a certain block of code if a condition is true.
+If the conidition is false then that block of code is skipped.
+The if statement's condition is number.between?(0, 10) is true.
+number references the syntax "number = rand(100)".
+number.between?(0, 10) checks if number is equal or between 0 & 10.
+If number is equal or between 0 & 10 then the between method will return true.
+If number is not equal or between 0 & 10 then the between method will return false.
+break means if a condition is true exit the loop.
+
+
+
+
 
 
 =end
