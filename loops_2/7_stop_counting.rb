696188e14d083cfff43a5223741cebb250b8f469
Justin Greger <justingreger@gmail.com>
2023-02-19 20:51:57 -0500

    Initial Syntax Given.

--- loops_2/7_stop_counting.rb
+++ loops_2/7_stop_counting.rb
@@ -4,7 +4,14 @@ The method below counts from 0 to 4.
 Modify the block so that it prints the current number and stops iterating when the current number equals 2.
 
 Initial Syntax Given:
+5.times do |index|
+  puts index
+  # ...
+end
+
 Explanation of Syntax:
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
