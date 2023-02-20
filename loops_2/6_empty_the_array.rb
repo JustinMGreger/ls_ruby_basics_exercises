5ce4945dde5805b6e65bd9d6117b45bec3a63e3b
Justin Greger <justingreger@gmail.com>
2023-02-19 20:13:14 -0500

    outline.

--- loops_2/6_empty_the_array.rb
+++ loops_2/6_empty_the_array.rb
@@ -2,4 +2,14 @@
 Description of Exercise:
 Given the array below, use loop to remove and print each name from first to last. 
 Stop the loop once names doesn't contain any more elements.
+
+Initial Syntax Given:
+
+Syntax of Solution:
+
+Explanation of Solution Syntax:
+
+Summary of Solution Syntax:
+
+
 =end
