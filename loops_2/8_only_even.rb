b3a81fe2f379fec096a54e0a97fea2a4acfa7c2d
Justin Greger <justingreger@gmail.com>
2023-02-20 17:21:17 -0500

    number += 1.

--- loops_2/8_only_even.rb
+++ loops_2/8_only_even.rb
@@ -23,8 +23,14 @@ until number == 10
 
 Explanation:
 run a loop until number == 10 is true.
+That is run a loop until local variable number is equal to 10.
 
+Syntax:
+  number += 1
 
+Explanation:
+number = number + 1
+That is local variable "number" is reassigned to "number" + 1.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
