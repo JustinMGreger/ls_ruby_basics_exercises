094d931f989357bbff9dc8c6706af4b7fc4c937a
Justin Greger <justingreger@gmail.com>
2023-02-20 17:27:27 -0500

    Explanation of Solution.

--- loops_2/8_only_even.rb
+++ loops_2/8_only_even.rb
@@ -49,5 +49,46 @@ until number == 10
 end
 
 Explanation of Solution Syntax:
+Syntax:
+number = 0
+
+Explanation:
+local variable "number" is assigned integer 0.
+
+Syntax:
+until number == 10
+
+Explanation:
+run a loop until number == 10 is true.
+That is run a loop until local variable number is equal to 10.
+
+Syntax:
+  number += 1
+
+Explanation:
+number = number + 1
+That is local variable "number" is reassigned to "number" + 1.
+
+Syntax:
+next if number.odd?
+
+Explanation:
+
+Syntax:
+  puts number
+end
+
+Explanation:
+print number to the screen.
+
 Summary of Solution Syntax:
+local variable "number" is assigned integer 0.
+run a loop until number == 10 is true.
+That is run a loop until local variable number is equal to 10.
+number = number + 1
+That is local variable "number" is reassigned to "number" + 1.
+
+next syntax.
+
+print number to the screen.
 =end
