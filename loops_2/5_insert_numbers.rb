0ec96c082a949cbae8e73ef6cbfef8d73f34a9df
Justin Greger <justingreger@gmail.com>
2023-02-19 19:34:13 -0500

    Syntax of Solution.

--- loops_2/5_insert_numbers.rb
+++ loops_2/5_insert_numbers.rb
@@ -48,6 +48,34 @@ Explanation:
 print to the screen numbers array.
 
 Syntax of Solution:
+Syntax:
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+
+  numbers.push(input)
+  break if numbers.size == 5
+end
+puts numbers
+
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
+local variable numbers is assigned the value of an empty array.
+run a loop.
+print to screen 'Enter any number:'
+local varibale "input" is assigned to gets.chomp.to_i.
+gets means get s which is get string. 
+That means get a string from the user that is get the string form of user input.
+.chomp removes the extra line created by gets.
+.to_i transforms the string to an integer.
+In summary local variable "input" is assinged the string form of user input that is transformed to an integer on 1 line.
+
+Summary of this syntax.
+numbers.push(input)
+  break if numbers.size == 5
+end
+
+print to the screen numbers array.
 =end
