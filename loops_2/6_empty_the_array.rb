289f27d41e7d7314f6b52f04363a5b1bd659e114
Justin Greger <justingreger@gmail.com>
2023-02-19 20:17:16 -0500

    Initial Syntax Given.

--- loops_2/6_empty_the_array.rb
+++ loops_2/6_empty_the_array.rb
@@ -2,8 +2,13 @@
 Description of Exercise:
 Given the array below, use loop to remove and print each name from first to last. 
 Stop the loop once names doesn't contain any more elements.
+Keep in mind to only use loop, not while, until, etc.
 
 Initial Syntax Given:
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+Explanation of Syntax:
+local variable names is assigned array of elements ['Sally', 'Joe', 'Lisa', 'Henry'].
 
 Syntax of Solution:
 
