a5bf569cf3329c89422300ed72bffd0f50c09345
Justin Greger <justingreger@gmail.com>
2023-02-19 20:50:51 -0500

    Description of Exercise.

--- loops_2/7_stop_counting.rb
+++ loops_2/7_stop_counting.rb
@@ -1,5 +1,7 @@
 =begin
 Description of Exercise:
+The method below counts from 0 to 4. 
+Modify the block so that it prints the current number and stops iterating when the current number equals 2.
 
 Initial Syntax Given:
 Explanation of Syntax:
