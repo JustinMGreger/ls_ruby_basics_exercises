db70276877d0a8cd2d7916cb0e60ea85a4915828
Justin Greger <justingreger@gmail.com>
2023-02-20 21:25:02 -0500

    Initial Syntax Given.

--- user_input/2_your_age_in_months.rb
+++ user_input/2_your_age_in_months.rb
@@ -1,8 +1,14 @@
 =begin
 Description of Exercise:
 Write a program that asks the user for their age in years, and then converts that age to months.
+Examples:
 
 Initial Syntax Given:
+$ ruby age.rb
+>> What is your age in years?
+35
+You are 420 months old.
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
