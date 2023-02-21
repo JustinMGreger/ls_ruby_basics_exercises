9522b9a39de4c7a0d7473b8baf231aa10eb3ddec
Justin Greger <justingreger@gmail.com>
2023-02-20 20:35:06 -0500

    number of greetings.

--- loops_2/10_greeting.rb
+++ loops_2/10_greeting.rb
@@ -44,5 +44,32 @@ while number_of_greetings > 0
 end
 
 Explanation of Solution Syntax:
+
+Syntax:
+def greeting
+
+Explanation:
+Def means define a method.
+this means define greeting method.
+
+Syntax:
+  puts 'Hello!'
+end
+
+Explanation:
+print to the screen 'Hello!'
+
+Syntax:
+number_of_greetings = 2
+
+Explanation:
+local variable number_of_greetings is assigned the value integer 2.
+
+
+
 Summary of Solution Syntax:
+Def means define a method.
+this means define greeting method.
+print to the screen 'Hello!'
+local variable number_of_greetings is assigned the value integer 2.
 =end
