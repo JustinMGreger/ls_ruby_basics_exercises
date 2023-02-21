dbe5a66a075aa1f37a62e10e393480046453f193
Justin Greger <justingreger@gmail.com>
2023-02-20 21:55:46 -0500

    Description of Exercise.

--- user_input/3_print_something_part_1.rb
+++ user_input/3_print_something_part_1.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
+Write a program that asks the user whether they want the program to print "something", 
+then print it if the user enters y. Otherwise, print nothing.
 
+Examples:
 Initial Syntax Given:
 Explanation of Syntax:
 Syntax of Solution:
