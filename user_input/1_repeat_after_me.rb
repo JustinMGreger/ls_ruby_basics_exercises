ee5d1f7eeab86322dc3330675b7db23c13dee132
Justin Greger <justingreger@gmail.com>
2023-02-20 20:58:38 -0500

    Description of Exercise.

--- user_input/1_repeat_after_me.rb
+++ user_input/1_repeat_after_me.rb
@@ -1,5 +1,9 @@
 =begin
 Description of Exercise:
+Write a program that asks the user to type something in, 
+after which your program should simply display what was entered.
+
+Example:
 
 Initial Syntax Given:
 Explanation of Syntax:
