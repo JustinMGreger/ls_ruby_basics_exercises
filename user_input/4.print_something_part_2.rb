8716d48767ec5275f802c21206620c2ee773d36d
Justin Greger <justingreger@gmail.com>
2023-02-21 21:11:26 -0500

    Syntax of Solution.

--- user_input/4.print_something_part_2.rb
+++ user_input/4.print_something_part_2.rb
@@ -208,8 +208,36 @@ Explanation:
 This is a string.
 This is the user input.
 
-
 Syntax of Solution:
+
+choice = nil
+loop do
+  puts '>> Do you want me to print something? (y/n)'
+  choice = gets.chomp.downcase
+  break if %w(y n).include?(choice)
+  puts '>> Invalid input! Please enter y or n'
+end
+puts 'something' if choice == 'y'
+
 Explanation of Solution Syntax:
+
+Syntax:
+choice = nil
+Explanation:
+
+
+
+
+
+
+
+loop do
+  puts '>> Do you want me to print something? (y/n)'
+  choice = gets.chomp.downcase
+  break if %w(y n).include?(choice)
+  puts '>> Invalid input! Please enter y or n'
+end
+puts 'something' if choice == 'y'
+
 Summary of Solution Syntax:
 =end
