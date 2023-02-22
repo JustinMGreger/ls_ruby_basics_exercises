2081cc5da59aa9daf4cc1d1812b6c588098dfb0d
Justin Greger <justingreger@gmail.com>
2023-02-21 21:05:57 -0500

    NO

--- user_input/4.print_something_part_2.rb
+++ user_input/4.print_something_part_2.rb
@@ -175,9 +175,12 @@ puts ">> Do you want me to print something? (y/n)"
 >> is an input prompt.
 (y/n) is the preferred input.
 
+Syntax:
+NO
 
-
-
+Explanation:
+This is a string.
+This is the user input.
 
 
 
