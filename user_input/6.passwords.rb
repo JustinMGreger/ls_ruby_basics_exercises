139a0f01b62e04ed228a5ce1a3c034c11ea394bd
Justin Greger <justingreger@gmail.com>
2023-02-22 21:24:57 -0500

    Description of Exercise.

--- user_input/6.passwords.rb
+++ user_input/6.passwords.rb
@@ -1,5 +1,11 @@
 =begin
 Description of Exercise:
+Write a program that displays a welcome message, 
+but only after the user enters the correct password, 
+where the password is a string that is defined as a constant in your program. 
+Keep asking for the password until the user enters the correct password.
+
+Examples:
 
 Initial Syntax Given:
 Explanation of Syntax:
