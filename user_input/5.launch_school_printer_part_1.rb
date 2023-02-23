e432ccc2b6120e5dfaf5bf3fd0d8b8d2f8db55d9
Justin Greger <justingreger@gmail.com>
2023-02-22 21:19:45 -0500

    completed.

--- user_input/5.launch_school_printer_part_1.rb
+++ user_input/5.launch_school_printer_part_1.rb
@@ -279,36 +279,18 @@ This is the last line of the loop that is any code following this is outside of 
 What this means:
 
 Local variable "number_of_lines" is assigned the value of nil.
-start running a loop.
-The following code is in the loop.
-puts means print to the screen the string.
-the string is '>> How many output lines do you want? Enter a number >= 3:'
->> is an input prompt.
-(>= 3) is the preferred input.
-: is where the user input goes.
+
+start running a loop the following code is in the loop:
+print to the screen the string '>> How many output lines do you want? Enter a number >= 3:'
 This is asking the user how many output lines do they want and to enter a number greater than or equal to 3.
-variable "number_of_lines" is reassigned to gets.to_i
-gets means get string that is get user input in string form.
-.to_i means transform to integer form.
-This means variable "number_of_lines" is reassigned to user input in string form that has been transformed to integer form.
-break out of the loop.
-the if statement means break out of the loop if the following condition is true.
-variable "number_of_lines" >= 3.
->= 3 means greater than or equal to 3.
-This means break out of the loop if variable "number_of_lines" is greater than or equal to 3.
-puts means put s which is put string which means print to the screen the string.
-">> That's not enough lines." is a string.
-This is a response to user input being invalid that is not a high enough integer value.
+variable "number_of_lines" is reassigned to user input in string form that has been transformed to integer form.
+break out of the loop if variable "number_of_lines" is greater than or equal to 3.
+In response to "number_of_lines" not being greater than or equal to 3 print to the screen the string ">> That's not enough lines."
 This is the last line of the loop that is any code following this is outside of the loop.
 
-start a while loop.
-The following code is in a while loop.
-while loops run as long as a condition is true.
-variable "number_of_lines" is compared to 0 to see if "number_of_lines" is greater than 0.
-This means run the while loop if "number_of_lines" is greater than 0.
-puts means put s which is put string which means print to the screen the string.
-'Launch School is the best!' is the string.
-This means print to the screen 'Launch School is the best!'
+start a while loop & the following code is in a while loop:
+run the while loop if "number_of_lines" is greater than 0.
+print to the screen 'Launch School is the best!'
 variable "number_of_lines" is reassigned to number_of_lines - 1.
-This is the last line of the loop that is any code following this is outside of the loop.
+This is the last line of the while loop that is any code following this is outside of the loop.
 =end
