a5a3d13b9069d66bb6a68a239c169aa82b04b8c9
Justin Greger <justingreger@gmail.com>
2023-02-23 21:11:04 -0500

    break if

--- user_input/7.user_name_and_password.rb
+++ user_input/7.user_name_and_password.rb
@@ -314,11 +314,19 @@ The variable "password_try" is assigned gets.chomp
 gets means get string that is get user input in string form.
 .chomp is a string method that removes new line character.
 
-
-
-
-
+Syntax:
   break if user_name == USERNAME && password_try == PASSWORD
+
+Explanation:
+break means break out of the loop. 
+break if the following condition is true.
+the following condition is if user_name == USERNAME && password_try == PASSWORD.
+variable "user_name" == USERNAME which is  "user_name" equals USERNAME.
+&& is logical and which only evaluates to true if both operands are true.
+variable "password_try" == PASSWORD which is "password_try" equals PASSWORD.
+This means break out of the loop if variable "user_name" equals constant USERNAME and variable "password_try" equals constant PASSWORD.
+
+
   puts '>> Authorization failed!'
 end
 
