a447ae2d3f9cda8b27a45ea9ffd63219c1eb59cc
Justin Greger <justingreger@gmail.com>
2023-02-23 19:55:23 -0500

    explanation.

--- user_input/7.user_name_and_password.rb
+++ user_input/7.user_name_and_password.rb
@@ -42,29 +42,197 @@ ruby means run ruby file.
 login.rb is a ruby file.
 This means run the ruby file login.rb
 
+Syntax:
 >> Please enter user name:
+
+Explanation:
+puts ">> Please enter user name:"
+print to the screen the string.
+This is a string.
+>> is an input prompt.
+: is where the user input goes.
+This asks the user to input their user name.
+
+Syntax:
 John
+
+Explanation:
+puts "John".
+print to the screen the string.
+This is a string.
+This is the user input.
+This is the user name.
+
+Syntax:
 >> Please enter your password:
+
+Explanation:
+This is a string.
+puts ">> Please enter your password:"
+That is print to the screen the string ">> Please enter your password:"
+>> is an input prompt.
+: is where the user input goes.
+This string asks the user to input their password.
+
+Syntax:
 Hello
+
+Explanation:
+puts "Hello"
+That is print to the screen the string "Hello".
+This is the user input value of their password.
+
+Syntax:
 >> Authorization failed!
+
+Explanation:
+puts ">> Authorization failed!"
+That is print to the screen the string ">> Authorization failed!"
+This is the result of the user input values being incorrect.
+
+Syntax:
 >> Please enter user name:
+
+Explanation:
+puts ">> Please enter user name:"
+print to the screen the string.
+This is a string.
+>> is an input prompt.
+: is where the user input goes.
+This asks the user to input their user name.
+
+Syntax:
 mary
+
+Explanation:
+puts "mary".
+print to the screen the string.
+This is a string.
+This is the user input.
+This is the user name.
+
+Syntax:
 >> Please enter your password:
+
+Explanation:
+This is a string.
+puts ">> Please enter your password:"
+That is print to the screen the string ">> Please enter your password:"
+>> is an input prompt.
+: is where the user input goes.
+This string asks the user to input their password.
+
+Syntax:
 SecreT
+
+Explanation:
+puts "SecreT"
+That is print to the screen the string "SecreT".
+This is the user input value of their password.
+
+Syntax:
 >> Authorization failed!
+
+Explanation:
+puts ">> Authorization failed!"
+That is print to the screen the string ">> Authorization failed!"
+This is the result of the user input values being incorrect.
+
+Syntax:
 >> Please enter user name:
+
+Explanation:
+puts ">> Please enter user name:"
+print to the screen the string.
+This is a string.
+>> is an input prompt.
+: is where the user input goes.
+This asks the user to input their user name.
+
+Syntax:
 admin
+
+Explanation:
+puts "admin".
+print to the screen the string.
+This is a string.
+This is the user input.
+This is the user name.
+
+Syntax:
 >> Please enter your password:
+
+Explanation:
+This is a string.
+puts ">> Please enter your password:"
+That is print to the screen the string ">> Please enter your password:"
+>> is an input prompt.
+: is where the user input goes.
+This string asks the user to input their password.
+
+Syntax:
 root
+
+Explanation:
+puts "root"
+That is print to the screen the string "root".
+This is the user input value of their password.
+
+Syntax:
 >> Authorization failed!
+
+Explanation:
+puts ">> Authorization failed!"
+That is print to the screen the string ">> Authorization failed!"
+This is the result of user input values being incorrect.
+
+Syntax:
 >> Please enter user name:
+
+Explanation:
+puts ">> Please enter user name:"
+print to the screen the string.
+This is a string.
+>> is an input prompt.
+: is where the user input goes.
+This asks the user to input their user name.
+
+Syntax:
 admin
+
+Explanation:
+puts "admin".
+print to the screen the string.
+This is a string.
+This is the user input.
+This is the user name.
+
+Syntax:
 >> Please enter your password:
+
+Explanation:
+This is a string.
+puts ">> Please enter your password:"
+That is print to the screen the string ">> Please enter your password:"
+>> is an input prompt.
+: is where the user input goes.
+This string asks the user to input their password.
+
+Syntax:
 SecreT
+
+Explanation:
+puts "SecreT"
+That is print to the screen the string "SecreT".
+This is the user input value of their password.
+
+Syntax:
 Welcome!
 
-
-
+Explanation:
+puts "Welcome!".
+That is print to the screen the string "Welcome!".
+This is the result of user input being correct.
 
 
 
