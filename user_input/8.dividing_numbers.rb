54642c9d4fb82f65325116bf28516e7c1eb38fa5
Justin Greger <justingreger@gmail.com>
2023-02-23 21:40:55 -0500

    Description of Exercise.

--- user_input/8.dividing_numbers.rb
+++ user_input/8.dividing_numbers.rb
@@ -1,5 +1,11 @@
 =begin
 Description of Exercise:
+Write a program that asks the user to enter two integers, 
+then prints the results of dividing the first by the second. 
+The second number must not be 0. 
+Since this is user input, there's a good chance that the user won't enter a valid integer. 
+Therefore, you must validate the input to be sure it is an integer. 
+You can use the following code to determine whether the input is an integer:
 
 Initial Syntax Given:
 Explanation of Syntax:
