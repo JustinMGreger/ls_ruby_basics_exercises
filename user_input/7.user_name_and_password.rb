1bf6b55e8edf7351bb80e41010858ad3722a0a52
Justin Greger <justingreger@gmail.com>
2023-02-23 20:13:21 -0500

    Syntax of Solution.

--- user_input/7.user_name_and_password.rb
+++ user_input/7.user_name_and_password.rb
@@ -234,12 +234,55 @@ puts "Welcome!".
 That is print to the screen the string "Welcome!".
 This is the result of user input being correct.
 
-
-
-
-
-
 Syntax of Solution:
+
+USERNAME = 'admin'
+PASSWORD = 'SecreT'
+
+loop do
+  puts '>> Please enter your user name:'
+  user_name = gets.chomp
+
+  puts '>> Please enter your password:'
+  password_try = gets.chomp
+
+  break if user_name == USERNAME && password_try == PASSWORD
+  puts '>> Authorization failed!'
+end
+
+puts 'Welcome!'
+
 Explanation of Solution Syntax:
+USERNAME = 'admin'
+PASSWORD = 'SecreT'
+
+loop do
+  puts '>> Please enter your user name:'
+  user_name = gets.chomp
+
+  puts '>> Please enter your password:'
+  password_try = gets.chomp
+
+  break if user_name == USERNAME && password_try == PASSWORD
+  puts '>> Authorization failed!'
+end
+
+puts 'Welcome!'
+
 Summary of Solution Syntax:
+USERNAME = 'admin'
+PASSWORD = 'SecreT'
+
+loop do
+  puts '>> Please enter your user name:'
+  user_name = gets.chomp
+
+  puts '>> Please enter your password:'
+  password_try = gets.chomp
+
+  break if user_name == USERNAME && password_try == PASSWORD
+  puts '>> Authorization failed!'
+end
+
+puts 'Welcome!'
 =end
