c24625a3d84e655f7be8479bdd288a32e5277882
Justin Greger <justingreger@gmail.com>
2023-02-23 21:52:50 -0500

    def valid

--- user_input/8.dividing_numbers.rb
+++ user_input/8.dividing_numbers.rb
@@ -51,8 +51,57 @@ a
 5
 >> 10 / 5 is 2
 
-
 Explanation of Syntax:
+
+Syntax:
+def valid_number?(number_string)
+
+Explanation:
+def means define method.
+define the valid_number method.
+? means boolean return true or false.
+This method determines if a number is valid or not.
+(number_string) is the parameter it is the place holder for the argument that the method accepts.
+
+
+  number_string.to_i.to_s == number_string
+end
+
+
+Examples:
+
+$ ruby division.rb
+>> Please enter the numerator:
+8
+>> Please enter the denominator:
+2
+>> 8 / 2 is 4
+
+$ ruby division.rb
+>> Please enter the numerator:
+8.3
+>> Invalid input. Only integers are allowed.
+>> Please enter the numerator:
+9
+>> Please enter the denominator:
+4
+>> 9 / 4 is 2
+
+
+$ ruby division.rb
+>> Please enter the numerator:
+10
+>> Please enter the denominator:
+a
+>> Invalid input. Only integers are allowed.
+>> Please enter the denominator:
+0
+>> Invalid input. A denominator of 0 is not allowed.
+>> Please enter the denominator:
+5
+>> 10 / 5 is 2
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
