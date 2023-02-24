aaaadfe75792e64969a8076860cdd4dfc55d5349
Justin Greger <justingreger@gmail.com>
2023-02-23 19:18:45 -0500

    Description of Exercise.

--- user_input/7.user_name_and_password.rb
+++ user_input/7.user_name_and_password.rb
@@ -1,5 +1,12 @@
 =begin
 Description of Exercise:
+In the previous exercise, you wrote a program to solicit a password. 
+In this exercise, you should modify the program so it also requires a user name. 
+The program should solicit both the user name and the password, 
+then validate both, and issue a generic error message if one or both are incorrect; 
+the error message should not tell the user which item is incorrect.
+
+Examples:
 
 Initial Syntax Given:
 Explanation of Syntax:
