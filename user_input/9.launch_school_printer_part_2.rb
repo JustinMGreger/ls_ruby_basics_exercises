a6247642cc65dbb338c36a820ba2870922046d36
Justin Greger <justingreger@gmail.com>
2023-02-25 21:12:07 -0500

    Descripion of Exercise.

--- user_input/9.launch_school_printer_part_2.rb
+++ user_input/9.launch_school_printer_part_2.rb
@@ -1,5 +1,7 @@
 =begin
 Description of Exercise:
+In an earlier exercise, you wrote a program that prints 'Launch School is the best!' repeatedly until 
+a certain number of lines have been printed. Our solution looked like this:
 
 Initial Syntax Given:
 Explanation of Syntax:
