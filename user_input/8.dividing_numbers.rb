f8d1040d54cf80efbb0f78fda06f0ea8d0c4db6c
Justin Greger <justingreger@gmail.com>
2023-02-25 19:40:19 -0500

    What this means

--- user_input/8.dividing_numbers.rb
+++ user_input/8.dividing_numbers.rb
@@ -791,6 +791,118 @@ the string "is" followed by the string interpolation of the variable "result" wh
 that is converted to integer form which is divided by the variable "denominator" which is reassigned to the user 
 input in string form with the new line character removed that is converted to integer form.
 
+What this means:
 
+define the valid_number method where number_string is the parameter it is the place holder for the argument that the valid_number method accepts,
+which will be converted to integer form then converted to string form then compared with itself to determine 
+if it is a valid_number if through this process it evaluates to true.
+print to the screen the string "end" this is the last line of the method that is any code following this is outside of the method.
+
+The variable numerator is assigned the value of nil.
+
+start running a loop.
+print to the screen '>> Please enter the numerator:'
+This means the variable "numerator" is reassigned to the user input in string form with the new line character removed.
+break out of the loop if the user input value that was reassigned to numerator is a valid number,
+which is determined by passing it as an argument to the valid_number method which will be converted to integer form,
+then converted to string form then compared with itself to determine if it evaluates to true.
+print to the screen the string '>> Invalid input. Only integers are allowed.'
+This means the user input for the numerator was invalid specifically a non-integer character was entered. 
+print to the screen the string "end" this is the end of the loop anything after this is outside of the "loop do" loop.
+
+the variable denominator is assigned the value nil.
+
+
+
+
+
+
+
+
+
+start running a loop.
+puts ">> Please enter the denominator:"
+puts means print to the screen the string.
+">> Please enter the denominator:" is the string.
+>> is an input prompt.
+: is where the user input goes.
+This asks the user to enter the denominator value.
+The variable "denominator" is reassigned to gets.chomp
+gets is get string which is user input in string form.
+.chomp is a string method that removes the new line character.
+This means the variable "denominator" is reassigned to the user input in string form with the new line character removed.
+"if" is an "if" statement.
+"if" statements meand "if" the following condition is true then do something.
+"denominator" is a variable.
+== means equal
+'0' is a string that represents the number 0.
+This means if the variable "dernominator" is equal to string '0' then do something.
+This is a string.
+puts means put s.
+Which is put string.
+Which is print string to the screen.
+Which means print the string '>> Invalid input. A denominator of 0 is not allowed.' to the screen.
+>> is an input prompt.
+This is in response to the user input for denominator being invalid.
+Specifically a denominator of 0 is not allowed.
+if the above is false then do the following.
+break means break out of the loop.
+"if" is an "if" statement.
+"if" statements meand "if" the following condition is true then do something.
+valid_number?(denominator) references the valid_number?(number_string) method.
+denominator is the argument passed to the valid_number method that is it replaces the parameter (number_string) when executed.
+This means break out of the loop if the user input value that was reassigned to denominator is a valid number,
+which is determined by passing it as an argument to the valid_number method which will be converted to integer form,
+then converted to string form then compared with itself to determine if it evaluates to true.
+This is a string.
+puts '>> Invalid input. Only integers are allowed.'
+puts means print to the screen the string.
+This means print to the screen the string '>> Invalid input. Only integers are allowed.'
+>> is an input prompt.
+This means the user input for the numerator was invalid. 
+Specifically a non-integer character was entered. 
+print to the screen the string "end" this is the end of the loop anything after this is outside of the "if 
+denominator == '0'" loop.
+
+print to the screen the string "end" this is the end of the loop anything after this is outside of the "loop do" loop.
+
+variable result is assigned numerator.to_i / denominator.to_i
+numerator is a variable.
+At this point in the code the variable "numerator" is reassigned to gets.chomp
+gets is get string which is user input in string form.
+.chomp is a string method that removes the new line character.
+This means the variable "numerator" is reassigned to the user input in string form with the new line character removed.
+.to_i means convert to integer form.
+/ means divide by.
+denominator is a variable.
+At this point in the code the variable "denominator" is reassigned to gets.chomp
+gets is get string which is user input in string form.
+.chomp is a string method that removes the new line character.
+This means the variable "denominator" is reassigned to the user input in string form with the new line character removed.
+.to_i means convert to integer form.
+
+This means the variable "result" is assigned the variable "numerator" which is reassigned to the user input in string form 
+with the new line character removed that is converted to integer form which is divided by the variable "denominator" 
+which is reassigned to the user input in string form with the new line character removed that is converted to integer form.
+
+This is a string.
+puts string "#{numerator} / #{denominator} is #{result}"
+That is print to the screen the string  "#{numerator} / #{denominator} is #{result}"
+"#{}" is string interpolation which is a way of embedding expressions in a string.
+The variables numerator, denominator and result are interpolated in this string.
+numerator is a variable.
+The variable "numerator" is reassigned to the user input in string form with the new line character removed.
+/ means divide by.
+The variable "denominator" is reassigned to the user input in string form with the new line character removed.
+The variable "result" is assigned the variable "numerator" which is reassigned to the user input in string form 
+with the new line character removed that is converted to integer form which is divided by the variable "denominator" 
+which is reassigned to the user input in string form with the new line character removed that is converted to integer form.
+This means print to the screen the string interpolation of the variable "numerator" which is reassigned to the user 
+input in string form with the new line character removed divided by the string interpolation of the variable 
+"denominator" which is reassigned to the user input in string form with the new line character removed followed by 
+the string "is" followed by the string interpolation of the variable "result" which is assigned the variable 
+"numerator" which is reassigned to the user input in string form with the new line character removed 
+that is converted to integer form which is divided by the variable "denominator" which is reassigned to the user 
+input in string form with the new line character removed that is converted to integer form.
 
 =end
