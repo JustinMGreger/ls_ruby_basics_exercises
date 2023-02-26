3de1fb0de86e595d465d7d88533d15765b513f51
Justin Greger <justingreger@gmail.com>
2023-02-25 21:13:25 -0500

    Initial Syntax Given.

--- user_input/9.launch_school_printer_part_2.rb
+++ user_input/9.launch_school_printer_part_2.rb
@@ -4,6 +4,19 @@ In an earlier exercise, you wrote a program that prints 'Launch School is the be
 a certain number of lines have been printed. Our solution looked like this:
 
 Initial Syntax Given:
+number_of_lines = nil
+loop do
+  puts '>> How many output lines do you want? Enter a number >= 3:'
+  number_of_lines = gets.to_i
+  break if number_of_lines >= 3
+  puts ">> That's not enough lines."
+end
+
+while number_of_lines > 0
+  puts 'Launch School is the best!'
+  number_of_lines -= 1
+end
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
