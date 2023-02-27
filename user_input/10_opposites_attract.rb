d6d5efb4b2382f311dd77c62c0d11024cee4d503
Justin Greger <justingreger@gmail.com>
2023-02-26 22:04:00 -0500

    && 0

--- user_input/10_opposites_attract.rb
+++ user_input/10_opposites_attract.rb
@@ -26,11 +26,24 @@ valid_number is the method name.
 ? is a boolean that results in true or false.
 (number_string) is a parameter that the valid_number accepts as an argument.
 
-
-
-
-
+Syntax:
   number_string.to_i.to_s == number_string && number_string.to_i != 0
+
+Explanation:
+number_string is the parameter.
+.to_i means convert to integer form.
+.to_s means convert to string form.
+== means equal to.
+number_string is the parameter.
+&& is the logical and that only returns boolean true when both boolean expressions are true and returns false otherwise.
+.to_i means convert to integer form.
+!= means not equal to.
+0 is the integer 0.
+This means the parameter number_string is converted to integer form then converted to string form then compared to see
+if equal to the parameter number_string this will be boolean true or false and this is compared using the logical and
+to the parameter number_string that is converted to integer form that is true when the integer form does not equal 
+the integer 0.
+
 end
 
 
