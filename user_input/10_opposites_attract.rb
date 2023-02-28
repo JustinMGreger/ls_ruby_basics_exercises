26ff6fdeeeca6d5d5becb3467155a3d2a8e5fc07
Justin Greger <justingreger@gmail.com>
2023-02-27 19:56:16 -0500

    >> Please

--- user_input/10_opposites_attract.rb
+++ user_input/10_opposites_attract.rb
@@ -109,8 +109,22 @@ This is an integer.
 This is a positive integer.
 This is the user input.
 
-
+Syntax:
 >> Please enter a positive or negative integer:
+
+Explanation:
+'>> Please enter a positive or negative integer:' is a string.
+>> is an input prompt.
+: is were the input goes.
+This was printed to the screen.
+puts means put s which is put string.
+put string means print to the screen the string.
+This means print to the screen the string '>> Please enter a positive or negative integer:'
+This asks the user to input a positive or negative integer.
+
+
+
+
 0
 >> Invalid input. Only non-zero integers are allowed.
 >> Please enter a positive or negative integer:
