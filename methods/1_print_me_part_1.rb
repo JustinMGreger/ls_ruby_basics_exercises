6e06ca2589e567fd0fea8a20687b57d636fe275c
Justin Greger <justingreger@gmail.com>
2023-02-28 21:45:27 -0500

    completed.

--- methods/1_print_me_part_1.rb
+++ methods/1_print_me_part_1.rb
@@ -67,4 +67,12 @@ This calls the print_me method.
 The result of calling the print_me method is the execution of the code puts "I'm printing within the method!"
 That is "I'm printing within the method!" is printed to the screen.
 
+This means:
+the definition of the print_me method is the following:
+print to the screen the string "I'm printing within the method!"
+This is the last line in the method that is everything after this is outisde of the method.
+
+The result of calling the print_me method is the execution of the code puts "I'm printing within the method!"
+That is "I'm printing within the method!" is printed to the screen.
+
 =end
