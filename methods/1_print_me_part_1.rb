3362434ea9abc23c2c3dfdce5e461da3cbda876e
Justin Greger <justingreger@gmail.com>
2023-02-28 21:31:09 -0500

    Syntax of Solution

--- methods/1_print_me_part_1.rb
+++ methods/1_print_me_part_1.rb
@@ -11,6 +11,26 @@ The method is called by typing the name of the method.
 This calls the print_me method.
 
 Syntax of Solution:
+
+def print_me
+  puts "I'm printing within the method!"
+end
+
+print_me
+
 Explanation of Solution Syntax:
+def print_me
+  puts "I'm printing within the method!"
+end
+
+print_me
+
 Summary of Solution Syntax:
+
+def print_me
+  puts "I'm printing within the method!"
+end
+
+print_me
+
 =end
