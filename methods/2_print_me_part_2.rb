4ba95816b51236a71cc9a22010275bd395fb442e
Justin Greger <justingreger@gmail.com>
2023-02-28 21:56:00 -0500

    Initial Syntax Given

--- methods/2_print_me_part_2.rb
+++ methods/2_print_me_part_2.rb
@@ -3,6 +3,8 @@ Description of Exercise:
 Write a method named print_me so that I'm printing the return value! is printed when running the following code:
 
 Initial Syntax Given:
+puts print_me
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
