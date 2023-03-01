10ad399a4ed3957d68b82b29fbb09f0f870128bb
Justin Greger <justingreger@gmail.com>
2023-02-28 21:26:36 -0500

    explanation.

--- methods/1_print_me_part_1.rb
+++ methods/1_print_me_part_1.rb
@@ -6,6 +6,10 @@ Initial Syntax Given:
 print_me
 
 Explanation of Syntax:
+print_me is a method.
+The method is called by typing the name of the method.
+This calls the print_me method.
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
