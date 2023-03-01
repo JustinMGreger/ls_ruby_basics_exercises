61ec7e7dc9ff26b5e09b46f700395f4396c144ee
Justin Greger <justingreger@gmail.com>
2023-02-28 21:25:23 -0500

    Initial Syntax Given

--- methods/1_print_me_part_1.rb
+++ methods/1_print_me_part_1.rb
@@ -3,6 +3,8 @@ Description of Exercise:
 Write a method named print_me that prints I'm printing within the method! when invoked as follows:
 
 Initial Syntax Given:
+print_me
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
