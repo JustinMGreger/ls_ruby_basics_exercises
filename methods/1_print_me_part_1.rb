846159a1b6c635007adfc88dbb0a5bb09ae83774
Justin Greger <justingreger@gmail.com>
2023-02-28 21:41:25 -0500

    print_me

--- methods/1_print_me_part_1.rb
+++ methods/1_print_me_part_1.rb
@@ -41,7 +41,14 @@ end
 Explanation:
 This is the last line in the method that is everything after this is outisde of the method.
 
+Syntax:
 print_me
+
+Explanation:
+print_me is the print_me method.
+This calls the print_me method.
+The result of calling the print_me method is the execution of the code puts "I'm printing within the method!"
+That is "I'm printing within the method!" is printed to the screen.
 
 Summary of Solution Syntax:
 
@@ -55,6 +62,9 @@ This means print to the screen the string "I'm printing within the method!"
 
 This is the last line in the method that is everything after this is outisde of the method.
 
-print_me
+print_me is the print_me method.
+This calls the print_me method.
+The result of calling the print_me method is the execution of the code puts "I'm printing within the method!"
+That is "I'm printing within the method!" is printed to the screen.
 
 =end
