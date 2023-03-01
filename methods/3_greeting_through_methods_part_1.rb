91b14f4f8e3c730abfeb17d3d0b2741ba01a26bd
Justin Greger <justingreger@gmail.com>
2023-03-01 17:50:51 -0500

    Explanation of Syntax

--- methods/3_greeting_through_methods_part_1.rb
+++ methods/3_greeting_through_methods_part_1.rb
@@ -10,6 +10,14 @@ Initial Syntax Given:
 Hello World
 
 Explanation of Syntax:
+
+Syntax:
+Hello World
+
+Explanation:
+This is  a string.
+This could be string interpolation for example the code could be something like puts "#{method_1} + #{method_2}"
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
