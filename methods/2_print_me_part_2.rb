8bd10f1266b1ad9009215849d975e74c423dfa98
Justin Greger <justingreger@gmail.com>
2023-03-01 17:24:03 -0500

    Syntax of Solution

--- methods/2_print_me_part_2.rb
+++ methods/2_print_me_part_2.rb
@@ -11,6 +11,27 @@ print_me is a method.
 puts print_me means print to the screen the result of calling the print_me method.
 
 Syntax of Solution:
+
+def print_me
+  "I'm printing the return value!"
+end
+
+puts print_me
+
 Explanation of Solution Syntax:
+
+def print_me
+  "I'm printing the return value!"
+end
+
+puts print_me
+
 Summary of Solution Syntax:
+
+def print_me
+  "I'm printing the return value!"
+end
+
+puts print_me
+
 =end
