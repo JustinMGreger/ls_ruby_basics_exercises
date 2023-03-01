d5862c08acae5efa06fe493ea46cc79a7934282e
Justin Greger <justingreger@gmail.com>
2023-03-01 17:44:22 -0500

    Description of Exercise

--- methods/3_greeting_through_methods_part_1.rb
+++ methods/3_greeting_through_methods_part_1.rb
@@ -1,5 +1,9 @@
 =begin
 Description of Exercise:
+Write two methods, one that returns the string "Hello" and one that returns the string "World". 
+Then print both strings using #puts, combining them into one sentence.
+
+Expected output:
 
 Initial Syntax Given:
 Explanation of Syntax:
