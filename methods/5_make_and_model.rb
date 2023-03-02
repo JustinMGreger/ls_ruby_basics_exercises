2f80b8b4e868d26ae9dda1c087861a4dd90e06b7
Justin Greger <justingreger@gmail.com>
2023-03-01 21:14:38 -0500

    corolla

--- methods/5_make_and_model.rb
+++ methods/5_make_and_model.rb
@@ -9,6 +9,15 @@ Initial Syntax Given:
 car('Toyota', 'Corolla')
 
 Explanation of Syntax:
+
+Syntax:
+car('Toyota', 'Corolla')
+
+Explanation:
+car is the method.
+'Toyota' is a string and an argument that the car method accepts.
+'Corolla' is a string and an argument that the car method accepts.
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
