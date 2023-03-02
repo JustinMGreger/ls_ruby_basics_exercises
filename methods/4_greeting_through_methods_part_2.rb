cc7589bc486a4f7d4ae190f4714eafcaaa537534
Justin Greger <justingreger@gmail.com>
2023-03-01 19:11:31 -0500

    Explanation of Initial Syntax Given.

--- methods/4_greeting_through_methods_part_2.rb
+++ methods/4_greeting_through_methods_part_2.rb
@@ -12,7 +12,47 @@ def world
   'World'
 end
 
-Explanation of Syntax:
+Explanation of Initial Syntax Given:
+Syntax:
+def hello
+
+Explanation:
+def means definition.
+hello is the method name.
+This means definition of the hello method is the following.
+
+Syntax:
+  'Hello'
+
+Explanation:
+'Hello' is a string.
+
+Syntax:
+end
+
+Explanation:
+This is the last line of the method that is everything after this line is outisde of the method.
+
+Syntax:
+def world
+
+Explanation:
+def means definition.
+world is the name of the method.
+This means definition of the world method is the following.
+
+Syntax:
+  'World'
+
+Explanation:
+ 'World' is a string.
+
+Syntax:
+end
+
+Explanation:
+This is the last line of the method that is everything after this line is outisde of the method.
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
