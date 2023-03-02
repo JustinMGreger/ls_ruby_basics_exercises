f9fbafe94bb90cda9541e05965c2818acca4ff5e
Justin Greger <justingreger@gmail.com>
2023-03-01 21:28:23 -0500

    toyota corolla

--- methods/5_make_and_model.rb
+++ methods/5_make_and_model.rb
@@ -25,6 +25,22 @@ Expected output:
 Additional Initial Syntax Given:
 Toyota Corolla
 
+Explanation of Additional Initial Syntax Given:
+Syntax:
+Toyota Corolla
+
+Explanation:
+Toyota Corolla is a string. 
+This is the result of car method accepting 2 arguments which means there is probably string interpolation inside the car
+method and also a puts which is put s which is put string which is print to the screen which means the car method probably
+has inside of it print to the screen the string interpolation of the 2 parameters it accepts. 
+Something like:
+def car (parameter_1, parameter_2)
+  puts "#{parameter_1} #{parameter_2}"
+end
+car('Toyota', 'Corolla')
+That would result in the print to the screen the string "Toyota Corolla".
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
