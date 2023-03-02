23727c37763f28f5be5a882e63e1b4e335ec9b99
Justin Greger <justingreger@gmail.com>
2023-03-01 20:00:23 -0500

    world

--- methods/4_greeting_through_methods_part_2.rb
+++ methods/4_greeting_through_methods_part_2.rb
@@ -91,10 +91,26 @@ end
 Explanation:
 This is the last line of the method that is everything after this line is outisde of the method.
 
-
+Syntax:
 def world
+
+Explanation:
+def means definition.
+world is the name of the method.
+This means definition of the world method is the following.
+
+Syntax:
   'World'
+
+Explanation:
+ 'World' is a string.
+
+Syntax:
 end
+
+Explanation:
+This is the last line of the method that is everything after this line is outisde of the method.
+
 
 def greet
   hello + ' ' + world
@@ -127,9 +143,12 @@ This means definition of the hello method is the following.
 This is the last line of the method that is everything after this line is outisde of the method.
 
 
-def world
-  'World'
-end
+def means definition.
+world is the name of the method.
+This means definition of the world method is the following.
+ 'World' is a string.
+This is the last line of the method that is everything after this line is outisde of the method.
+
 
 def greet
   hello + ' ' + world
