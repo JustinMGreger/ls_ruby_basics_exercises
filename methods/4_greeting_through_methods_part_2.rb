296f6adbc0875ac63afe324752dee5c6fab18174
Justin Greger <justingreger@gmail.com>
2023-03-01 19:08:57 -0500

    Initial Syntax Given

--- methods/4_greeting_through_methods_part_2.rb
+++ methods/4_greeting_through_methods_part_2.rb
@@ -3,6 +3,15 @@ Description of Exercise:
 Write a method named greet that invokes the following methods:
 
 Initial Syntax Given:
+
+def hello
+  'Hello'
+end
+
+def world
+  'World'
+end
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
