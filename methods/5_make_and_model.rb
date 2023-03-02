bf74559c17543676a922d1ed91846565be13b1ff
Justin Greger <justingreger@gmail.com>
2023-03-01 21:11:15 -0500

    Description of Exercise

--- methods/5_make_and_model.rb
+++ methods/5_make_and_model.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+Using the following code, write a method called car that takes two arguments and prints a string containing the values 
+of both arguments.
+
 Initial Syntax Given:
 Explanation of Syntax:
 Syntax of Solution:
