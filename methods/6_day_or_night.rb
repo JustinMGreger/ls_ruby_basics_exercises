3b8ee010e2eb2b0c53ef673fcafa3232d4bdb192
Justin Greger <justingreger@gmail.com>
2023-03-03 20:59:06 -0500

    Description of Exercise

--- methods/6_day_or_night.rb
+++ methods/6_day_or_night.rb
@@ -1,6 +1,11 @@
 =begin
 Description of Exercise:
 
+The variable below will be randomly assigned as true or false. 
+Write a method named time_of_day that, given a boolean as an argument, 
+prints "It's daytime!" if the boolean is true and "It's nighttime!" if it's false. 
+Pass daylight into the method as the argument to determine whether it's day or night.
+
 Initial Syntax Given:
 Explanation of Syntax:
 Syntax of Solution:
