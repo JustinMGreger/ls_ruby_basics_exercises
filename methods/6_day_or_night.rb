9df6fd022c62c358a14566d615b31f280dff2a3e
Justin Greger <justingreger@gmail.com>
2023-03-03 21:01:31 -0500

    Initial Syntax Given.

--- methods/6_day_or_night.rb
+++ methods/6_day_or_night.rb
@@ -7,6 +7,9 @@ prints "It's daytime!" if the boolean is true and "It's nighttime!" if it's fals
 Pass daylight into the method as the argument to determine whether it's day or night.
 
 Initial Syntax Given:
+
+daylight = [true, false].sample
+
 Explanation of Syntax:
 Syntax of Solution:
 Explanation of Solution Syntax:
