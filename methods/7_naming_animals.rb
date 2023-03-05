dc8558284626aa0943533b16aa3ef4f0f4ea9361
Justin Greger <justingreger@gmail.com>
2023-03-04 19:31:24 -0500

    Description of Exercise.

--- methods/7_naming_animals.rb
+++ methods/7_naming_animals.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Run the code as it is shown below, and take notice of any error messages.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
