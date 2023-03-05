46c5d0432a34beb349e15fd458a6abf6b99fed5b
Justin Greger <justingreger@gmail.com>
2023-03-04 19:32:38 -0500

    Initial Syntax Given

--- methods/7_naming_animals.rb
+++ methods/7_naming_animals.rb
@@ -4,7 +4,30 @@ Description of Exercise:
 Run the code as it is shown below, and take notice of any error messages.
 
 Initial Syntax Given:
+def dog
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat}."
+
 Explanation of Initial Syntax Given:
+
+def dog
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat}."
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
