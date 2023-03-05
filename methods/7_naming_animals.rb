e21b1d615abfb173af4b2f2e7da3331e4fb0c58b
Justin Greger <justingreger@gmail.com>
2023-03-04 19:52:29 -0500

    def cat

--- methods/7_naming_animals.rb
+++ methods/7_naming_animals.rb
@@ -23,7 +23,7 @@ def dog
 Explanation:
 def means definition.
 dog is the method name.
-this means the dog method does not accept any parameter and is defined as the following:
+this means the dog method does not accept any parameter which will cause an argument error and is defined as the following:
 
 Syntax:
   return name
@@ -39,7 +39,15 @@ end
 Explanation:
 this is the last line of the method that is everything after this line is outside of the method.
 
+Syntax:
 def cat(name)
+
+Explanation:
+def means definition.
+cat is the method name.
+(name) is the parameter that the emthod accepts.
+This means the cat method accepts the (name) parameter and is defined as following:
+
   return name
 end
 
