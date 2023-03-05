230300a675e66efd48845d05f0d6df5b2ebb1b4d
Justin Greger <justingreger@gmail.com>
2023-03-04 19:29:45 -0500

    outline

---
+++ methods/7_naming_animals.rb
@@ -0,0 +1,9 @@
+=begin
+Description of Exercise:
+
+Initial Syntax Given:
+Explanation of Initial Syntax Given:
+Syntax of Solution:
+Explanation of Solution Syntax:
+Summary of Solution Syntax:
+=end
