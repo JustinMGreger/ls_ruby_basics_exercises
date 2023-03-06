fba87c2f1ed3aa936bf4ec1a657bb342653abc44
Justin Greger <justingreger@gmail.com>
2023-03-05 19:39:07 -0500

    Initial Syntax Given

--- methods/9_multiply_the_sum.rb
+++ methods/9_multiply_the_sum.rb
@@ -4,7 +4,17 @@ Description of Exercise:
 Write the following methods so that each output is true.
 
 Initial Syntax Given:
+
+puts add(2, 2) == 4
+puts add(5, 4) == 9
+puts multiply(add(2, 2), add(5, 4)) == 36
+
 Explanation of Initial Syntax Given:
+
+puts add(2, 2) == 4
+puts add(5, 4) == 9
+puts multiply(add(2, 2), add(5, 4)) == 36
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
