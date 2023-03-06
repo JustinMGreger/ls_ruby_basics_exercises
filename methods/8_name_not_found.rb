77cf5b14d00376f34776ef2df4d5e22a9dd0acbc
Justin Greger <justingreger@gmail.com>
2023-03-05 19:24:30 -0500

    completed

--- methods/8_name_not_found.rb
+++ methods/8_name_not_found.rb
@@ -134,13 +134,11 @@ this is the last line of the assign_name method that is everything after this is
 
 print to the screen the boolean value that is the true or false determination of if the method assign_name
 that accepts the string argument ('Kevin') is equal to the string 'Kevin'.
-
 the assign_name method accepts the string argument ('Kevin') as the parameter name that means the string 'Kevin' is 
 assigned to name which is equal to the string 'Kevin' which is boolean true which is printed to the screen.
 
 print to the screen the boolean value that is the true or false determination of if the method assign_name 
 is equal to the string 'Bob'.
-
 the assign_name method accepts the parameter name that is assigned to the string 'Bob' and is defined as 
 follows if the assign_name method does not accept an argument then the name parameter will be assigned the string
 'Bob' as no argument was passed to the assign_name method the name parameter was assigned the string 'Bob' which is 
