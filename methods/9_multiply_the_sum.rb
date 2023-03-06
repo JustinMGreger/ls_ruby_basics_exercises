6b2aef77e51a6b4c3a2faffe99f63fe2560d01d7
Justin Greger <justingreger@gmail.com>
2023-03-05 19:47:39 -0500

    explained syntax puts add 2 2 == 4

--- methods/9_multiply_the_sum.rb
+++ methods/9_multiply_the_sum.rb
@@ -11,7 +11,20 @@ puts multiply(add(2, 2), add(5, 4)) == 36
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 puts add(2, 2) == 4
+
+Explanation:
+puts means put s which is put string which means print the string to the screen.
+add is a function that takes two arguments and returns their sum.
+add is the method.
+(2, 2) are integers.
+== means equal to and returns boolean true or false.
+4 is an integer.
+this means print to the screen the boolean true or false result of comparing if the add method accepting the arugments
+integer 2 and integer 2 are equal to integer 4.
+
+
 puts add(5, 4) == 9
 puts multiply(add(2, 2), add(5, 4)) == 36
 
