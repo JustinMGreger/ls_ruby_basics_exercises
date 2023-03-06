8b1103e64b934f76210b2d78f5404b23bd8aac50
Justin Greger <justingreger@gmail.com>
2023-03-05 21:34:36 -0500

    Description of Exercise

--- methods/10_random_sentence.rb
+++ methods/10_random_sentence.rb
@@ -1,6 +1,11 @@
 =begin
 Description of Exercise:
 
+The variables below are both assigned to arrays. The first one, names, contains a list of names. 
+The second one, activities, contains a list of activities. 
+Write the methods name and activity so that they each take the appropriate array and return a random value from it. 
+Then write the method sentence that combines both values into a sentence and returns it from the method.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
