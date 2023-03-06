3aca31a7aa186f3e3a5e83b72b29d67d846a22d5
Justin Greger <justingreger@gmail.com>
2023-03-05 21:02:23 -0500

    puts multiply add 2 2 add 5 4 == 36

--- methods/9_multiply_the_sum.rb
+++ methods/9_multiply_the_sum.rb
@@ -147,9 +147,25 @@ add is the method.
 this means print to the screen the boolean true or false result of comparing if the add method accepting the arugments
 integer 5 and integer 4 are equal to integer 9.
 
-
-
+Syntax:
 puts multiply(add(2, 2), add(5, 4)) == 36
+
+Explanation
+puts means put s which is put string which means print the string to the screen.
+multiply is a function that takes two arguments and returns their product.
+multiply is the method.
+add(2, 2) and add(5, 4) are arguments.
+add is a function that takes two arguments and returns their sum.
+add is the method.
+(2, 2) are integers and arguments.
+add is a function that takes two arguments and returns their sum.
+add is the method.
+(5, 4) are integers and arguments.
+== means equal to and returns boolean true or false.
+36 is an integer.
+this means print to the screen the boolean true or false result of comparing if the product of the add method accepting 
+the arugments integer 2 and integer 2 and the add method accepting the arugments integer 5 and integer 4 is equal to the
+integer 36.
 
 Summary of Solution Syntax:
 
@@ -191,7 +207,21 @@ add is the method.
 this means print to the screen the boolean true or false result of comparing if the add method accepting the arugments
 integer 5 and integer 4 are equal to integer 9.
 
-puts multiply(add(2, 2), add(5, 4)) == 36
+puts means put s which is put string which means print the string to the screen.
+multiply is a function that takes two arguments and returns their product.
+multiply is the method.
+add(2, 2) and add(5, 4) are arguments.
+add is a function that takes two arguments and returns their sum.
+add is the method.
+(2, 2) are integers and arguments.
+add is a function that takes two arguments and returns their sum.
+add is the method.
+(5, 4) are integers and arguments.
+== means equal to and returns boolean true or false.
+36 is an integer.
+this means print to the screen the boolean true or false result of comparing if the product of the add method accepting 
+the arugments integer 2 and integer 2 and the add method accepting the arugments integer 5 and integer 4 is equal to the
+integer 36.
 
 This means:
 =end
