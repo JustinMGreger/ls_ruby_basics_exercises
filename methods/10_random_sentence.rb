45f236a46ccc49795c89368c83c05fff2e3ed102
Justin Greger <justingreger@gmail.com>
2023-03-06 22:03:34 -0500

    this means

--- methods/10_random_sentence.rb
+++ methods/10_random_sentence.rb
@@ -234,9 +234,35 @@ activity is a method.
 the activity method accepts the argument activities.
 activities is a variable that is assigned the array of elements ['walking', 'running', 'cycling'].
 this means print to the screen result of the sentence method accepting the arguments name and activity where the name 
-method accepts the argument names which is a variable that is assigned the array of elements 
+method accepts the argument names which is a variable that is assigned randomly from the array of elements 
 ['Dave', 'Sally', 'George', 'Jessica'] and activity method accepts the argument activities which is a variable that is 
-assigned the array of elements ['walking', 'running', 'cycling'].
+assigned randomly from the array of elements ['walking', 'running', 'cycling'].
 
 This means:
+
+the name method accepts the names parameter and is defined as follows:
+return the value of randomly selecting an element from the parameter that the method accepts.
+this is the last line of the method that is everything after this line is outside of the method.
+
+the activity method accepts the parameter activities and is defined as follows:
+return the value of randomly selecting an element from the parameter that the method accepts.
+this is the last line of the method that is everything after this line is outside of the method.
+
+the sentence method accepts parameters name and activity and is defined as follows:
+return the value of the string that has string interpolation of the parameters name and activity.
+this is the last line in the method that is everything after this line is outside of the method.
+
+names is a variable that is assigned the array of elements ['Dave', 'Sally', 'George', 'Jessica'].
+
+activities is a variable that is assigned the array of elements ['walking', 'running', 'cycling'].
+
+print to the screen result of the sentence method where the sentence method accepts parameters name and activity and is 
+defined as the return the value of the string that has string interpolation of the parameters name and activity that 
+is "#{name} went #{activity} today!" that accepts the arguments name and activity where the name method accepts the names
+parameter and is defined as the return value of randomly selecting an element from the parameter that the method accepts 
+which is the argument names which is a variable that is assigned randomly from the array of elements 
+['Dave', 'Sally', 'George', 'Jessica'] and the activity method accepts the parameter activities and is defined as the 
+return tvalue of randomly selecting an element from the parameter that the method accepts which is the argument activities
+that is a variable that is assigned randomly from the array of elements ['walking', 'running', 'cycling'].
+
 =end
