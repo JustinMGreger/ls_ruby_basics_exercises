b8cf03c98547999d51a2ccbe917e7c9ee4ddb096
Justin Greger <justingreger@gmail.com>
2023-03-07 19:35:39 -0500

    puts meal

--- return/1_breakfast_lunch_or_dinner_part_1.rb
+++ return/1_breakfast_lunch_or_dinner_part_1.rb
@@ -35,7 +35,14 @@ end
 Explanation:
 this is the last line of the method that is everything after this line is outside of the method.
 
+Syntax:
 puts meal
+
+Exlpanation:
+puts is put s which is put string which is print the string to the screen.
+meal is the meal method.
+the meal method is defined as stop running the method and the value returned from the method is the string 'Breakfast'
+this means print to the screen the return value of meal which is the string 'Breakfast'.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
