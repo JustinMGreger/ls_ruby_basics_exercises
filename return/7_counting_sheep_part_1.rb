a841486363c9660871b2662bd173271de04547ef
Justin Greger <justingreger@gmail.com>
2023-03-08 21:08:10 -0500

    5 times do sheep

--- return/7_counting_sheep_part_1.rb
+++ return/7_counting_sheep_part_1.rb
@@ -23,10 +23,17 @@ def means definition.
 count_sheep is the method name.
 this means the count_sheep method is defined as follows:
 
-
-
-
+Syntax:
   5.times do |sheep|
+
+Explanation:
+5 is an integer.
+.times is a method used for iterating a block of code a specific number of times.
+do is used to define the begining of a block.
+|sheep| is a block variable.
+this means iterate through the block 5 times and |sheep| is used to capture the iteration number.
+
+
     puts sheep
   end
 end
