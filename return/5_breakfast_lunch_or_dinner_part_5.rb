db210639b491e30bc42a5b67a64ed29f62b81689
Justin Greger <justingreger@gmail.com>
2023-03-08 19:09:45 -0500

    Initial Syntax Given

--- return/5_breakfast_lunch_or_dinner_part_5.rb
+++ return/5_breakfast_lunch_or_dinner_part_5.rb
@@ -4,7 +4,23 @@ Description of Exercise:
 What will the following code print? Why? Don't run it until you've attempted to answer.
 
 Initial Syntax Given:
+
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
+
 Explanation of Initial Syntax Given:
+
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
