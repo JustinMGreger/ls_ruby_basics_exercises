39e73732ad1efc9abdf467855be5966ae72be8a8
Justin Greger <justingreger@gmail.com>
2023-03-08 20:52:11 -0500

    Initial Syntax Given

--- return/7_counting_sheep_part_1.rb
+++ return/7_counting_sheep_part_1.rb
@@ -4,7 +4,25 @@ Description of Exercise:
 What will the following code print? Why? Don't run it until you've attempted to answer.
 
 Initial Syntax Given:
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+  end
+end
+
+puts count_sheep
+
 Explanation of Initial Syntax Given:
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+  end
+end
+
+puts count_sheep
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
