069c65fc6065d25c05d84a0d11475602c9d692da
Justin Greger <justingreger@gmail.com>
2023-03-09 19:41:01 -0500

    Initial Syntax Given

--- return/9_counting_sheep_part_3.rb
+++ return/9_counting_sheep_part_3.rb
@@ -4,7 +4,31 @@ Description of Exercise:
 What will the following code print? Why? Don't run it until you've attempted to answer.
 
 Initial Syntax Given:
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+    if sheep >= 2
+      return
+    end
+  end
+end
+
+p count_sheep
+
 Explanation of Initial Syntax Given:
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+    if sheep >= 2
+      return
+    end
+  end
+end
+
+p count_sheep
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
