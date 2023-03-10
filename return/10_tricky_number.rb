d99909911fa61f981280c30e3f7a694560532368
Justin Greger <justingreger@gmail.com>
2023-03-09 21:38:23 -0500

    Initial Syntax Given

--- return/10_tricky_number.rb
+++ return/10_tricky_number.rb
@@ -4,7 +4,29 @@ Description of Exercise:
 What will the following code print? Why? Don't run it until you've attempted to answer.
 
 Initial Syntax Given:
+
+def tricky_number
+  if true
+    number = 1
+  else
+    2
+  end
+end
+
+puts tricky_number
+
 Explanation of Initial Syntax Given:
+
+def tricky_number
+  if true
+    number = 1
+  else
+    2
+  end
+end
+
+puts tricky_number
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
