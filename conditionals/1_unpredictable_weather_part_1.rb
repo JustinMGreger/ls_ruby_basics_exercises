fb0bbbeee5e82c971048dcecbdd0e8355cf7f82c
Justin Greger <justingreger@gmail.com>
2023-03-10 21:46:38 -0500

    Summary of Solution Syntax

--- conditionals/1_unpredictable_weather_part_1.rb
+++ conditionals/1_unpredictable_weather_part_1.rb
@@ -68,5 +68,24 @@ Explanation:
 this is the last line of the if statement that is everything after this is outside of the if statement.
 
 Summary of Solution Syntax:
+
+sun is a variable.
+= means assignment.
+['visible', 'hidden'] is an array of elements.
+.sample is a method that randomly selects an element from an array.
+this means the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
+
+if statement means if a condition is boolean true then do something.
+sun is assigned to the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
+== means equal.
+'visible' is a string that is an element in the sun array.
+this means if the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'] and if that
+is specifically equal to the string 'visible' which is an element in the sun array then do something.
+
+puts is put s which is put string which means print the string to the screen.
+'The sun is so bright!' is a string.
+this means print to the screen the string 'The sun is so bright!'.
+
+
 This means:
 =end
