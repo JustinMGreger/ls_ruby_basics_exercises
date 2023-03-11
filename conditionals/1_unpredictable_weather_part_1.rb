9e2a86dd1e82740c4fae51eaab79f9488f5d0f53
Justin Greger <justingreger@gmail.com>
2023-03-10 21:19:10 -0500

    Syntax of Solution

--- conditionals/1_unpredictable_weather_part_1.rb
+++ conditionals/1_unpredictable_weather_part_1.rb
@@ -23,12 +23,18 @@ Additional Description of Exercise:
 
 Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
 
-
-
-
-
 Syntax of Solution:
+
+if sun == 'visible'
+  puts 'The sun is so bright!'
+end
+
 Explanation of Solution Syntax:
+
+if sun == 'visible'
+  puts 'The sun is so bright!'
+end
+
 Summary of Solution Syntax:
 This means:
 =end
