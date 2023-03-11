fc904bd925e1de6358404837cb58adea3dd2b4d3
Justin Greger <justingreger@gmail.com>
2023-03-10 21:38:29 -0500

    if sun == visible

--- conditionals/1_unpredictable_weather_part_1.rb
+++ conditionals/1_unpredictable_weather_part_1.rb
@@ -24,6 +24,7 @@ Additional Description of Exercise:
 Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
 
 Syntax of Solution:
+sun = ['visible', 'hidden'].sample
 
 if sun == 'visible'
   puts 'The sun is so bright!'
@@ -31,7 +32,28 @@ end
 
 Explanation of Solution Syntax:
 
+Syntax:
+sun = ['visible', 'hidden'].sample
+
+Explanation:
+sun is a variable.
+= means assignment.
+['visible', 'hidden'] is an array of elements.
+.sample is a method that randomly selects an element from an array.
+this means the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
+
+Syntax:
 if sun == 'visible'
+
+Explanation:
+if statement means if a condition is boolean true then do something.
+sun is assigned to the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
+== means equal.
+'visible' is a string that is an element in the sun array.
+this means if the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'] and if that
+is specifically equal to the string 'visible' which is an element in the sun array then do something.
+
+
   puts 'The sun is so bright!'
 end
 
