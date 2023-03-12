7cf04fda6e48a645fd66ede67601cba23cf0e78d
Justin Greger <justingreger@gmail.com>
2023-03-11 21:38:16 -0500

    Explanation of Initial Syntax Given

--- conditionals/3_unpredictable_weather_part_3.rb
+++ conditionals/3_unpredictable_weather_part_3.rb
@@ -8,6 +8,17 @@ Initial Syntax Given:
 sun = ['visible', 'hidden'].sample
 
 Explanation of Initial Syntax Given:
+
+Syntax:
+sun = ['visible', 'hidden'].sample
+
+Explanation:
+sun is a variable.
+= means assignment.
+['visible', 'hidden'] is an array of elements.
+.sample is a method that randomly selects an element from an array.
+this means the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
