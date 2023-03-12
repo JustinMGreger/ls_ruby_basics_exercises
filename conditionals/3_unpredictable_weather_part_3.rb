58e5be8f9f35678206214716755eca8c7e293c4b
Justin Greger <justingreger@gmail.com>
2023-03-11 21:35:21 -0500

    Initial Syntax Given

--- conditionals/3_unpredictable_weather_part_3.rb
+++ conditionals/3_unpredictable_weather_part_3.rb
@@ -4,6 +4,9 @@ Description of Exercise:
 In the code below, sun is randomly assigned as 'visible' or 'hidden'.
 
 Initial Syntax Given:
+
+sun = ['visible', 'hidden'].sample
+
 Explanation of Initial Syntax Given:
 Syntax of Solution:
 Explanation of Solution Syntax:
