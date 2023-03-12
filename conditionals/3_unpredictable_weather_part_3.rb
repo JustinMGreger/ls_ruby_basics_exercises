b4231630197b084faea4878a528851947c6102b5
Justin Greger <justingreger@gmail.com>
2023-03-11 21:43:44 -0500

    Additional Description of Exercise

--- conditionals/3_unpredictable_weather_part_3.rb
+++ conditionals/3_unpredictable_weather_part_3.rb
@@ -19,6 +19,14 @@ sun is a variable.
 .sample is a method that randomly selects an element from an array.
 this means the variable sun is assigned the randomly selected element from the array ['visible', 'hidden'].
 
+Additional Description of Exercise:
+
+Write an if statement that prints "The sun is so bright!" if sun equals visible. 
+Also, write an unless statement that prints "The clouds are blocking the sun!" unless sun equals visible.
+
+When writing these statements, take advantage of Ruby's expressiveness and use statement modifiers instead of an 
+if...end statement to print results only when some condition is met or not met.
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
