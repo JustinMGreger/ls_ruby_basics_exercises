fadf277eea4498ecf4059543589fb2e79138d924
Justin Greger <justingreger@gmail.com>
2023-03-11 19:50:54 -0500

    Description of Exercise

--- conditionals/2_unpredictable_weather_part_2.rb
+++ conditionals/2_unpredictable_weather_part_2.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+In the code below, sun is randomly assigned as 'visible' or 'hidden'.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
