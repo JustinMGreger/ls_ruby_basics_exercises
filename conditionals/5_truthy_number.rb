13935650f43e0279e317a40d069041e5f9f5030b
Justin Greger <justingreger@gmail.com>
2023-03-13 19:40:53 -0400

    if number

--- conditionals/5_truthy_number.rb
+++ conditionals/5_truthy_number.rb
@@ -24,7 +24,14 @@ number is a variable.
 7 is an integer.
 this means the variable number is assigned to the integer 7.
 
+Syntax:
 if number
+
+Explanation:
+if statements mean if boolean true then do something.
+number is the variable number.
+this means if number is boolean true then do something.
+
   puts "My favorite number is #{number}."
 else
   puts "I don't have a favorite number."
