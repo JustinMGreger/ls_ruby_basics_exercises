9e3a8c204b1d7469b3bf95f10ef57887694f1ec8
Justin Greger <justingreger@gmail.com>
2023-03-13 19:33:04 -0400

    completed.

--- conditionals/4_true_or_false.rb
+++ conditionals/4_true_or_false.rb
@@ -26,12 +26,43 @@ Write a ternary operator that prints "I'm true!" if boolean equals true and prin
 
 Syntax of Solution:
 
+boolean = [true, false].sample
+
 boolean ? puts("I'm true!") : puts("I'm false!")
 
 Explanation of Solution Syntax:
 
+Syntax:
+boolean = [true, false].sample
+
+Explanation:
+boolean is a variable.
+= means assignment.
+[true, false] is an array of elements.
+true and false are elements in the array.
+.sample is an array method that randomly selects elements in an array.
+this means that the variable boolean is assigned a randomly selected element from the array [true, false].
+
+Syntax:
 boolean ? puts("I'm true!") : puts("I'm false!")
 
-Summary of Solution Syntax:
-This means:
+Explanation:
+boolean is a variable.
+= means assignment.
+[true, false] is an array of elements.
+true and false are elements in the array.
+.sample is an array method that randomly selects elements in an array.
+this means that the variable boolean is assigned a randomly selected element from the array [true, false].
+this is a ternary operator that follows the format <condition> ? <result if true> : <result if false>
+? means if boolean true or false.
+puts means put s which is put string which is print to the screen the string.
+("I'm true!") is a string.
+: means seperation.
+puts means put s which is put string which is print to the screen the string.
+("I'm false!") is a string.
+
+this means:
+that the variable boolean is assigned a randomly selected element from the array [true, false] if true then
+print to the screen the string ("I'm true!") and if false print to the screen the string ("I'm false!").
+
 =end
