ff29fe6f2681c1651eebb44a96eeda54f39c37e6
Justin Greger <justingreger@gmail.com>
2023-03-12 21:54:08 -0400

    Initial Syntax Given

--- conditionals/4_true_or_false.rb
+++ conditionals/4_true_or_false.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 In the code below, boolean is randomly assigned as true or false.
 
 Initial Syntax Given:
+
+boolean = [true, false].sample
+
 Explanation of Initial Syntax Given:
+
+boolean = [true, false].sample
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
