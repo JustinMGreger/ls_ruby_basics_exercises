0fb6ecd03cf308f359d41f95d22c05eef4f58352
Justin Greger <justingreger@gmail.com>
2023-03-13 19:55:12 -0400

    puts my favorite number

--- conditionals/5_truthy_number.rb
+++ conditionals/5_truthy_number.rb
@@ -32,7 +32,16 @@ if statements mean if boolean true then do something.
 number is the variable number.
 this means if number is boolean true then do something.
 
+Syntax:
   puts "My favorite number is #{number}."
+
+Explanation:
+puts is put s which means put string which means print to the screen the string.
+"My favorite number is #{number}." is a string.
+#{number} is string interpolation referencing the varaible number.
+this means print to the screen the string "My favorite number is #{number}." which is string interpolation referencing 
+the varaible number.
+
 else
   puts "I don't have a favorite number."
 end
