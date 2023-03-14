7d44c5e50da987c62bcdf3479d6176e6b1ae1a2d
Justin Greger <justingreger@gmail.com>
2023-03-13 20:31:30 -0400

    Description of Exercise

--- conditionals/6_stoplight_part_1.rb
+++ conditionals/6_stoplight_part_1.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+In the code below, stoplight is randomly assigned as 'green', 'yellow', or 'red'.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
