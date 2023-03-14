50adeb60887697046270e06757fa03ab25c2e89a
Justin Greger <justingreger@gmail.com>
2023-03-13 20:40:08 -0400

    Additional Description of Exercise

--- conditionals/6_stoplight_part_1.rb
+++ conditionals/6_stoplight_part_1.rb
@@ -10,15 +10,19 @@ stoplight = ['green', 'yellow', 'red'].sample
 Explanation of Initial Syntax Given:
 
 Syntax:
+stoplight = ['green', 'yellow', 'red'].sample
+
+Explanation:
 stoplight is a variable.
 = is assignment.
 ['green', 'yellow', 'red'] is an array of string elements.
 .sample is an array method that randomly selects an element from the array of elements.
 this means the stoplight variable is assigned a randomly selected string element from the array ['green', 'yellow', 'red'].
 
+Additional Description of Exercise:
 
-
-stoplight = ['green', 'yellow', 'red'].sample
+Write a case statement that prints "Go!" if stoplight equals 'green', "Slow down!" if stoplight equals 'yellow', and 
+"Stop!" if stoplight equals 'red'.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
