2a328e1ab1eae999d2ca4ed5233bc30f7e34c034
Justin Greger <justingreger@gmail.com>
2023-03-14 19:24:27 -0400

    stoplight

--- conditionals/7_stoplight_part_2.rb
+++ conditionals/7_stoplight_part_2.rb
@@ -18,7 +18,18 @@ end
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 stoplight = ['green', 'yellow', 'red'].sample
+
+Explanation:
+stoplight is a variable.
+= means assignment.
+['green', 'yellow', 'red'] is an array of string elements.
+.sample is an array method that randomly selects from the elements.
+this means the variable stoplight is assigned to the randomly selected string element from the array 
+['green', 'yellow', 'red'].
+
+
 
 case stoplight
 when 'green'
