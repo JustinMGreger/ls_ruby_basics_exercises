8654a828963eca3ee89e928052dfea6242de9f01
Justin Greger <justingreger@gmail.com>
2023-03-13 20:33:36 -0400

    Initial Syntax Given

--- conditionals/6_stoplight_part_1.rb
+++ conditionals/6_stoplight_part_1.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 In the code below, stoplight is randomly assigned as 'green', 'yellow', or 'red'.
 
 Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
 Explanation of Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
