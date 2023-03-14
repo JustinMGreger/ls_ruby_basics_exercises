811ebb801515ec1c3cc83146d5a909931bb35584
Justin Greger <justingreger@gmail.com>
2023-03-14 19:19:18 -0400

    Initial Syntax Given

--- conditionals/7_stoplight_part_2.rb
+++ conditionals/7_stoplight_part_2.rb
@@ -4,7 +4,31 @@ Description of Exercise:
 Convert the following case statement to an if statement.
 
 Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+  puts 'Go!'
+when 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
 Explanation of Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+  puts 'Go!'
+when 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
