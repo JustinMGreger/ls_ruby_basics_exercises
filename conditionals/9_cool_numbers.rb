45f7742cf7398b851f71684532d767e3048093b2
Justin Greger <justingreger@gmail.com>
2023-03-15 19:27:25 -0400

    Initial Syntax Given

--- conditionals/9_cool_numbers.rb
+++ conditionals/9_cool_numbers.rb
@@ -5,7 +5,25 @@ In the code below, number is randomly assigned a number between 0 and 9.
 Then, an if statement is used to print "5 is a cool number!" or "Other numbers are cool too!" based on the value of number.
 
 Initial Syntax Given:
+
+number = rand(10)
+
+if number = 5
+  puts '5 is a cool number!'
+else
+  puts 'Other numbers are cool too!'
+end
+
 Explanation of Initial Syntax Given:
+
+number = rand(10)
+
+if number = 5
+  puts '5 is a cool number!'
+else
+  puts 'Other numbers are cool too!'
+end
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
