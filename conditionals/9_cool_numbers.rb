61a85e8e9dc0eba895175ec5601269cdae560746
Justin Greger <justingreger@gmail.com>
2023-03-15 19:21:49 -0400

    Description of Exercise

--- conditionals/9_cool_numbers.rb
+++ conditionals/9_cool_numbers.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+In the code below, number is randomly assigned a number between 0 and 9. 
+Then, an if statement is used to print "5 is a cool number!" or "Other numbers are cool too!" based on the value of number.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
