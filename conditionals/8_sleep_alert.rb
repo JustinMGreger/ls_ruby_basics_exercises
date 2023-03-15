8fa94c9b84550e5c84e1a2374e493c6432888072
Justin Greger <justingreger@gmail.com>
2023-03-14 21:23:00 -0400

    status

--- conditionals/8_sleep_alert.rb
+++ conditionals/8_sleep_alert.rb
@@ -9,7 +9,15 @@ status = ['awake', 'tired'].sample
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 status = ['awake', 'tired'].sample
+
+Explanation:
+status is a variable.
+= means assignment.
+['awake', 'tired'] is array of string.
+.sample is an array method that randomly selects elements from an array.
+this means the variable status is assigned the randomly selected string element from the array ['awake', 'tired'].
 
 Syntax of Solution:
 Explanation of Solution Syntax:
