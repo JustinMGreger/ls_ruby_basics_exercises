5c772f60b4568b4c5cb55d700c10c5b3287f16be
Justin Greger <justingreger@gmail.com>
2023-03-15 19:33:38 -0400

    number = rand 10

--- conditionals/9_cool_numbers.rb
+++ conditionals/9_cool_numbers.rb
@@ -16,7 +16,13 @@ end
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 number = rand(10)
+
+Explanation:
+number is a variable.
+= means assignment.
+rand(10) means return a random integer between 0 and 9 which includes 0 but excludes 10.
 
 if number = 5
   puts '5 is a cool number!'
