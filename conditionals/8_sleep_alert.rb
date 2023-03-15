eee5db46382b50cea98dc257fc32bdd0f7098c10
Justin Greger <justingreger@gmail.com>
2023-03-14 21:20:06 -0400

    Initial Syntax Given

--- conditionals/8_sleep_alert.rb
+++ conditionals/8_sleep_alert.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 In the code below, status is randomly assigned as 'awake' or 'tired'.
 
 Initial Syntax Given:
+
+status = ['awake', 'tired'].sample
+
 Explanation of Initial Syntax Given:
+
+status = ['awake', 'tired'].sample
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
