0e81b21ca2bea4e6ab979158aba7e5efb97c15e5
Justin Greger <justingreger@gmail.com>
2023-03-15 21:37:17 -0400

    Description of Exercise

--- conditionals/10_stoplight_part_3.rb
+++ conditionals/10_stoplight_part_3.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Reformat the following case statement so that it only takes up 5 lines.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
