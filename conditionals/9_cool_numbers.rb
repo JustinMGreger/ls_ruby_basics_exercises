594dd6af48f4f7aef6841e6ae213c26f796d58af
Justin Greger <justingreger@gmail.com>
2023-03-15 20:36:52 -0400

    Syntax of Solution

--- conditionals/9_cool_numbers.rb
+++ conditionals/9_cool_numbers.rb
@@ -66,7 +66,25 @@ Currently, "5 is a cool number!" is being printed every time the program is run.
 Fix the code so that "Other numbers are cool too!" gets a chance to be executed.
 
 Syntax of Solution:
+
+number = rand(10)
+
+if number == 5
+  puts '5 is a cool number!'
+else
+  puts 'Other numbers are cool too!'
+end
+
 Explanation of Solution Syntax:
+
+number = rand(10)
+
+if number == 5
+  puts '5 is a cool number!'
+else
+  puts 'Other numbers are cool too!'
+end
+
 Summary of Solution Syntax:
 This means:
 =end
