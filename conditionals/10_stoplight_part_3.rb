e66abc29a2ceb3b4f228d54531bb1758cdee8284
Justin Greger <justingreger@gmail.com>
2023-03-15 21:38:25 -0400

    Initial Syntax Given

--- conditionals/10_stoplight_part_3.rb
+++ conditionals/10_stoplight_part_3.rb
@@ -4,7 +4,31 @@ Description of Exercise:
 Reformat the following case statement so that it only takes up 5 lines.
 
 Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+  puts 'Go!'
+when 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
 Explanation of Initial Syntax Given:
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+  puts 'Go!'
+when 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
