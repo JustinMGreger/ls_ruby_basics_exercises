6962da72286918556014205fe104cf1cacc9f6af
Justin Greger <justingreger@gmail.com>
2023-03-17 19:56:04 -0400

    puts its

--- strings/2_quote_confusion.rb
+++ strings/2_quote_confusion.rb
@@ -9,7 +9,19 @@ puts 'It\'s now 12 o\'clock.'
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 puts 'It\'s now 12 o\'clock.'
+
+Explanation:
+puts mean put s which is put string which is print to the screen the string.
+'It\'s now 12 o\'clock.' is a string.
+\ backslash is used to escape the single quotes within the string this is done so that the string doesn't end at the '
+which would cause a syntax error for the rest of the string following the ' that is the ' is treated as a literal 
+character.
+this means print to the screen the string 'It\'s now 12 o\'clock.' and the \ backslash is used to escape the single quotes
+within the string this is done so that the string doesn't end at the ' which would cause a syntax error for the rest of 
+the string following the ' that is the ' is treated as a literal character which results in the string being printed to
+the screen being      It's now 12 o'clock.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
