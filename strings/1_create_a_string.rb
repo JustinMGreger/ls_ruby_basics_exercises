c43cec1bb62fa3e3811d92f9f01b2e379ff00049
Justin Greger <justingreger@gmail.com>
2023-03-17 19:22:47 -0400

    Initial Syntax Given

--- strings/1_create_a_string.rb
+++ strings/1_create_a_string.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 Create an empty string using the String class and assign it to a variable.
 
 Initial Syntax Given:
+
+empty_string = String.new
+
 Explanation of Initial Syntax Given:
+
+empty_string = String.new
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
