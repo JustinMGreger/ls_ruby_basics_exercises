b6e595e66b6b2ffc4df20726489bc6f75bb0a0c1
Justin Greger <justingreger@gmail.com>
2023-03-17 19:37:59 -0400

    completed.

--- strings/1_create_a_string.rb
+++ strings/1_create_a_string.rb
@@ -9,10 +9,19 @@ empty_string = String.new
 
 Explanation of Initial Syntax Given:
 
+Syntax:
+
 empty_string = String.new
 
-Syntax of Solution:
-Explanation of Solution Syntax:
-Summary of Solution Syntax:
-This means:
+Explanation:
+
+empty_string is a variable.
+= means assignment.
+String is string class in Ruby.
+.new creates a new instance of a class in Ruby.
+
+this means: 
+the variable empty_string is assigned to the new instance of the Ruby class String which means the variable
+empty_string is assigned to an empty string.
+
 =end
