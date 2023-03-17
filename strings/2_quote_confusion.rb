9256935ab6e3b561f7ed3e8454de75cc0cab3fc7
Justin Greger <justingreger@gmail.com>
2023-03-17 19:47:04 -0400

    Initial Syntax Given

--- strings/2_quote_confusion.rb
+++ strings/2_quote_confusion.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 Modify the following code so that double-quotes are used instead of single-quotes.
 
 Initial Syntax Given:
+
+puts 'It\'s now 12 o\'clock.'
+
 Explanation of Initial Syntax Given:
+
+puts 'It\'s now 12 o\'clock.'
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
