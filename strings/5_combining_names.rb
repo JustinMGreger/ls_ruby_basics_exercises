523c01aff020b3041617fb5e61e5c7c047af1652
Justin Greger <justingreger@gmail.com>
2023-03-18 21:48:08 -0400

    Description of Exercise

--- strings/5_combining_names.rb
+++ strings/5_combining_names.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+Using the following code, combine the two names together to form a full name and assign that value to a variable named 
+full_name. Then, print the value of full_name.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
