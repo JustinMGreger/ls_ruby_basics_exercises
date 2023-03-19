9d93bb91a73fa9bcdf328f8ce22649e41de1a96a
Justin Greger <justingreger@gmail.com>
2023-03-18 21:49:27 -0400

    Initial Syntax Given

--- strings/5_combining_names.rb
+++ strings/5_combining_names.rb
@@ -5,7 +5,15 @@ Using the following code, combine the two names together to form a full name and
 full_name. Then, print the value of full_name.
 
 Initial Syntax Given:
+
+first_name = 'John'
+last_name = 'Doe'
+
 Explanation of Initial Syntax Given:
+
+first_name = 'John'
+last_name = 'Doe'
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
