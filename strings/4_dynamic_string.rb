a0bc18b09287ad083edcf36561a77e3fce2be26a
Justin Greger <justingreger@gmail.com>
2023-03-18 20:31:30 -0400

    Initial Syntax Given

--- strings/4_dynamic_string.rb
+++ strings/4_dynamic_string.rb
@@ -4,7 +4,17 @@ Description of Exercise:
 Modify the following code so that the value of name is printed within "Hello, !".
 
 Initial Syntax Given:
+
+name = 'Elizabeth'
+
+puts "Hello, !"
+
 Explanation of Initial Syntax Given:
+
+name = 'Elizabeth'
+
+puts "Hello, !"
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
