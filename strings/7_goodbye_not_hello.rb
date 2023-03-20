0c0446731b35dae1635c4312a497a1e09363b363
Justin Greger <justingreger@gmail.com>
2023-03-20 19:30:58 -0400

    Initial Syntax Given

--- strings/7_goodbye_not_hello.rb
+++ strings/7_goodbye_not_hello.rb
@@ -4,7 +4,15 @@ Description of Exercise:
 Given the following code, invoke a destructive method on greeting so that Goodbye! is printed instead of Hello!.
 
 Initial Syntax Given:
+
+greeting = 'Hello!'
+puts greeting
+
 Explanation of Initial Syntax Given:
+
+greeting = 'Hello!'
+puts greeting
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
