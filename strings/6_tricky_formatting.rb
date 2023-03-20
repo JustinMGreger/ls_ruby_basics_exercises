b51e7f6074d1e53af33d460ea88f9b75f973a587
Justin Greger <justingreger@gmail.com>
2023-03-19 21:13:40 -0400

    Initial Syntax Given

--- strings/6_tricky_formatting.rb
+++ strings/6_tricky_formatting.rb
@@ -1,8 +1,17 @@
 =begin
 Description of Exercise:
 
+Using the following code, capitalize the value of state then print the modified value.
+Note that state should have the modified value both before and after you print it.
+
 Initial Syntax Given:
+
+state = 'tExAs'
+
 Explanation of Initial Syntax Given:
+
+state = 'tExAs'
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
