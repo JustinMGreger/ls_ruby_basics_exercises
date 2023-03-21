68c834f60c546e2c6c55e2323d6b4d5db6c76c04
Justin Greger <justingreger@gmail.com>
2023-03-20 21:57:56 -0400

    Additional Initial Syntax Given

--- strings/9_pluralize.rb
+++ strings/9_pluralize.rb
@@ -1,10 +1,9 @@
 =begin
-Description of Exercise:
 
+Description of Exercise:
 Given the following code, use Array#each to print the plural of each word in words.
 
 Initial Syntax Given:
-
 words = 'car human elephant airplane'
 
 Explanation of Initial Syntax Given:
@@ -18,10 +17,20 @@ words is a variable.
 'car human elephant airplane' is a string.
 this means the variable words is assigned the string 'car human elephant airplane'
 
-
 Additional Description of Exercise:
-
 Expected output:
+
+Additional Initial Syntax Given:
+cars
+humans
+elephants
+airplanes
+
+Explanation of Additional Initial Syntax Given:
+cars
+humans
+elephants
+airplanes
 
 Syntax of Solution:
 Explanation of Solution Syntax:
