fe29b0f6ee1640ed7fe7cf9d322ced32d6408018
Justin Greger <justingreger@gmail.com>
2023-03-20 20:31:37 -0400

    Initial Syntax Given

--- strings/8_print_the_alphabet.rb
+++ strings/8_print_the_alphabet.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 Using the following code, split the value of alphabet by individual characters and print each character.
 
 Initial Syntax Given:
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 Explanation of Initial Syntax Given:
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
