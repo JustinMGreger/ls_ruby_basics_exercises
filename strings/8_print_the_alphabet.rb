c735699288d95aa6cadde96878415c3eed75492d
Justin Greger <justingreger@gmail.com>
2023-03-20 20:32:48 -0400

    Initial Syntax Given

--- strings/8_print_the_alphabet.rb
+++ strings/8_print_the_alphabet.rb
@@ -9,7 +9,14 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+Explanation:
+alphabet is a variable.
+= means assignment.
+'abcdefghijklmnopqrstuvwxyz' is a string.
+this means the variable alphabet is assigned the string 'abcdefghijklmnopqrstuvwxyz'.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
