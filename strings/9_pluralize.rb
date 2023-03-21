5c75de9a3efb082e60500cf33bd59f78a462c57b
Justin Greger <justingreger@gmail.com>
2023-03-20 21:50:38 -0400

    Description of Exercise

--- strings/9_pluralize.rb
+++ strings/9_pluralize.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Given the following code, use Array#each to print the plural of each word in words.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
