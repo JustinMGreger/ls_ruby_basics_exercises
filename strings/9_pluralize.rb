92fa1eb70d41c6a51901352e3dc78cf9615d3e92
Justin Greger <justingreger@gmail.com>
2023-03-20 21:52:28 -0400

    Initial Syntax Given

--- strings/9_pluralize.rb
+++ strings/9_pluralize.rb
@@ -4,7 +4,13 @@ Description of Exercise:
 Given the following code, use Array#each to print the plural of each word in words.
 
 Initial Syntax Given:
+
+words = 'car human elephant airplane'
+
 Explanation of Initial Syntax Given:
+
+words = 'car human elephant airplane'
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
