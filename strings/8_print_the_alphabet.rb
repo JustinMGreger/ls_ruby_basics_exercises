03b7586411723c3845f18c1434c4010f2d0d9182
Justin Greger <justingreger@gmail.com>
2023-03-20 20:36:08 -0400

    Additional Initial Syntax Given

--- strings/8_print_the_alphabet.rb
+++ strings/8_print_the_alphabet.rb
@@ -22,6 +22,67 @@ Additional Description of Exercise:
 
 Expected output:
 
+Additional Initial Syntax Given:
+
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+
+Explanation of Additional Initial Syntax Given:
+
+Syntax:
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+
+Explanation:
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
