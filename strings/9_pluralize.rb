d52803ae47c96757eb0e17c39b3f8d7d749a081f
Justin Greger <justingreger@gmail.com>
2023-03-20 21:54:40 -0400

    Explanation of Initial Syntax Given

--- strings/9_pluralize.rb
+++ strings/9_pluralize.rb
@@ -9,7 +9,14 @@ words = 'car human elephant airplane'
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 words = 'car human elephant airplane'
+
+Explanation:
+words is a variable.
+= means assignment.
+'car human elephant airplane' is a string.
+this means the variable words is assigned the string 'car human elephant airplane'
 
 Syntax of Solution:
 Explanation of Solution Syntax:
