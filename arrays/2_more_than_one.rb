f225a04cbd30caf4af8a2d18ef4a0572bd9cf204
Justin Greger <justingreger@gmail.com>
2023-03-22 19:07:00 -0400

    Additional Description of Exercise

--- arrays/2_more_than_one.rb
+++ arrays/2_more_than_one.rb
@@ -11,6 +11,10 @@ pets is a variable.
 ['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
 this means the variable pets is assigned the array of string elements ['cat', 'dog', 'fish', 'lizard'].
 
+Additional Description of Exercise:
+Write some code that selects 'fish' and 'lizard' from the pets array - select the two items at the same time. 
+Assign the return value to a variable named my_pets, then print the contents of my_pets as a single string, e.g.:
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
