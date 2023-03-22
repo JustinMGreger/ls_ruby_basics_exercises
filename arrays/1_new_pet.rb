6f74082f2033a8a1a0b69c92a0a0186aa4868d36
Justin Greger <justingreger@gmail.com>
2023-03-21 21:16:43 -0400

    Additional Description of Exercise

--- arrays/1_new_pet.rb
+++ arrays/1_new_pet.rb
@@ -11,6 +11,11 @@ pets is a varible.
 ['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
 this means the varible pets is assigned the array of string elements ['cat', 'dog', 'fish', 'lizard'].
 
+Additional Description of Exercise:
+Select 'fish' from pets, assign the return value to a variable named my_pet, then print the value of my_pet.
+
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
