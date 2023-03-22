2d956cabeb0d13276db402d6bd05b33892f46daa
Justin Greger <justingreger@gmail.com>
2023-03-22 18:58:51 -0400

    Initial Syntax Given

--- arrays/2_more_than_one.rb
+++ arrays/2_more_than_one.rb
@@ -1,10 +1,12 @@
 =begin
 Description of Exercise:
-
 In the code below, an array containing different types of pets is assigned to pets.
 
 Initial Syntax Given:
+pets = ['cat', 'dog', 'fish', 'lizard']
+
 Explanation of Initial Syntax Given:
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
