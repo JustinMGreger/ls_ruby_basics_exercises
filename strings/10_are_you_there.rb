821e9fd02eca0924124fab42a999c1125bfee738
Justin Greger <justingreger@gmail.com>
2023-03-21 20:23:12 -0400

    Initial Syntax Given

--- strings/10_are_you_there.rb
+++ strings/10_are_you_there.rb
@@ -1,11 +1,13 @@
 =begin
 Description of Exercise:
-
 Using the following code, print true if colors includes the color 'yellow' and print false if it doesn't. Then, print true
 if colors includes the color 'purple' and print false if it doesn't.
 
 Initial Syntax Given:
+colors = 'blue pink yellow orange'
+
 Explanation of Initial Syntax Given:
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
