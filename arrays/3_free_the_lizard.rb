cb4677d91a10dedd242b6faa0055d0d55df88e98
Justin Greger <justingreger@gmail.com>
2023-03-23 19:53:57 -0400

    Description of Exercise

--- arrays/3_free_the_lizard.rb
+++ arrays/3_free_the_lizard.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+In the code below, an array containing different types of pets is assigned to pets. Also, the return value of pets[2..3],
+which is ['fish', 'lizard'], is assigned to my_pets.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
