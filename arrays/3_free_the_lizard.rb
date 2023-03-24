7d13b03204af584b35cbf23ba7ec0f126a92c6e1
Justin Greger <justingreger@gmail.com>
2023-03-23 20:44:39 -0400

    pets = cat dog fish lizard

--- arrays/3_free_the_lizard.rb
+++ arrays/3_free_the_lizard.rb
@@ -59,12 +59,33 @@ puts "I have a pet #{my_pets[0]}!"
 
 Explanation of Solution Syntax:
 
+Syntax:
 pets = ['cat', 'dog', 'fish', 'lizard']
+
+Explanation:
+pets is a variable.
+= means assignment.
+['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
+this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+
+
 my_pets = pets[2..3]
 
 my_pets.pop
 puts "I have a pet #{my_pets[0]}!"
 
 Summary of Solution Syntax:
+
+pets is a variable.
+= means assignment.
+['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
+this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+
 This means:
+
+pets is a variable.
+= means assignment.
+['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
+this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+
 =end
