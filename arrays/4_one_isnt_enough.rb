a13c843102249735b498353a493b13fe147c7739
Justin Greger <justingreger@gmail.com>
2023-03-24 19:06:33 -0400

    my_pets = pets 2..3

--- arrays/4_one_isnt_enough.rb
+++ arrays/4_one_isnt_enough.rb
@@ -20,9 +20,25 @@ pets is a variable.
 ['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
 this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
 
-
-
+Syntax:
 my_pets = pets[2..3]
+
+Explanation:
+my_pets is a variable.
+= means assignment.
+pets is a variable.
+= means assignment.
+['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
+this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+[2..3] is a range of index location for the array of string elements ['cat', 'dog', 'fish', 'lizard'] that is
+['cat', 'dog', 'fish', 'lizard'][2..3] accesses the string element 'fish' for index location [2] and the string element
+'lizard' for index location [3].
+this means the variable my_pets is assigned the variable pets which is assigned to the array of string elements 
+['cat', 'dog', 'fish', 'lizard'] which is accessed by the range of index locations [2..3] that is the string element 'fish'
+for index location [2] and the string element 'lizard' for index location [3].
+
+
+
 my_pets.pop
 
 Syntax of Solution:
