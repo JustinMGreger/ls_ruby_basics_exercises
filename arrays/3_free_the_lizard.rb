cb06379bcfee61dce64580063807937de43f469d
Justin Greger <justingreger@gmail.com>
2023-03-23 21:44:11 -0400

    completed.

--- arrays/3_free_the_lizard.rb
+++ arrays/3_free_the_lizard.rb
@@ -197,63 +197,22 @@ pet fish!
 
 This means:
 
-pets is a variable.
-= means assignment.
-['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
-this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
 
-my_pets is a variable.
-= means assignment.
-pets is a variable.
-= means assignment.
-['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
-this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
-[2..3] are a range of index locations.
-this means the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3].
+the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned the array 
+of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2] and 
+'lizard' at index location [3].
 
-my_pets is a variable.
-= means assignment.
-pets is a variable.
-= means assignment.
-['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
-this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
-[2..3] are a range of index locations.
-this means the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3].
-.pop method is used to remove and return the last element of the array.
-this means the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3] that is my_pets is assigned to the array ['fish', 'lizard'] and then the last element 
-of the array is removed and returned which results in the array my_pets being assigned ['fish'].
+the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned the array 
+of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2] and 
+'lizard' at index location [3] that is my_pets is assigned to the array ['fish', 'lizard'] and then the last element of 
+the array is removed and returned which results in the array my_pets being assigned ['fish'].
 
-puts means put s which is put string which is print to the screen the string.
-"I have a pet #{my_pets[0]}!" is a string.
-#{my_pets[0]} is string interpolation.
-my_pets is a variable.
-= means assignment.
-pets is a variable.
-= means assignment.
-['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
-this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
-[2..3] are a range of index locations.
-this means the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3].
-.pop method is used to remove and return the last element of the array.
-this means the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3] that is my_pets is assigned to the array ['fish', 'lizard'] and then the last element 
-of the array is removed and returned which results in the array my_pets being assigned ['fish'].
-[0] references the index location of my_pets which is ['fish'][0] which is the string element 'fish'.
-this means print to the screen the string "I have a pet #{my_pets[0]}!" which is string interpolation #{my_pets[0]} 
-the variable my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned
-the array of string elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2]
-and 'lizard' at index location [3] that is my_pets is assigned to the array ['fish', 'lizard'] and then the last element 
-of the array is removed and returned which results in the array my_pets being assigned ['fish'] and [0] references the 
-index location of my_pets which is ['fish'][0] which is the string element 'fish' which results in the output: I have a 
-pet fish!
+print to the screen the string "I have a pet #{my_pets[0]}!" which is string interpolation #{my_pets[0]} the variable 
+my_pets is assigned to the range of index locations [2..3] of the variable pets which is assigned the array of string 
+elements ['cat', 'dog', 'fish', 'lizard'] which are the string elements 'fish' at index location [2] and 'lizard' at index
+location [3] that is my_pets is assigned to the array ['fish', 'lizard'] and then the last element of the array is removed
+and returned which results in the array my_pets being assigned ['fish'] and [0] references the index location of my_pets 
+which is ['fish'][0] which is the string element 'fish' which results in the output: I have a pet fish!
 
 =end
