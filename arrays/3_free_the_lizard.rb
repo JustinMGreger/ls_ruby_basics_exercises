83467d79d8ca6e062468eb1285e93311641badf3
Justin Greger <justingreger@gmail.com>
2023-03-23 20:10:47 -0400

    my_pets = pets 2..3

--- arrays/3_free_the_lizard.rb
+++ arrays/3_free_the_lizard.rb
@@ -18,8 +18,20 @@ Explanation:
 pets is a variable.
 = means assignment.
 ['cat', 'dog', 'fish', 'lizard'] is an array of strings.
+this means the variable pets is assigned to the array of strings ['cat', 'dog', 'fish', 'lizard'].
 
+Syntax:
+my_pets = pets[2..3]
 
+Explanation:
+my_pets is a variable.
+= means assignment.
+the variable pets is assigned to the array of strings ['cat', 'dog', 'fish', 'lizard'].
+[2..3] of ['cat', 'dog', 'fish', 'lizard'] are index locations where [2] is the string element 'fish' and [3] is string 
+element 'lizard' that is [2..3] is a range of index locations.
+this means that the variable my_pets is assigned to the range of index locations [2..3] of the the variable pets which is 
+assigned to the array of strings ['cat', 'dog', 'fish', 'lizard'] where [2] is the string element 'fish' and [3] is string
+element 'lizard'.
 
 Syntax of Solution:
 Explanation of Solution Syntax:
