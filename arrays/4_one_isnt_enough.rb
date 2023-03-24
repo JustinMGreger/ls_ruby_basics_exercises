6740c106c106b41f44e31b46626a54c9d111e9b0
Justin Greger <justingreger@gmail.com>
2023-03-23 21:52:02 -0400

    Initial Syntax Given

--- arrays/4_one_isnt_enough.rb
+++ arrays/4_one_isnt_enough.rb
@@ -4,7 +4,17 @@ Description of Exercise:
 Without changing the code below, select 'dog' from pets, add the return value to my_pets, then print the value of my_pets.
 
 Initial Syntax Given:
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
 Explanation of Initial Syntax Given:
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
