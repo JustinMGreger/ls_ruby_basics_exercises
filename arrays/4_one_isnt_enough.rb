01fdc60fb193883d2dc86d4e541e1910bf7f4842
Justin Greger <justingreger@gmail.com>
2023-03-23 21:50:25 -0400

    Description of Exercise

--- arrays/4_one_isnt_enough.rb
+++ arrays/4_one_isnt_enough.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Without changing the code below, select 'dog' from pets, add the return value to my_pets, then print the value of my_pets.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
