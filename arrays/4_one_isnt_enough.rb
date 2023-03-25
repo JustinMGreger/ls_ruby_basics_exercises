2c247a0c3fba853189e0a4158ce20e9a995a010b
Justin Greger <justingreger@gmail.com>
2023-03-24 20:33:27 -0400

    push

--- arrays/4_one_isnt_enough.rb
+++ arrays/4_one_isnt_enough.rb
@@ -132,8 +132,32 @@ for index location [2] and the string element 'lizard' for index location [3] wh
 location then the string element 'lizard' is removed and returned from the array which results in the variable my_pets
 being assigned the array of string elements ['fish'].
 
-
+Syntax:
 my_pets.push(pets[1])
+
+Explanation:
+my_pets is a variable.
+= means assignment.
+pets is a variable.
+= means assignment.
+['cat', 'dog', 'fish', 'lizard'] is an array of string elements.
+this means the variable pets is assigned to the array of string elements ['cat', 'dog', 'fish', 'lizard'].
+[2..3] is a range of index location for the array of string elements ['cat', 'dog', 'fish', 'lizard'] that is
+['cat', 'dog', 'fish', 'lizard'][2..3] accesses the string element 'fish' for index location [2] and the string element
+'lizard' for index location [3].
+.pop method is used to remove and return the last element from an array.
+this means the variable my_pets is assigned the variable pets which is assigned to the array of string elements 
+['cat', 'dog', 'fish', 'lizard'] which is accessed by the range of index locations [2..3] that is the string element 'fish'
+for index location [2] and the string element 'lizard' for index location [3] which for it's own array is [0,1] at index 
+location then the string element 'lizard' is removed and returned from the array which results in the variable my_pets
+being assigned the array of string elements ['fish'].
+.push method adds an element to the end an array.
+
+
+
+
+
+
 puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
 
 Summary of Solution Syntax:
