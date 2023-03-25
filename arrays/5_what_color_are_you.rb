217e99fe8b2fa2f113e550d03d26fc444388fa11
Justin Greger <justingreger@gmail.com>
2023-03-24 21:55:16 -0400

    Initial Syntax Given

--- arrays/5_what_color_are_you.rb
+++ arrays/5_what_color_are_you.rb
@@ -1,10 +1,12 @@
 =begin
 Description of Exercise:
-
 In the code below, an array containing different types of colors is assigned to colors.
 
 Initial Syntax Given:
+colors = ['red', 'yellow', 'purple', 'green']
+
 Explanation of Initial Syntax Given:
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
