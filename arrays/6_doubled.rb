ce1c021880703ca94f48209baa60b4e1e377c13f
Justin Greger <justingreger@gmail.com>
2023-03-26 19:00:06 -0400

    Initial Syntax Given

--- arrays/6_doubled.rb
+++ arrays/6_doubled.rb
@@ -4,6 +4,9 @@ Description of Exercise:
 In the code below, an array containing the numbers 1 through 5 is assigned to numbers.
 
 Initial Syntax Given:
+
+numbers = [1, 2, 3, 4, 5]
+
 Explanation of Initial Syntax Given:
 Syntax of Solution:
 Explanation of Solution Syntax:
