d5419348ecd1d25f6d9a1ab8de517f698519f1fb
Justin Greger <justingreger@gmail.com>
2023-03-26 19:02:22 -0400

    Explanation of Initial Syntax Given

--- arrays/6_doubled.rb
+++ arrays/6_doubled.rb
@@ -8,6 +8,11 @@ Initial Syntax Given:
 numbers = [1, 2, 3, 4, 5]
 
 Explanation of Initial Syntax Given:
+numbers is a variable.
+= means assignment.
+[1, 2, 3, 4, 5] is an array of integer elements.
+this means the variable numbers is assigned to the array of integer elements [1, 2, 3, 4, 5].
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
