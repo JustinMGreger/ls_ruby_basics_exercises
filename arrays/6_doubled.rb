3c128b22bb255628598c1c6bda65fb4dac25666f
Justin Greger <justingreger@gmail.com>
2023-03-26 19:04:41 -0400

    Additional Description of Exercise

--- arrays/6_doubled.rb
+++ arrays/6_doubled.rb
@@ -13,6 +13,12 @@ numbers is a variable.
 [1, 2, 3, 4, 5] is an array of integer elements.
 this means the variable numbers is assigned to the array of integer elements [1, 2, 3, 4, 5].
 
+Additional Description of Exercise:
+Use Array#map to iterate over numbers and return a new array with each number doubled. 
+Assign the returned array to a variable named doubled_numbers and print its value using #p.
+Expected output:
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
