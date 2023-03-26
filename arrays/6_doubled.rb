1813cfb330bd3bacfb47331fbc21a5d087577800
Justin Greger <justingreger@gmail.com>
2023-03-26 18:58:18 -0400

    Description of Exercise

--- arrays/6_doubled.rb
+++ arrays/6_doubled.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+In the code below, an array containing the numbers 1 through 5 is assigned to numbers.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
