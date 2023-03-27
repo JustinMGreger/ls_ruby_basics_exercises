f670d9990533d50262833f6d6c7ed8b880f4894e
Justin Greger <justingreger@gmail.com>
2023-03-26 21:45:03 -0400

    Explanation of Initial Syntax Given

--- arrays/7_divisible_by_three.rb
+++ arrays/7_divisible_by_three.rb
@@ -7,6 +7,11 @@ Initial Syntax Given:
 numbers = [5, 9, 21, 26, 39]
 
 Explanation of Initial Syntax Given:
+numbers is a variable.
+= means assignment.
+[5, 9, 21, 26, 39] is an array of integer elements.
+this means the variable numbers is assigned the array of integer elements [5, 9, 21, 26, 39].
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
