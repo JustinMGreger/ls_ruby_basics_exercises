4050abdeb97de5507c2b43c5a2ccefbc48d9b885
Justin Greger <justingreger@gmail.com>
2023-03-26 21:41:46 -0400

    Initial Syntax Given

--- arrays/7_divisible_by_three.rb
+++ arrays/7_divisible_by_three.rb
@@ -4,6 +4,8 @@ Description of Exercise:
 In the code below, an array containing five numbers is assigned to numbers.
 
 Initial Syntax Given:
+numbers = [5, 9, 21, 26, 39]
+
 Explanation of Initial Syntax Given:
 Syntax of Solution:
 Explanation of Solution Syntax:
