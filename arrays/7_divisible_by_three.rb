9bd33f0bc1bd941d8ec4ddcddbc22c9c4dcb5fa9
Justin Greger <justingreger@gmail.com>
2023-03-26 21:59:36 -0400

    Explanation of Additional Initial Syntax Given

--- arrays/7_divisible_by_three.rb
+++ arrays/7_divisible_by_three.rb
@@ -12,6 +12,21 @@ numbers is a variable.
 [5, 9, 21, 26, 39] is an array of integer elements.
 this means the variable numbers is assigned the array of integer elements [5, 9, 21, 26, 39].
 
+Additional Description of Exercise:
+Use Array#select to iterate over numbers and return a new array that contains only numbers divisible by three. 
+Assign the returned array to a variable named divisible_by_three and print its value using #p.
+Expected output:
+
+Additional Initial Syntax Given:
+[9, 21, 39]
+
+Explanation of Additional Intiial Syntax Given:
+this was assigned to the variable divisible_by_three.
+this is an array of integers.
+this was pritned with p.
+the syntax for this is p divisible_by_three 
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
