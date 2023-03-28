46b60601a5dfd8d7f5f2294108f4aa607fe55dec
Justin Greger <justingreger@gmail.com>
2023-03-28 19:30:12 -0400

    Description of Exericse

--- arrays/9_favorite_number_part_2.rb
+++ arrays/9_favorite_number_part_2.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+In the code below, a nested array containing three groups of names and numbers is assigned to favorites.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
