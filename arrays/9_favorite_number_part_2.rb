c90c5213abd3edcff29554379eeca9a09b1b4bd2
Justin Greger <justingreger@gmail.com>
2023-03-28 19:33:06 -0400

    Initial Syntax Given

--- arrays/9_favorite_number_part_2.rb
+++ arrays/9_favorite_number_part_2.rb
@@ -4,7 +4,11 @@ Description of Exercise:
 In the code below, a nested array containing three groups of names and numbers is assigned to favorites.
 
 Initial Syntax Given:
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
 Explanation of Initial Syntax Given:
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
