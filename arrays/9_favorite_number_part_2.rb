52aca4445ace68e8f1903323880b963fe7d1bbe0
Justin Greger <justingreger@gmail.com>
2023-03-28 19:38:50 -0400

    Explanation of Initial Syntax Given

--- arrays/9_favorite_number_part_2.rb
+++ arrays/9_favorite_number_part_2.rb
@@ -7,7 +7,11 @@ Initial Syntax Given:
 favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
 
 Explanation of Initial Syntax Given:
-
+favorites is a variable.
+= means assignment.
+[['Dave', 7], ['Miranda', 3], ['Jason', 11]] is nested array three arrays of string elements and integer elements.
+this means the favorites variable is assigned the nested array of three arrays of string elements and integer elements 
+[['Dave', 7], ['Miranda', 3], ['Jason', 11]].
 
 Syntax of Solution:
 Explanation of Solution Syntax:
