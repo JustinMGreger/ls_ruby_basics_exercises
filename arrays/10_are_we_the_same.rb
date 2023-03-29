4dcead552adc28c971c62ffefe2a00a810f1c330
Justin Greger <justingreger@gmail.com>
2023-03-28 22:00:04 -0400

    puts array1 == array2

--- arrays/10_are_we_the_same.rb
+++ arrays/10_are_we_the_same.rb
@@ -58,7 +58,24 @@ array2 is a variable.
 [1, 9, 5] is an array of integer elements.
 this means the variable array2 is assigned the array of integer elements [1, 9, 5].
 
+Syntax:
 puts array1 == array2
+
+Explanation:
+puts means put s which is put string which means print to the screen the string.
+array1 is a variable.
+= means assignment.
+[1, 5, 9] is an array of integer elements.
+this means the variable array1 is assigned the array of integer elements [1, 5, 9].
+== means equals and return boolean true if the compared values are equal and false if they are not.
+array2 is a variable.
+= means assignment.
+[1, 9, 5] is an array of integer elements.
+this means the variable array2 is assigned the array of integer elements [1, 9, 5].
+this means print to the screen boolean true if the variable array1 which is assigned the array of integer elements 
+[1, 5, 9] is equal to the variable array2 which is assigned the array of integer elements [1, 9, 5] and return boolean
+false if it is not equal.
+the result of which is the output: false
 
 Summary of Solution Syntax:
 
@@ -72,6 +89,21 @@ array2 is a variable.
 [1, 9, 5] is an array of integer elements.
 this means the variable array2 is assigned the array of integer elements [1, 9, 5].
 
+puts means put s which is put string which means print to the screen the string.
+array1 is a variable.
+= means assignment.
+[1, 5, 9] is an array of integer elements.
+this means the variable array1 is assigned the array of integer elements [1, 5, 9].
+== means equals and return boolean true if the compared values are equal and false if they are not.
+array2 is a variable.
+= means assignment.
+[1, 9, 5] is an array of integer elements.
+this means the variable array2 is assigned the array of integer elements [1, 9, 5].
+this means print to the screen boolean true if the variable array1 which is assigned the array of integer elements 
+[1, 5, 9] is equal to the variable array2 which is assigned the array of integer elements [1, 9, 5] and return boolean
+false if it is not equal.
+the result of which is the output: false
+
 This means:
 
 array1 is a variable.
@@ -83,5 +115,20 @@ array2 is a variable.
 = means assignment.
 [1, 9, 5] is an array of integer elements.
 this means the variable array2 is assigned the array of integer elements [1, 9, 5].
+
+puts means put s which is put string which means print to the screen the string.
+array1 is a variable.
+= means assignment.
+[1, 5, 9] is an array of integer elements.
+this means the variable array1 is assigned the array of integer elements [1, 5, 9].
+== means equals and return boolean true if the compared values are equal and false if they are not.
+array2 is a variable.
+= means assignment.
+[1, 9, 5] is an array of integer elements.
+this means the variable array2 is assigned the array of integer elements [1, 9, 5].
+this means print to the screen boolean true if the variable array1 which is assigned the array of integer elements 
+[1, 5, 9] is equal to the variable array2 which is assigned the array of integer elements [1, 9, 5] and return boolean
+false if it is not equal.
+the result of which is the output: false
 
 =end
