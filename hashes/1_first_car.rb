fdb42440b9ec85dc23128b1714e3e4cabfe6c478
Justin Greger <justingreger@gmail.com>
2023-03-29 19:58:20 -0400

    Syntax of Solution

--- hashes/1_first_car.rb
+++ hashes/1_first_car.rb
@@ -9,8 +9,26 @@ type	color	mileage
 sedan	blue	80_000
 
 Explanation of Initial Syntax Given:
+type is string assigned sedan which is a string.
+color is a string assigned blue which is a string.
+mileage is a string assigned 80_000 which is an integer.
+
 Syntax of Solution:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000
+}
+
 Explanation of Solution Syntax:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000
+}
+
 Summary of Solution Syntax:
 This means:
 =end
