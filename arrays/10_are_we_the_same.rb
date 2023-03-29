503552a4f78392400c7e7052a5808cbe78d12627
Justin Greger <justingreger@gmail.com>
2023-03-28 21:22:43 -0400

    Description of Exercise

--- arrays/10_are_we_the_same.rb
+++ arrays/10_are_we_the_same.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+In the code below, two arrays containing several numbers are assigned to two variables, array1 and array2.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
