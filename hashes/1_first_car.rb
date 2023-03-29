1035bb10087ab6e63fb393543d5be3e3543623a5
Justin Greger <justingreger@gmail.com>
2023-03-29 19:16:06 -0400

    Description of Exercise

--- hashes/1_first_car.rb
+++ hashes/1_first_car.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Create a hash that contains the following data and assign it to a variable named car.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
