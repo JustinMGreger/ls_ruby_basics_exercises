1f7cfb1b1b9d72d965e7cf8364e3cccfe688a9db
Justin Greger <justingreger@gmail.com>
2023-03-28 21:25:08 -0400

    Initial Syntax Given

--- arrays/10_are_we_the_same.rb
+++ arrays/10_are_we_the_same.rb
@@ -4,7 +4,15 @@ Description of Exercise:
 In the code below, two arrays containing several numbers are assigned to two variables, array1 and array2.
 
 Initial Syntax Given:
+
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
 Explanation of Initial Syntax Given:
+
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
