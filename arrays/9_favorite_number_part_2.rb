84aab983f5c5fabd82dd8e464466c25c922ca2e3
Justin Greger <justingreger@gmail.com>
2023-03-28 20:06:57 -0400

    Additional Initial Syntax Given

--- arrays/9_favorite_number_part_2.rb
+++ arrays/9_favorite_number_part_2.rb
@@ -14,10 +14,15 @@ this means the favorites variable is assigned the nested array of three arrays o
 [['Dave', 7], ['Miranda', 3], ['Jason', 11]].
 
 Additional Description of Exercise:
-Flatten and print this array. That is, the printed result should not have any subarrays, but should have all of the original strings and numbers from the original array:
+Flatten and print this array. That is, the printed result should not have any subarrays, 
+but should have all of the original strings and numbers from the original array:
 
 Expected output:
 
+Additional Initial Syntax Given:
+["Dave", 7, "Miranda", 3, "Jason", 11]
+
+Explanation of Additional Initial Syntax Given:
 
 Syntax of Solution:
 Explanation of Solution Syntax:
