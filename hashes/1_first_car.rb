d4588728efbb3b7ee3ac1844652aecdf637a191a
Justin Greger <justingreger@gmail.com>
2023-03-29 19:18:19 -0400

    Initial Syntax Given

--- hashes/1_first_car.rb
+++ hashes/1_first_car.rb
@@ -4,6 +4,10 @@ Description of Exercise:
 Create a hash that contains the following data and assign it to a variable named car.
 
 Initial Syntax Given:
+
+type	color	mileage
+sedan	blue	80_000
+
 Explanation of Initial Syntax Given:
 Syntax of Solution:
 Explanation of Solution Syntax:
