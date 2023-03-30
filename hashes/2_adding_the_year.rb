33b11d7b3e75126133f6e718b76e2551db921f42
Justin Greger <justingreger@gmail.com>
2023-03-29 20:33:05 -0400

    Description of Exercise

--- hashes/2_adding_the_year.rb
+++ hashes/2_adding_the_year.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Using the code below, add the key :year and the value 2003 to car.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
