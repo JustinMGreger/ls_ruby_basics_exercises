3392c4277c481962e9bc42b9c3811369e3390792
Justin Greger <justingreger@gmail.com>
2023-03-29 20:47:45 -0400

    Initial Syntax Given

--- hashes/2_adding_the_year.rb
+++ hashes/2_adding_the_year.rb
@@ -4,7 +4,21 @@ Description of Exercise:
 Using the code below, add the key :year and the value 2003 to car.
 
 Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000
+}
+
 Explanation of Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
