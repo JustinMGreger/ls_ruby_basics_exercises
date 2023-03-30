34131fe7fc2acd2c240868a0d6f5875ce467e08c
Justin Greger <justingreger@gmail.com>
2023-03-30 16:29:22 -0400

    Initial Syntax Given

--- hashes/3_broken_odometer.rb
+++ hashes/3_broken_odometer.rb
@@ -4,7 +4,23 @@ Description of Exercise:
 Using the following code, delete the key :mileage and its associated value from car.
 
 Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000,
+  year:    2003
+}
+
 Explanation of Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000,
+  year:    2003
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
