10fd9fecd3c1dafaeaebd7bb47ef486073fa6e3f
Justin Greger <justingreger@gmail.com>
2023-03-30 21:34:31 -0400

    Initial Syntax Given

--- hashes/4_what_color.rb
+++ hashes/4_what_color.rb
@@ -4,7 +4,21 @@ Description of Exercise:
 Using the following code, select the value 'blue' from car and print it with #puts.
 
 Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  year:    2003
+}
+
 Explanation of Initial Syntax Given:
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  year:    2003
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
