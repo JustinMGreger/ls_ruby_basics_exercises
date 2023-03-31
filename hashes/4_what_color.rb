707d1725179fcae0023d681e98f7f87be5bb94d4
Justin Greger <justingreger@gmail.com>
2023-03-30 21:34:01 -0400

    Description of Exercise

--- hashes/4_what_color.rb
+++ hashes/4_what_color.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Using the following code, select the value 'blue' from car and print it with #puts.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
