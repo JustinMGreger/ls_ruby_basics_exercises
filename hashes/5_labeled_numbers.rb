07fe326fbf8c92dbaf14fa86810b7e689f68b6c3
Justin Greger <justingreger@gmail.com>
2023-03-31 16:32:25 -0400

    numbers = {

--- hashes/5_labeled_numbers.rb
+++ hashes/5_labeled_numbers.rb
@@ -11,7 +11,16 @@ numbers = {
 
 Explanation of Initial Syntax Given:
 
+Syntax:
 numbers = {
+
+Explanation:
+numbers is a variable.
+= means assignment.
+{ means open a hash data structure.
+this means the variable numbers is assigned to a hash data structure.
+
+
   high:   100,
   medium: 50,
   low:    10
