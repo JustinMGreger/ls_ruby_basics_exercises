8f6c2f65e517c2549ed948d1d04594d12f9abd62
Justin Greger <justingreger@gmail.com>
2023-03-31 16:30:39 -0400

    Initial Syntax Given

--- hashes/5_labeled_numbers.rb
+++ hashes/5_labeled_numbers.rb
@@ -1,10 +1,22 @@
 =begin
 Description of Exercise:
-
 Use Hash#each to iterate over numbers and print each element's key/value pair.
 
 Initial Syntax Given:
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Explanation of Initial Syntax Given:
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
