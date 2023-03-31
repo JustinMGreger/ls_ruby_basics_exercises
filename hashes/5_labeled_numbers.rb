8804761ee9676498560daed9f9c75c66b2871bba
Justin Greger <justingreger@gmail.com>
2023-03-31 16:28:50 -0400

    Description of Exercise

--- hashes/5_labeled_numbers.rb
+++ hashes/5_labeled_numbers.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+Use Hash#each to iterate over numbers and print each element's key/value pair.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
