50b1d6d1a9b28ae14d306a644cbabcc0d0fe7b12
Justin Greger <justingreger@gmail.com>
2023-03-31 14:52:31 -0400

    color: 'blue'

--- hashes/4_what_color.rb
+++ hashes/4_what_color.rb
@@ -84,7 +84,15 @@ type: is a symbol key.
 , means more key value pairs after this.
 this means type: is a symbol key assigned the 'sedan' string value.
 
+Syntax:
   color:   'blue',
+
+Explanation:
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+
   year:    2003
 }
 
@@ -100,6 +108,10 @@ type: is a symbol key.
 'sedan' is a string value.
 , means more key value pairs after this.
 this means type: is a symbol key assigned the 'sedan' string value.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
 
 This means:
 
@@ -111,5 +123,9 @@ type: is a symbol key.
 'sedan' is a string value.
 , means more key value pairs after this.
 this means type: is a symbol key assigned the 'sedan' string value.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
 
 =end
