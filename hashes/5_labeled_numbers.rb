98ddd3d0bcccfcb4401125ce711c3f827b4a5c63
Justin Greger <justingreger@gmail.com>
2023-03-31 16:36:40 -0400

    high: 100

--- hashes/5_labeled_numbers.rb
+++ hashes/5_labeled_numbers.rb
@@ -20,8 +20,16 @@ numbers is a variable.
 { means open a hash data structure.
 this means the variable numbers is assigned to a hash data structure.
 
-
+Syntax:
   high:   100,
+
+Explanation:
+high: is a symbol key.
+100 is an integer value.
+, means more key value pairs follow this.
+this means high: is a symbol key assigned 100 which is an integer value.
+
+
   medium: 50,
   low:    10
 }
