ce97f5c0e19de5892d0da607fc1fd4e49e834cad
Justin Greger <justingreger@gmail.com>
2023-03-31 15:54:58 -0400

    puts car[:color]

--- hashes/4_what_color.rb
+++ hashes/4_what_color.rb
@@ -129,6 +129,15 @@ year: is a symbol key.
 this means year: is a symbol key assigned the 2003 integer value.
 this means close the hash data structure that is everything after this line is outside of the hash data structure.
 Hash[] returns the value associated with the given key.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+this means print to the screen car[:color] which is the return the value associated with the given key color: 
+which is a symbol key assigned the 'blue' string value from within the variable car is assigned to a hash data structure 
+containing: type: which is a symbol key assigned the 'sedan' string value, color: which is a symbol key assigned the 
+'blue' string value, year: which is a symbol key assigned the 2003 integer value.
+the output is: blue
 
 Summary of Solution Syntax:
 
@@ -149,6 +158,34 @@ year: is a symbol key.
 this means year: is a symbol key assigned the 2003 integer value.
 this means close the hash data structure that is everything after this line is outside of the hash data structure.
 
+puts means put s which is put string which means print to the screen the string.
+car is a variable.
+= means assignment.
+{ means open a hash data structure.
+this means the variable car is assigned to a hash data structure.
+type: is a symbol key.
+'sedan' is a string value.
+, means more key value pairs after this.
+this means type: is a symbol key assigned the 'sedan' string value.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+year: is a symbol key.
+2003 is a integer value.
+this means year: is a symbol key assigned the 2003 integer value.
+this means close the hash data structure that is everything after this line is outside of the hash data structure.
+Hash[] returns the value associated with the given key.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+this means print to the screen car[:color] which is the return the value associated with the given key color: 
+which is a symbol key assigned the 'blue' string value from within the variable car is assigned to a hash data structure 
+containing: type: which is a symbol key assigned the 'sedan' string value, color: which is a symbol key assigned the 
+'blue' string value, year: which is a symbol key assigned the 2003 integer value.
+the output is: blue
+
 This means:
 
 car is a variable.
@@ -168,4 +205,31 @@ year: is a symbol key.
 this means year: is a symbol key assigned the 2003 integer value.
 this means close the hash data structure that is everything after this line is outside of the hash data structure.
 
+puts means put s which is put string which means print to the screen the string.
+car is a variable.
+= means assignment.
+{ means open a hash data structure.
+this means the variable car is assigned to a hash data structure.
+type: is a symbol key.
+'sedan' is a string value.
+, means more key value pairs after this.
+this means type: is a symbol key assigned the 'sedan' string value.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+year: is a symbol key.
+2003 is a integer value.
+this means year: is a symbol key assigned the 2003 integer value.
+this means close the hash data structure that is everything after this line is outside of the hash data structure.
+Hash[] returns the value associated with the given key.
+color: is a symbol key.
+'blue' is a string value.
+, means more key value pairs after this.
+this means color: is a symbol key assigned the 'blue' string value.
+this means print to the screen car[:color] which is the return the value associated with the given key color: 
+which is a symbol key assigned the 'blue' string value from within the variable car is assigned to a hash data structure 
+containing: type: which is a symbol key assigned the 'sedan' string value, color: which is a symbol key assigned the 
+'blue' string value, year: which is a symbol key assigned the 2003 integer value.
+the output is: blue
 =end
