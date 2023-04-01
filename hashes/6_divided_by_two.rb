7ff59cf599426c4268ee3d74567efd26852d9527
Justin Greger <justingreger@gmail.com>
2023-03-31 21:47:10 -0400

    Initial Syntax Given

--- hashes/6_divided_by_two.rb
+++ hashes/6_divided_by_two.rb
@@ -1,11 +1,23 @@
 =begin
 Description of Exercise:
-
 Use Enumerable#map to iterate over numbers and return an array containing each number divided by 2. 
 Assign the returned array to a variable named half_numbers and print its value using #p.
 
 Initial Syntax Given:
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Explanation of Initial Syntax Given:
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
