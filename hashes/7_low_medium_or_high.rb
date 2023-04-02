fdf7e421d2f0a48ce1f8db529680dbc3e7a57447
Justin Greger <justingreger@gmail.com>
2023-04-01 21:32:53 -0400

    Description of Exercise

--- hashes/7_low_medium_or_high.rb
+++ hashes/7_low_medium_or_high.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+Use Hash#select to iterate over numbers and return a hash containing only key-value pairs where the value is less than 25.
+Assign the returned hash to a variable named low_numbers and print its value using #p.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
