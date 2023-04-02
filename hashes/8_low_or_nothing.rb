e6610b668885205893a220b978942acce3c27365
Justin Greger <justingreger@gmail.com>
2023-04-02 19:54:06 -0400

    Description of Exercise

--- hashes/8_low_or_nothing.rb
+++ hashes/8_low_or_nothing.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+In the following code, numbers isn't mutated because #select isn't a destructive method. 
+However, there is a destructive version of #select named #select!. Modify the code to use #select! instead of #select.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
