b3b706835e88bd0069f54ca19ff13df4a3cddf4c
Justin Greger <justingreger@gmail.com>
2023-04-01 21:33:52 -0400

    Initial Syntax Given

--- hashes/7_low_medium_or_high.rb
+++ hashes/7_low_medium_or_high.rb
@@ -5,7 +5,21 @@ Use Hash#select to iterate over numbers and return a hash containing only key-va
 Assign the returned hash to a variable named low_numbers and print its value using #p.
 
 Initial Syntax Given:
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Explanation of Initial Syntax Given:
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
