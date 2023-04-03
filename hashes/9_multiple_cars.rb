09e5c590794b52a45bb58cd77657b10c9b3113f1
Justin Greger <justingreger@gmail.com>
2023-04-03 19:16:20 -0400

    Initial Syntax Given

--- hashes/9_multiple_cars.rb
+++ hashes/9_multiple_cars.rb
@@ -1,10 +1,29 @@
 =begin
 Description of Exercise:
-
 Create a nested hash using the following data.
 
 Initial Syntax Given:
+
+Car
+
+type	color	year
+sedan	blue	2003
+
+Truck
+
+type	color	year
+pickup red	1998
+
 Explanation of Initial Syntax Given:
+
+
+
+
+
+
+
+
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
