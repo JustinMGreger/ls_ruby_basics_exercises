71aa4468e594bbc6b9c31ea62902339fe0f1ba7c
Justin Greger <justingreger@gmail.com>
2023-04-02 20:50:11 -0400

    p low_numbers

--- hashes/8_low_or_nothing.rb
+++ hashes/8_low_or_nothing.rb
@@ -130,6 +130,8 @@ low_numbers is a variable assigned a new hash containing key value pairs from th
 a hash data structure containing: high: which is a symbol key that is assigned the 100 integer value, medium: which is a 
 symbol key that is assigned the 50 integer value, low: which is a symbol key that is assigned the 10 integer value that 
 return boolean true when values are less than integer 25.
+the result is low_numbers is assigned a hash containing low: which is a symbol key that is assigned the 10 integer value
+that is low_numbers = {low: 10}
 
 Syntax:
                end
@@ -142,8 +144,49 @@ p low_numbers
 
 Explanation:
 p means output.
-
-
+value is block parameter and part of the key value pairs inside of hash data structures.
+< 25 means less that integer 25.
+this means return values less than integer 25 which are boolean true.
+low_numbers is a variable.
+= means assignment.
+numbers is a variable.
+= means assignment.
+{ means open a hash data structure.
+high: is a symbol key.
+100 is a integer value.
+, means more key value pairs following this.
+this means that high: is a symbol key that is assigned the  100 integer value.
+medium: is a symbol key.
+50 is a integer value.
+, means more key value pairs following this.
+this means that medium: is a symbol key that is assigned the 50 integer value.
+low: is a symbol key.
+10 is a integer value.
+this means that low: is a symbol key that is assigned the 10 integer value.
+this means close the hash data structure that is everything after this line is outside of the hash data structure.
+hash.select method returns a new hash containing key value pairs from the original hash that return boolean true when 
+passed block parameters.
+do means open block.
+|key, value| are block parameters and the key value pairs of hash data structure.
+this means low_numbers which is a variable assigned a new hash containing key value pairs from the original hash that 
+return boolean true when block parameters are passed to the variable numbers which is assigned a hash data structure 
+containing: high: which is a symbol key that is assigned the 100 integer value, medium: which is a symbol key that is 
+assigned the 50 integer value, low: which is a symbol key that is assigned the 10 integer value.
+this means close the hash data structure that is everything after this line is outside of the hash data structure.
+this means 
+low_numbers is a variable assigned a new hash containing key value pairs from the variable numbers which is assigned
+a hash data structure containing: high: which is a symbol key that is assigned the 100 integer value, medium: which is a 
+symbol key that is assigned the 50 integer value, low: which is a symbol key that is assigned the 10 integer value that 
+return boolean true when values are less than integer 25.
+the result is low_numbers is assigned a hash containing low: which is a symbol key that is assigned the 10 integer value
+that is low_numbers = {low: 10}
+this means output low_numbers which is a variable assigned a new hash containing key value pairs from the variable numbers
+which is assigned a hash data structure containing: high: which is a symbol key that is assigned the 100 integer value, 
+medium: which is a symbol key that is assigned the 50 integer value, low: which is a symbol key that is assigned the 10 
+integer value that return boolean true when values are less than integer 25.
+the result is: low_numbers is assigned a hash containing low: which is a symbol key that is assigned the 10 integer value
+that is low_numbers = {low: 10}.
+the output is: {:low=>10}
 
 p numbers
 
