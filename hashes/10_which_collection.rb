980fb5290101c5d5bb9cc3ea72a435a45a57e1c6
Justin Greger <justingreger@gmail.com>
2023-04-03 21:18:31 -0400

    Initial Syntax Given

--- hashes/10_which_collection.rb
+++ hashes/10_which_collection.rb
@@ -4,7 +4,21 @@ Description of Exercise:
 Rewrite car as a nested array containing the same key-value pairs.
 
 Initial Syntax Given:
+
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
 Explanation of Initial Syntax Given:
+
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
