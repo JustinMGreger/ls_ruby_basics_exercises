a8b61d21a0febe3f85996c95fdf4a3d0d04697a8
Justin Greger <justingreger@gmail.com>
2023-04-03 21:13:09 -0400

    Description of Exercise

---
+++ hashes/10_which_collection.rb
@@ -0,0 +1,12 @@
+=begin
+Description of Exercise:
+
+Rewrite car as a nested array containing the same key-value pairs.
+
+Initial Syntax Given:
+Explanation of Initial Syntax Given:
+Syntax of Solution:
+Explanation of Solution Syntax:
+Summary of Solution Syntax:
+This means:
+=end
