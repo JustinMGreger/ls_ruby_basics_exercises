1affe6f1d2decc9756f543a32b05265e6d1c60e8
Justin Greger <justingreger@gmail.com>
2023-04-04 18:50:39 -0400

    Description of Exercise

--- debugging/1_reading_error_messages.rb
+++ debugging/1_reading_error_messages.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+You come across the following code. 
+What errors does it raise for the given examples and what exactly do the error messages mean?
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
