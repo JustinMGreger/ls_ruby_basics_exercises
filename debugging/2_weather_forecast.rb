5e44f4465faac538a391af4c702649d58f49b582
Justin Greger <justingreger@gmail.com>
2023-04-05 21:52:36 -0400

    Initial Syntax Given

--- debugging/2_weather_forecast.rb
+++ debugging/2_weather_forecast.rb
@@ -5,7 +5,29 @@ Our predict_weather method should output a message indicating whether a sunny or
 However, the output is the same every time the method is invoked. Why? Fix the code so that it behaves as expected.
 
 Initial Syntax Given:
+
+def predict_weather
+  sunshine = ['true', 'false'].sample
+
+  if sunshine
+    puts "Today's weather will be sunny!"
+  else
+    puts "Today's weather will be cloudy!"
+  end
+end
+
 Explanation of Initial Syntax Given:
+
+def predict_weather
+  sunshine = ['true', 'false'].sample
+
+  if sunshine
+    puts "Today's weather will be sunny!"
+  else
+    puts "Today's weather will be cloudy!"
+  end
+end
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
