908ec89703dec6e3bd4ded21156706ccea94854b
Justin Greger <justingreger@gmail.com>
2023-04-05 21:46:58 -0400

    Description of Exercise

--- debugging/2_weather_forecast.rb
+++ debugging/2_weather_forecast.rb
@@ -1,6 +1,9 @@
 =begin
 Description of Exercise:
 
+Our predict_weather method should output a message indicating whether a sunny or cloudy day lies ahead. 
+However, the output is the same every time the method is invoked. Why? Fix the code so that it behaves as expected.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
