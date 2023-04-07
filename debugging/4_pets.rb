f4dd8eed814d7ab5f007effd9c74c7d0c6f65a47
Justin Greger <justingreger@gmail.com>
2023-04-07 17:24:18 -0400

    Initial Syntax Given

--- debugging/4_pets.rb
+++ debugging/4_pets.rb
@@ -7,7 +7,21 @@ After doing so, she realizes that her dogs Sparky and Fido have been mistakenly 
 Help Magdalena fix her code so that all three of her dogs' names will be associated with the key :dog in the pets hash.
 
 Initial Syntax Given:
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] = 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>"bowser", :fish=>"oscar"}
+
 Explanation of Initial Syntax Given:
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] = 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>"bowser", :fish=>"oscar"}
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
