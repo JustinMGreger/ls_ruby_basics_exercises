b77027e747b213d0311af7cf9de24dae1706f8c0
Justin Greger <justingreger@gmail.com>
2023-04-06 20:52:23 -0400

    Initial Syntax Given

--- debugging/3_multiply_by_five.rb
+++ debugging/3_multiply_by_five.rb
@@ -4,7 +4,27 @@ Description of Exercise:
 When the user inputs 10, we expect the program to print The result is 50!, but that's not the output we see. Why not?
 
 Initial Syntax Given:
+
+def multiply_by_five(n)
+  n * 5
+end
+
+puts "Hello! Which number would you like to multiply by 5?"
+number = gets.chomp
+
+puts "The result is #{multiply_by_five(number)}!"
+
 Explanation of Initial Syntax Given:
+
+def multiply_by_five(n)
+  n * 5
+end
+
+puts "Hello! Which number would you like to multiply by 5?"
+number = gets.chomp
+
+puts "The result is #{multiply_by_five(number)}!"
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
