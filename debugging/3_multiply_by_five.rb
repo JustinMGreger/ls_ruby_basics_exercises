c14600447ffec94c20fc0bd6870b40c3b84a9270
Justin Greger <justingreger@gmail.com>
2023-04-06 21:16:32 -0400

    puts Hello Which number would you like to multiply by 5

--- debugging/3_multiply_by_five.rb
+++ debugging/3_multiply_by_five.rb
@@ -40,7 +40,18 @@ Explanation:
 this is the last line of the multiply_by_five method that is everything after this line is outside of the  
 multiply_by_five method.
 
+Syntax:
 puts "Hello! Which number would you like to multiply by 5?"
+
+Explanation:
+puts means put s which is put string which is print to the screen the string.
+"Hello! Which number would you like to multiply by 5?" is a string.
+this means print to the screen the string "Hello! Which number would you like to multiply by 5?".
+
+
+
+
+
 number = gets.chomp
 
 puts "The result is #{multiply_by_five(number)}!"
