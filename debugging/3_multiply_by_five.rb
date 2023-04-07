fa1530f028d50d3beba4347fe6058bb53e3c6033
Justin Greger <justingreger@gmail.com>
2023-04-06 20:26:53 -0400

    Description of Exercise

--- debugging/3_multiply_by_five.rb
+++ debugging/3_multiply_by_five.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+When the user inputs 10, we expect the program to print The result is 50!, but that's not the output we see. Why not?
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
