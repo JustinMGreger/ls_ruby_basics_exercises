2712f3c20deac34eae8e2095f3a850abaca37d7c
Justin Greger <justingreger@gmail.com>
2023-04-07 20:30:30 -0400

    Initial Syntax Given

--- debugging/5_even_numbers.rb
+++ debugging/5_even_numbers.rb
@@ -1,8 +1,29 @@
 =begin
 Description of Exercise:
 
+We want to iterate through the numbers array and return a new array containing only the even numbers. 
+However, our code isn't producing the expected output. Why not? How can we change it to produce the expected result?
+
 Initial Syntax Given:
+
+numbers = [5, 2, 9, 6, 3, 1, 8]
+
+even_numbers = numbers.map do |n|
+  n if n.even?
+end
+
+p even_numbers # expected output: [2, 6, 8]
+
 Explanation of Initial Syntax Given:
+
+numbers = [5, 2, 9, 6, 3, 1, 8]
+
+even_numbers = numbers.map do |n|
+  n if n.even?
+end
+
+p even_numbers # expected output: [2, 6, 8]
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
