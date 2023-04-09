e851d37bcea9c2607a4c3946ac5606b3ce2fce60
Justin Greger <justingreger@gmail.com>
2023-04-09 15:50:07 -0400

    Description of Exercise

--- debugging/6_confucius_says.rb
+++ debugging/6_confucius_says.rb
@@ -1,6 +1,11 @@
 =begin
 Description of Exercise:
 
+You want to have a small script delivering motivational quotes, 
+but with the following code you run into a very common error message: 
+no implicit conversion of nil into String (TypeError). 
+What is the problem and how can you fix it?
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
