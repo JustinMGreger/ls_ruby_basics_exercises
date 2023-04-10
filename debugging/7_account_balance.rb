48157a6d9fd71a1fc6cc47e22c033e56630ed71f
Justin Greger <justingreger@gmail.com>
2023-04-09 20:15:23 -0400

    Description of Exercise

--- debugging/7_account_balance.rb
+++ debugging/7_account_balance.rb
@@ -1,6 +1,12 @@
 =begin
 Description of Exercise:
 
+Time for a check of your financial situation.
+
+The output of the code below tells you that you have around $70. 
+However, you expected your bank account to have about $238. 
+What did we do wrong?
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
