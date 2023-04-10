15b1b33d167cb1c48069f2d8320224b6dfdfdd63
Justin Greger <justingreger@gmail.com>
2023-04-10 19:52:30 -0400

    balance = calculate_balance(month

--- debugging/7_account_balance.rb
+++ debugging/7_account_balance.rb
@@ -287,7 +287,122 @@ element march is assigned the hash data structure that contains: the income: sym
 array of float values, the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values and
 close the hash data structure that is everything after this is outside of the hash data structure.
 
+Syntax:
   balance = calculate_balance(month)
+
+Explanation:
+balance is a variable.
+= means assignment.
+0 is a integer.
+this means the variable balance is assigned the integer 0.
+= means assignment.
+def means definition.
+calculate_balance is a method.
+(month) is a parameter.
+this means the calculate_balance method accepts the parameter (month) and is defined as follows:
+plus is a variable.
+= means assignment.
+month is a parameter.
+:income is a symbol key that is assigned an array of float values.
+.sum adds up the elements of an array.
+this means the variable plus is assigned the sum of the elements of the array of float values that the symbol key 
+[:income] is assigned within the hash data structure that is selected by the parameter month that is the argument passed 
+to the calculate_balance method.
+minus is a variable.
+= means assignment.
+month is a parameter.
+:expenses is a symbol key that is assigned an array of float values.
+.sum adds up the elements of an array.
+this means the variable minus is assigned the sum of the elements of the array of float values that the symbol key 
+[:expenses] is assigned within the hash data structure that is selected by the parameter month that is the argument passed 
+to the calculate_balance method.
+plus is a variable.
+= means assignment.
+month is a parameter.
+:income is a symbol key that is assigned an array of float values.
+.sum adds up the elements of an array.
+this means the variable plus is assigned the sum of the elements of the array of float values that the symbol key 
+[:income] is assigned within the hash data structure that is selected by the parameter month that is the argument passed 
+to the calculate_balance method.
+- means subtraction.
+minus is a variable.
+= means assignment.
+month is a parameter.
+:expenses is a symbol key that is assigned an array of float values.
+.sum adds up the elements of an array.
+this means the variable minus is assigned the sum of the elements of the array of float values that the symbol key 
+[:expenses] is assigned within the hash data structure that is selected by the parameter month that is the argument passed 
+to the calculate_balance method.
+this means:
+subtract the variable plus which is assigned the sum of the elements of the array of float values that the symbol key 
+[:income] is assigned within the hash data structure that is selected by the parameter month that is the argument passed 
+to the calculate_balance method from the variable minus which is assigned the sum of the elements of the array of float 
+values that the symbol key [:expenses] is assigned within the hash data structure that is selected by the parameter month
+that is the argument passed to the calculate_balance method.
+this is the last line of the calculate_balance method that is everything after this is outside of the calculate_balance 
+method.
+(month) is a parameter.
+[january, february, march] is an array of hash data structures containing:
+january is a variable.
+= means assignment.
+{ means open the hash data structure.
+this means the variable january is assigned the hash data structure that contains:
+income: is a symbol key.
+[ 1200, 75 ] is a array of float values.
+, means more key value pairs follow this.
+this means the income: symbol key is assigned the [ 1200, 75 ] array of float values.
+expenses: is a symbol key.
+[ 650, 140, 33.2, 100, 26.9, 78 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 33.2, 100, 26.9, 78 ] array of float values.
+this means close the hash data structure that is everything after this is outside of the hash data structure.
+february is a variable.
+= means assignment.
+{ means open the hash data structure.
+this means the variable february is assigned the hash data structure that contains:
+income: is a symbol key.
+[ 1200 ] is a array of float values.
+, means more key value pairs follow this.
+this means the income: symbol key is assigned the [ 1200 ] array of float values.
+expenses: is a symbol key.
+[ 650, 140, 320, 46.7, 122.5 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 320, 46.7, 122.5 ] array of float values.
+this means close the hash data structure that is everything after this is outside of the hash data structure.
+march is a variable.
+= means assignment.
+{ means open a hash data structure.
+this means the variable march is assigned the hash data structure that contains:
+income: is a symbol key.
+[ 1200, 10, 75 ] is a array of float values.
+, means more key value pairs follow this.
+this means the income: symbol key is assigned the [ 1200, 10, 75 ] array of float values.
+expenses: is a symbol key.
+[ 650, 140, 350, 12, 59.9, 2.5 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values.
+this means close the hash data structure that is everything after this is outside of the hash data structure.
+.each method allows you to iterate over a collection of elements and perform a block of code for each element 
+in the collection.
+do means open the block.
+|month| is a parameter.
+this means:
+accept the |month| parameter and iterate over a collection of elements and perform a block of code for each element in the
+collection [january, february, march] which is a array of hash data structures containing: the variable array element 
+january which is assigned the hash data structure that contains: the income: symbol key is assigned the [ 1200, 75 ] array
+of float values, the expenses: symbol key is assigned the [ 650, 140, 33.2, 100, 26.9, 78 ] array of float values and 
+close the hash data structure that is everything after this is outside of the hash data structure, the variable array 
+element february which is assigned the hash data structure that contains: the income: symbol key is assigned the [ 1200 ]
+array of float values, the expenses: symbol key is assigned the [ 650, 140, 320, 46.7, 122.5 ] array of float values and 
+close the hash data structure that is everything after this is outside of the hash data structure, the variable array 
+element march is assigned the hash data structure that contains: the income: symbol key is assigned the [ 1200, 10, 75 ] 
+array of float values, the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values and
+close the hash data structure that is everything after this is outside of the hash data structure.
+
+this means:
+
+
+
+
+
+==================================================
 end
 
 puts balance
