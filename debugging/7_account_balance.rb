061eb389a556d38576479b69eb3117b26da2a494
Justin Greger <justingreger@gmail.com>
2023-04-11 20:25:22 -0400

    expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]

--- debugging/7_account_balance.rb
+++ debugging/7_account_balance.rb
@@ -807,9 +807,19 @@ income: is a symbol key.
 , means more key value pairs follow this.
 this means the income: symbol key is assigned the [ 1200, 10, 75 ] array of float values.
 
-
-
+Syntax:
   expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
+
+Explanation:
+expenses: is a symbol key.
+[ 650, 140, 350, 12, 59.9, 2.5 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values.
+
+
+
+
+
+
 }
 
 def calculate_balance(month)
@@ -866,6 +876,10 @@ income: is a symbol key.
 [ 1200, 10, 75 ] is a array of float values.
 , means more key value pairs follow this.
 this means the income: symbol key is assigned the [ 1200, 10, 75 ] array of float values.
+expenses: is a symbol key.
+[ 650, 140, 350, 12, 59.9, 2.5 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values.
+
 
 
 This means:
@@ -909,6 +923,10 @@ income: is a symbol key.
 [ 1200, 10, 75 ] is a array of float values.
 , means more key value pairs follow this.
 this means the income: symbol key is assigned the [ 1200, 10, 75 ] array of float values.
+expenses: is a symbol key.
+[ 650, 140, 350, 12, 59.9, 2.5 ] is a array of float values.
+this means the expenses: symbol key is assigned the [ 650, 140, 350, 12, 59.9, 2.5 ] array of float values.
+
 
 
 =end
