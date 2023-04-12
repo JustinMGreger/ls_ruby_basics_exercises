522397f89d32fc196159769b6c7f35a34781db0c
Justin Greger <justingreger@gmail.com>
2023-04-12 19:20:03 -0400

    Description of Exercise

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -1,6 +1,8 @@
 =begin
 Description of Exercise:
 
+The following code throws an error. Find out what is wrong and think about how you would fix it.
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
