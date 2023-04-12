48704da73c9161b7f014d2368a16e7e4e287d927
Justin Greger <justingreger@gmail.com>
2023-04-12 19:20:53 -0400

    Initial Syntax Given

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -4,7 +4,47 @@ Description of Exercise:
 The following code throws an error. Find out what is wrong and think about how you would fix it.
 
 Initial Syntax Given:
+
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  break if i > colors.length
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+
+  i += 1
+end
+
 Explanation of Initial Syntax Given:
+
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  break if i > colors.length
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+
+  i += 1
+end
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
