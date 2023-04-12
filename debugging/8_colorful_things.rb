a7e1b55fdc927491e6b241d3679f85591ba25d8a
Justin Greger <justingreger@gmail.com>
2023-04-12 19:25:55 -0400

    things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -36,10 +36,15 @@ colors is a variable.
 the variable colors is assigned the array of string elements 
 ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
 
-
-
-
+Syntax:
 things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+Explanation:
+things is a variable.
+= means assignment.
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'] is a array of string elements.
+the variable things is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'].
 
 colors.shuffle!
 things.shuffle!
