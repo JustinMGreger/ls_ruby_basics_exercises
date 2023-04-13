94a1a861b0fbc49cc19c21fce0f33b0a00400142
Justin Greger <justingreger@gmail.com>
2023-04-12 20:26:30 -0400

    break if i > colors.length

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -87,7 +87,38 @@ loop do
 Explanation:
 begin loop.
 
+Syntax:
   break if i > colors.length
+
+Explanation:
+exit the loop.
+if statements mean if boolean true then execute the code.
+i is a variable.
+= means assignment.
+0 is a integer.
+this means the variable i is assigned the integer 0.
+> means greater than.
+colors is a variable.
+= means assignment.
+['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] is a array of string elements.
+the variable colors is assigned the array of string elements 
+['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
+.shuffle! method means permanently randomize the order of elements in the array.
+this means permanently randomize the order of the variable colors which is assigned the array of string elements 
+['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
+.length method returns the length that is the number of characters of that string or array.
+this means:
+exit the loop if the variable i which is assigned the integer 0 is greater than the the number of characters in the 
+permanently randomized order of the variable colors which is assigned the array of string elements 
+['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] then execute the code:
+
+
+
+
+
+
+
+
 
   if i == 0
     puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
