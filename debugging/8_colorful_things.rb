30e370274b0e11748fe50236893fc811ebf7cc66
Justin Greger <justingreger@gmail.com>
2023-04-12 20:00:49 -0400

    things.shuffle

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -59,8 +59,23 @@ the variable colors is assigned the array of string elements
 this means permanently randomize the order of the variable colors which is assigned the array of string elements 
 ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
 
-
+Syntax:
 things.shuffle!
+
+Explanation:
+things is a variable.
+= means assignment.
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'] is a array of string elements.
+the variable things is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'].
+.shuffle! method means permanently randomize the order of elements in the array.
+this means permanently randomize the order of the variable things which is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'].
+
+
+
+
+
 
 i = 0
 loop do
