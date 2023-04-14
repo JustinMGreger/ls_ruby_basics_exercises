50d603c8c959cf6ba99cea17ce67bd76074b02f4
Justin Greger <justingreger@gmail.com>
2023-04-13 20:42:47 -0400

    things

--- debugging/8_colorful_things.rb
+++ debugging/8_colorful_things.rb
@@ -360,20 +360,31 @@ i is a variable.
 0 is a integer.
 this means the variable i is assigned the integer 0.
 > means greater than.
-colors is a variable.
-= means assignment.
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] is a array of string elements.
-the variable colors is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
-.shuffle! method means permanently randomize the order of elements in the array.
-this means permanently randomize the order of the variable colors which is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
+
+things syntax
+
+
+
 .length method returns the length that is the number of characters of that string or array.
 - 1 means subtract 1.
 this means:
 exit the loop if the variable i which is assigned the integer 0 is greater than the number of characters inside of the 
-permanently randomized order of the variable colors which is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] which is then subtracted by 1.
+permanently randomized order of the variable things which is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'] which is then subtracted by 1.
+
+this means:
+the permanently randomized order of the variable things which is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'] has 7 elements.
+
+
+
+
+
+
+
+
+
+
 
 Syntax:
   if i == 0
@@ -543,20 +554,24 @@ i is a variable.
 0 is a integer.
 this means the variable i is assigned the integer 0.
 > means greater than.
-colors is a variable.
-= means assignment.
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] is a array of string elements.
-the variable colors is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
-.shuffle! method means permanently randomize the order of elements in the array.
-this means permanently randomize the order of the variable colors which is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'].
+
+things syntax
+
+
+
+
+
+
+
+
 .length method returns the length that is the number of characters of that string or array.
 - 1 means subtract 1.
 this means:
 exit the loop if the variable i which is assigned the integer 0 is greater than the number of characters inside of the 
-permanently randomized order of the variable colors which is assigned the array of string elements 
-['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black'] which is then subtracted by 1.
+permanently randomized order of the variable things which is assigned the array of string elements 
+['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook'] which is then subtracted by 1.
+
+
 if statements mean if boolean true then execute the code.
 i is a variable.
 = means assignment.
