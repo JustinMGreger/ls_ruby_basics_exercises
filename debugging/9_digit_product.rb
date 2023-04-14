10dba77124f6aac5651cd547e21965178c8516a0
Justin Greger <justingreger@gmail.com>
2023-04-13 21:32:11 -0400

    Description of Exercise

--- debugging/9_digit_product.rb
+++ debugging/9_digit_product.rb
@@ -1,6 +1,11 @@
 =begin
 Description of Exercise:
 
+Given a String of digits, our digit_product method should return the product of all digits in the String argument. 
+You've been asked to implement this method without using reduce or inject.
+
+When testing the method, you are surprised by a return value of 0. What's wrong with this code and how can you fix it?
+
 Initial Syntax Given:
 Explanation of Initial Syntax Given:
 Syntax of Solution:
