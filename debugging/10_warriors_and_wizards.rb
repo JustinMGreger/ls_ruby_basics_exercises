d0badaa01b7d4a4ae219e5fc883450c36b74d4c3
Justin Greger <justingreger@gmail.com>
2023-04-14 21:45:11 -0400

    Initial Syntax Given:

--- debugging/10_warriors_and_wizards.rb
+++ debugging/10_warriors_and_wizards.rb
@@ -4,7 +4,51 @@ Description of Exercise:
 We started writing an RPG game, but we have already run into an error message. Find the problem and fix it.
 
 Initial Syntax Given:
+
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then the player picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength:  20 },
+  thief:   { dexterity: 20 },
+  scout:   { stamina:   20 },
+  mage:    { charisma:  20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase
+
+player.merge(character_classes[input])
+
+puts 'Your character stats:'
+puts player
+
 Explanation of Initial Syntax Given:
+
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then the player picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength:  20 },
+  thief:   { dexterity: 20 },
+  scout:   { stamina:   20 },
+  mage:    { charisma:  20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase
+
+player.merge(character_classes[input])
+
+puts 'Your character stats:'
+puts player
+
 Syntax of Solution:
 Explanation of Solution Syntax:
 Summary of Solution Syntax:
