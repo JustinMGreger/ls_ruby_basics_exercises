11a030265d1e6b2d4892efbebd6198c6a602f1aa
Justin Greger <justingreger@gmail.com>
2023-04-14 21:50:16 -0400

    player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }

--- debugging/10_warriors_and_wizards.rb
+++ debugging/10_warriors_and_wizards.rb
@@ -30,7 +30,30 @@ Explanation of Initial Syntax Given:
 
 # Each player starts with the same basic stats.
 
+Syntax:
 player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+Explanation:
+player is a variable.
+= means assignment.
+{ strength: 10, dexterity: 10, charisma: 10, stamina: 10 } is a hash data structure of symbol key integer value pairs.
+{ means open the hash data structure.
+strength: is a symbol key.
+10 is a integer value.
+, means more symbol key integer value pairs after this.
+this means the strength: symbol key is assigned the 10 integer value.
+charisma: is a symbol key.
+10 is a integer value.
+, means more symbol key integer value pairs after this.
+this means the charisma: symbol key is assigned the 10 integer value.
+stamina: is a symbol key.
+10 is a integer value.
+this means the stamina: symbol key is assigned the 10 integer value.
+} means close the hash data structure that is everything after this is outside of the hash data structure.
+
+
+
+
 
 # Then the player picks a character class and gets an upgrade accordingly.
 
