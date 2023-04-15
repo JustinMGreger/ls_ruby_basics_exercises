3e8ca6345ef1b852aade93695e4e6ff4a646b413
Justin Greger <justingreger@gmail.com>
2023-04-14 21:39:55 -0400

    completed

--- debugging/9_digit_product.rb
+++ debugging/9_digit_product.rb
@@ -598,134 +598,18 @@ the output of which is:
 
 This means:
 
-def means definition.
-digit_product is a method.  
-(str_num) is a parameter.
-this means the digit_product method accepts the (str_num) parameter and is defined as follows:
-digits is a variable.
-= means assignment.
-str_num is a parameter.
-.chars method splits the original string into individual character strings and creates an array of those strings.
-.map method applies a block of code to each element and returns a new array with the transformed values.
-{ |n| n.to_i } is a hash.
-|n| is a block parameter.
-|n| is a block parameter.
-.to_i means transform to integer form.
-this means: 
-the variable digits is assigned the parameter str_num which the digit_product method accepts that is split the original 
-string form of the parameter str_num into individual character string elements and create a new array with the transformed
-values then transform to integer form each of those string elements and returns a new array with the transformed values.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-digits is a variable.
-= means assignment.
-str_num is a parameter.
-.chars method splits the original string into individual character strings and creates an array of those strings.
-.map method applies a block of code to each element and returns a new array with the transformed values.
-{ |n| n.to_i } is a hash.
-|n| is a block parameter.
-|n| is a block parameter.
-.to_i means transform to integer form.
-this means: 
-the variable digits is assigned the parameter str_num which the digit_product method accepts that is split the original 
-string form of the parameter str_num into individual character string elements and create a new array with the transformed
-values then transform to integer form each of those string elements and returns a new array with the transformed values.
-.each method means iterate over a data structure and execute a block of code for each element.
-do means open the block.
-|digit| is the block parameter.
-this means:
-iterate over the variable digits is assigned the parameter str_num which the digit_product method accepts that is split 
-the original string form of the parameter str_num into individual character string elements and create a new array with 
-the transformed values then transform to integer form each of those string elements and returns a new array with the 
-transformed values and execute a block of code for each element.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-*= means varialbe *= means variable = that is assigned to variable * variable that is variable is reassigned to variable
-multiplied by variable.
-this means:
-the variable product is assigned the integer 1 and then reassigned to product * digit.
+the digit_product method accepts the (str_num) parameter and is defined as 
+iterate over the variable digits which is assigned the parameter str_num which the digit_product method accepts that is 
+split the original string form of the parameter str_num into individual character string elements and create a new array
+with the transformed values then transform to integer form each of those string elements and returns a new array with the 
+transformed values and the variable product is assigned the integer 1 and then reassigned to product * digit which are the
+transformed elements.
 this is the last line of the .each method that is everything after this is outside of the .each method.
-return the value of product.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-*= means varialbe *= means variable = that is assigned to variable * variable that is variable is reassigned to variable
-multiplied by variable.
-this means:
-return the value of the variable product which is assigned the integer 1 and then reassigned to product * digit.
+return the value of the variable product which is assigned the integer 1 and then reassigned to product * digit which are
+the transformed elements.
 this is the last line of the digit_product method that is everything after this line is outisde of the digit_product 
 method.
 
-p means output.
-def means definition.
-digit_product is a method.  
-(str_num) is a parameter.
-this means the digit_product method accepts the (str_num) parameter and is defined as follows:
-digits is a variable.
-= means assignment.
-str_num is a parameter.
-.chars method splits the original string into individual character strings and creates an array of those strings.
-.map method applies a block of code to each element and returns a new array with the transformed values.
-{ |n| n.to_i } is a hash.
-|n| is a block parameter.
-|n| is a block parameter.
-.to_i means transform to integer form.
-this means: 
-the variable digits is assigned the parameter str_num which the digit_product method accepts that is split the original 
-string form of the parameter str_num into individual character string elements and create a new array with the transformed
-values then transform to integer form each of those string elements and returns a new array with the transformed values.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-digits is a variable.
-= means assignment.
-str_num is a parameter.
-.chars method splits the original string into individual character strings and creates an array of those strings.
-.map method applies a block of code to each element and returns a new array with the transformed values.
-{ |n| n.to_i } is a hash.
-|n| is a block parameter.
-|n| is a block parameter.
-.to_i means transform to integer form.
-this means: 
-the variable digits is assigned the parameter str_num which the digit_product method accepts that is split the original 
-string form of the parameter str_num into individual character string elements and create a new array with the transformed
-values then transform to integer form each of those string elements and returns a new array with the transformed values.
-.each method means iterate over a data structure and execute a block of code for each element.
-do means open the block.
-|digit| is the block parameter.
-this means:
-iterate over the variable digits is assigned the parameter str_num which the digit_product method accepts that is split 
-the original string form of the parameter str_num into individual character string elements and create a new array with 
-the transformed values then transform to integer form each of those string elements and returns a new array with the 
-transformed values and execute a block of code for each element.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-*= means varialbe *= means variable = that is assigned to variable * variable that is variable is reassigned to variable
-multiplied by variable.
-this means:
-the variable product is assigned the integer 1 and then reassigned to product * digit.
-this is the last line of the .each method that is everything after this is outside of the .each method.
-return the value of product.
-product is a variable.
-= means assignment.
-1 is a integer.
-this means the variable product is assigned the integer 1.
-*= means varialbe *= means variable = that is assigned to variable * variable that is variable is reassigned to variable
-multiplied by variable.
-this means:
-return the value of the variable product which is assigned the integer 1 and then reassigned to product * digit.
-this is the last line of the digit_product method that is everything after this line is outisde of the digit_product 
-method.
-('12345') is the argument passed to the digit_product method which accepts the parameter (str_num).
-this means:
 output the return the value of the variable product which is the result of ('12345') passed to the digit_product method 
 which accepts the parameter (str_num) and is defined as follows: iterate over the variable digits which is assigned the 
 parameter str_num which the digit_product method accepts that is split the original string form of the parameter str_num 
