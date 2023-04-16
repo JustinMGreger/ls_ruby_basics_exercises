6ac15ad8d875a06e8ec35122f3df64faa053d8b8
Justin Greger <justingreger@gmail.com>
2023-04-15 20:29:41 -0400

    dexterity: 20

--- debugging/10_warriors_and_wizards.rb
+++ debugging/10_warriors_and_wizards.rb
@@ -81,9 +81,6 @@ warrior: is a symbol key assigned { strength:  20 } which is a hash data structu
 is inside of a hash data structure which means this is a nested hash data structure which contains the strength: symbol 
 key which is assigned the 20 integer value.
 
-
-
-
 Syntax:
   thief:   { dexterity: 20 },
 
@@ -91,18 +88,13 @@ Explanation:
 thief: is a symbol key.
 { dexterity: 20 } is a hash data structure of a symbol key integer value pair that is inside of a hash data structure 
 which means this is a nested hash data structure.
-dexterity:
-20
-
-
-
-
+dexterity: is a symbol key.
+20 is a integer value.
 , means more symbol key nested hash data structure value pairs after this.
 this means:
-
-
 thief: is a symbol key assigned { dexterity: 20 } which is a hash data structure of a symbol key integer value pair that 
-is inside of a hash data structure which means this is a nested hash data structure.
+is inside of a hash data structure which means this is a nested hash data structure which contains the dexterity: symbol 
+key which is assigned the 20 integer value.
 
 
   scout:   { stamina:   20 },
