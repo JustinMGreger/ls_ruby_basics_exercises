f555f3423600dd30f74946ac0a1b35c7e4897895
Justin Greger <justingreger@gmail.com>
2023-04-16 20:27:54 -0400

    puts player

--- debugging/10_warriors_and_wizards.rb
+++ debugging/10_warriors_and_wizards.rb
@@ -267,8 +267,139 @@ puts is put s which is put string which means print to the screen the string.
 'Your character stats:' is a string.
 this means print to the screen the string 'Your character stats:'.
 
-
+Syntax:
 puts player
+
+Explanation:
+puts is put s which is put string which means print to the screen the string.
+player is a variable.
+= means assignment.
+{ strength: 10, dexterity: 10, charisma: 10, stamina: 10 } is a hash data structure of symbol key integer value pairs.
+{ means open the hash data structure.
+strength: is a symbol key.
+10 is a integer value.
+, means more symbol key integer value pairs after this.
+this means the strength: symbol key is assigned the 10 integer value.
+dexterity: is a symbol key.
+10 is a integer value.
+this means the dexterity: symbol key is assigned the 10 integer value.
+, means more symbol key integer value pairs after this.
+charisma: is a symbol key.
+10 is a integer value.
+, means more symbol key integer value pairs after this.
+this means the charisma: symbol key is assigned the 10 integer value.
+stamina: is a symbol key.
+10 is a integer value.
+this means the stamina: symbol key is assigned the 10 integer value.
+} means close the hash data structure that is everything after this is outside of the hash data structure.
+.merge method combines hash data structures into a new hash without duplicates.
+(character_classes[input]) means access the value of the key input in the character_classes outer hash data structure.
+character_classes is a variable.
+= means assignment.
+{ means open the outer hash data structure.
+this means the character_classes variable is assigned the outer hash data structure which contains:
+warrior: is a symbol key.
+{ strength:  20 } is a hash data structure of a symbol key integer value pair that is inside of a hash data structure which
+means this is a nested hash data structure.
+strength: is a symbol key.
+20 is a integer value.
+, means more symbol key nested hash data structure value pairs after this.
+this means:
+warrior: is a symbol key assigned { strength:  20 } which is a hash data structure of a symbol key integer value pair that 
+is inside of a hash data structure which means this is a nested hash data structure which contains the strength: symbol 
+key which is assigned the 20 integer value.
+thief: is a symbol key.
+{ dexterity: 20 } is a hash data structure of a symbol key integer value pair that is inside of a hash data structure 
+which means this is a nested hash data structure.
+dexterity: is a symbol key.
+20 is a integer value.
+, means more symbol key nested hash data structure value pairs after this.
+this means:
+thief: is a symbol key assigned { dexterity: 20 } which is a hash data structure of a symbol key integer value pair that 
+is inside of a hash data structure which means this is a nested hash data structure which contains the dexterity: symbol 
+key which is assigned the 20 integer value.
+scout: is a symbol key.
+{ stamina:  20 } is a hash data structure of a symbol key integer value pair that is inside of a hash data structure 
+which means this is a nested hash data structure.
+stamina: is a symbol key.
+20 is a integer value.
+, means more symbol key nested hash data structure value pairs after this.
+this means:
+scout: is a symbol key assigned { stamina: 20 } which is a hash data structure of a symbol key integer value pair that is 
+inside of a hash data structure which means this is a nested hash data structure which contains the stamina: symbol key 
+which is assigned the 20 integer value.
+mage: is a symbol key.
+{ charisma:  20 } is a hash data structure of a symbol key integer value pair that is inside of a hash data structure 
+which means this is a nested hash data structure.
+charisma: is a symbol key.
+20 is a integer value.
+this means:
+mage: is a symbol key assigned { charisma: 20 } which is a hash data structure of a symbol key integer value pair that is 
+inside of a hash data structure which means this is a nested hash data structure which contains the charisma: symbol key 
+which is assigned the 20 integer value.
+} means close the character_classes outer hash data structure.
+this means everything after this line is outside of the character_classes outer hash data structure.
+input is a variable.
+= means assignment.
+gets is get s which is get string which is get user input in string form.
+.chomp method removes the new line character at the end of the string.
+.downcase method converts all upcase letters in a string to the lowercase version of the letters.
+this means:
+input is a variable assigned the user input in string form then remove the new line character at the end of the string 
+then convert all the upcase letters in the string to the lowercase version of the letters.
+this means:
+combine the variable player which is assigned the { strength: 10, dexterity: 10, charisma: 10, stamina: 10 } hash data 
+structure of symbol key integer value pairs which contains: the strength: symbol key which is assigned the 10 integer 
+value, the dexterity: symbol key which is assigned the 10 integer value, the charisma: symbol key which is assigned the 10
+integer value, the stamina: symbol key which is assigned the 10 integer value and close the hash data structure that is 
+everything after this is outside of the hash data structure with the accessed value of the key input in the 
+character_classes outer hash data structure into a new hash without duplicates where input is a variable assigned the user
+input in string form which then removes the new line character at the end of the string and then converts all the upcase 
+letters in the string to the lowercase version of the letters and where the character_classes variable is assigned the 
+outer hash data structure which contains: the symbol key warrior: which is assigned { strength:  20 } which is a hash data
+structure of a symbol key integer value pair that is inside of a hash data structure which means this is a nested hash 
+data structure which contains the strength: symbol key which is assigned the 20 integer value, the thief: symbol key which
+is assigned { dexterity: 20 } which is a hash data structure of a symbol key integer value pair that is inside of a hash 
+data structure which means this is a nested hash data structure which contains the dexterity: symbol key which is assigned
+the 20 integer value, the scout: symbol key which is assigned { stamina: 20 } which is a hash data structure of a symbol 
+key integer value pair that is inside of a hash data structure which means this is a nested hash data structure which 
+contains the stamina: symbol key which is assigned the 20 integer value, the mage: symbol key which is assigned 
+{ charisma: 20 } which is a hash data structure of a symbol key integer value pair that is inside of a hash data structure
+which means this is a nested hash data structure which contains the charisma: symbol key which is assigned the 20 integer
+value and close the character_classes outer hash data structure which means everything after this line is outside of the 
+character_classes outer hash data structure.
+the result of which is: 
+the error is that input is in string form and not symbol form which is what the character_classes outer hash data 
+structure uses which means it is not accessible that is it doesn't exist and the result of which is nil.
+the error is that the result of the merge method does not mutate the original data and is not assigned to a variable which
+means that the transformed new hash data structure is not accessible.
+this means:
+print to the screen the result of combining the variable player which is assigned the 
+{ strength: 10, dexterity: 10, charisma: 10, stamina: 10 } hash data structure of symbol key integer value pairs which 
+contains: the strength: symbol key which is assigned the 10 integer value, the dexterity: symbol key which is assigned the
+10 integer value, the charisma: symbol key which is assigned the 10 integer value, the stamina: symbol key which is 
+assigned the 10 integer value and close the hash data structure that is everything after this is outside of the hash data
+structure with the accessed value of the key input in the character_classes outer hash data structure into a new hash 
+without duplicates where input is a variable assigned the user input in string form which then removes the new line 
+character at the end of the string and then converts all the upcase letters in the string to the lowercase version of the
+letters and where the character_classes variable is assigned the outer hash data structure which contains: the symbol key
+warrior: which is assigned { strength:  20 } which is a hash data structure of a symbol key integer value pair that is 
+inside of a hash data structure which means this is a nested hash data structure which contains the strength: symbol key
+which is assigned the 20 integer value, the thief: symbol key which is assigned { dexterity: 20 } which is a hash data 
+structure of a symbol key integer value pair that is inside of a hash data structure which means this is a nested hash
+data structure which contains the dexterity: symbol key which is assigned the 20 integer value, the scout: symbol key 
+which is assigned { stamina: 20 } which is a hash data structure of a symbol key integer value pair that is inside of a 
+hash data structure which means this is a nested hash data structure which contains the stamina: symbol key which is 
+assigned the 20 integer value, the mage: symbol key which is assigned { charisma: 20 } which is a hash data structure of a
+symbol key integer value pair that is inside of a hash data structure which means this is a nested hash data structure 
+which contains the charisma: symbol key which is assigned the 20 integer value and close the character_classes outer hash
+data structure which means everything after this line is outside of the character_classes outer hash data structure.
+the result of which is: 
+the error is that input is in string form and not symbol form which is what the character_classes outer hash data 
+structure uses which means it is not accessible that is it doesn't exist and the result of which is nil.
+the error is that the result of the merge method does not mutate the original data and is not assigned to a variable which
+means that the transformed new hash data structure is not accessible.
+
 
 Syntax of Solution:
 Explanation of Solution Syntax:
